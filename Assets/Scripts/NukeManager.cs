eb21cc3029ce5f61de4576a724f8f9ca0714412d
João Buratto <46575243+joaoburatto@users.noreply.github.com>
2019-08-11 19:07:55 -0300

    Tiles and Mobs (#29)
    * New Materials
    
    Palette05 and Palette05 Emission
    
    * A little step
    
    - Folder hierarchy set up for models and prefabs.
    - A new scene "Station" for the recreation of NSS Cyberiad.
    - Materials: Glass, Palette01, Palette03, Palette05 and Palette 05 Emission added.
    - Added 5 tiles, Blue14th, Blue22th, Blue34th, Blue44th and BlueCheckers.
    - Added Crowbar and Flashlight models. (may need collision rework).
    - Added Locker and Filing Cabinet (may need collision rework).
    -  Added Microwave and Nuke. (may need collision rework).
    
    * Tiles
    
    * Cockroach and Map changes
    
    Added the cockroach model, idle and moving animations too, and little changes to the map
    
    * Tiles and Mobs
    
    Added Beepsky and the Cockroach, both has animation controllers on them.
    Changed a little the materials and added a few needed
    (Emissive AI, Emissive Green, Emissive Red, Emissive Palette05 (realocated).
    - Tiles realocated.
    - Orange tiles added, 14th, 24th, 34th, 44th, and checkers, also added, half checker half floor added too.
    - Added a placeholder for the freezer floor.
    - Added the wood floor.
    
    * collision to the wall panels
    
    * Beepsky fixed
    
    Due changing of TempMovement to Human Movement, the script was missing.
    
    * Extremely basic nuke script and fixes
    
    - NukeManager script added, it just hide nuke's disk.
    - Changes into the prefabs, mostly corrections
    - Nuke model fixed, scale was on 100.

---
+++ Assets/Scripts/NukeManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NukeManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject disk;
+    [SerializeField]
+    private bool hasDisk;
+
+    void Update()
+    {
+        disk.transform.localScale = hasDisk ? new Vector3(1, 1, 1) : new Vector3(0, 0, 0); // verifies hasDisk and then choose which transform will be
+    }
+}
