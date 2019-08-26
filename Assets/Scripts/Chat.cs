e706f296336e3e3ba7d7f8173a4e18b5aadfa0c2
João Buratto <joaoburattoc@gmail.com>
2019-08-26 03:05:05 -0300

    Added basic airlock interaction
    Airlocks now open by proximity, and click (no click range yet), and sounds.

---
+++ Assets/Scripts/Chat.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chat : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("T"))
+        {
+         //
+        }       
+    }
+}
