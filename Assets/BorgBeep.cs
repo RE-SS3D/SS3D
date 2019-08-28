9edd0dfc4bd02aa897a7fb03a307517943963e92
João Buratto <joaoburattoc@gmail.com>
2019-08-28 08:20:21 -0300

    Airlocks and Borgs
    Added the AirlockManager , a chair, sit animation, and the beep borg

---
+++ Assets/BorgBeep.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BorgBeep : MonoBehaviour
+
+   
+
+{
+
+    [SerializeField] bool on = false;
+
+    [SerializeField] Light l1;
+    [SerializeField] Light l2;
+
+    [SerializeField] MeshRenderer beepLight;
+
+    [SerializeField] float intensity;
+
+    [SerializeField] Material lOn;
+    [SerializeField] Material lOff;
+
+    MaterialChanger mc = new MaterialChanger();
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("c"))
+        {
+            UseSelf();
+        }
+    }
+
+    public void UseSelf()
+    {
+        if (!on)
+        {
+            mc.ChangeMaterial(beepLight, lOn);
+            l1.intensity = intensity;
+            l2.intensity = intensity;
+            on = true;
+
+        }
+        else
+        {
+            mc.ChangeMaterial(beepLight, lOff);
+            l1.intensity = 0f;
+            l2.intensity = 0f;
+            on = false;
+
+        }
+    }
+}
---
+++ Assets/Chair.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chair : MonoBehaviour
+{
+    [SerializeField]
+    GameObject seat_slot;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnMouseDown()
+    {
+        Sit();
+    }
+
+    void Sit(GameObject target)
+    {
+        target.transform.position = seat_slot.transform.position;
+    }
+}
