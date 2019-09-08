9d94fee57d2aeef48bcc6ed6d8e47603d04d35f1
Alex K <BangbangfmGames@gmail.com>
2019-09-08 04:43:28 +0300

    Bit better camera
    Small changes in camera movement and Q/E camera rotation.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -13,9 +13,17 @@ public class CameraFollow : MonoBehaviour
 
     // the input
     public float scrollSpeed = 2f; // scroll speed multiplier
-    private bool rotation = false; // the rotation bool
+    public float rotationSpeed = 110f; // rotation speed
+    public float camera_angle = 62f;
+
+    private float rotation = 0; // current rotation delta
+    private float rotationLast = 0; // rotation memory 
     private float mouseX; // the previous X mouse position
     private float scrollWheel; // the scroll wheel
+    private float currentAngle; // curent camera angle 
+
+    private float input_timer; // track when button was pressed
+    private Vector3 targetFollower = Vector3.zero;
     
     private void LateUpdate() 
     {
@@ -29,65 +37,72 @@ public class CameraFollow : MonoBehaviour
         {
             return;
         }
-            // input handling
-            scrollWheel = Input.GetAxisRaw("Mouse ScrollWheel");
-            rotation = Input.GetButton("Camera Rotation"); 
 
+        // Save previous rotation value
+        rotationLast = rotation;
 
-            if (rotation) // is the Middle Mouse button pressed
-            {
-                if(Input.mousePosition.x != mouseX) // did the mouse move from the last position                  
-                {
-                    angle += (Input.mousePosition.x - mouseX) * 10f * Time.deltaTime; // recalculate the angle                        
-                }
-            }
+        // input handling
+        scrollWheel = Input.GetAxisRaw("Mouse ScrollWheel");
+        rotation = Input.GetAxisRaw("Camera Rotation");
+        
+        if (Input.GetButtonDown("Camera Rotation")) input_timer = Time.time;
+        if (Input.GetButtonUp("Camera Rotation") && Time.time - input_timer < 0.3f)
+        {
+            angle += (rotationLast > 0 ? 1 :-1) * (angle % 90 == 0 ? 90 : 45);
+            angle = Mathf.Round(angle / 90) * 90;
+        }
 
-            ZoomCap(); // limits the zoom
+        if (Input.GetButton("Camera Rotation") && Time.time - input_timer >= 0.3f)
+        {
+            angle += rotation * Time.deltaTime * rotationSpeed;
+        }
 
-            //zooms in and out based on the scroll wheel
-            distance -= scrollWheel * scrollSpeed;
-            height -= scrollWheel * scrollSpeed;
+        ZoomCap(); // limits the zoom
 
-            Vector3 worldPos = (Vector3.forward * -distance) + (Vector3.up * height); // calculates the camera position
-            Vector3 camAngle = Quaternion.AngleAxis(angle, Vector3.up) * worldPos; // calculates the camera angle
+        //zooms in and out based on the scroll wheel
+        distance -= scrollWheel * scrollSpeed;
+        height -= scrollWheel * scrollSpeed;
+
+        targetFollower = Vector3.Lerp(targetFollower, target.transform.position, Time.deltaTime * 8f);
+
+        Vector3 worldPos = (Vector3.forward * -distance) + (Vector3.up * height); // calculates the camera position
+        currentAngle = Mathf.LerpAngle(currentAngle, angle, Time.deltaTime * 8f);
+        Vector3 camAngle = Quaternion.AngleAxis(currentAngle, Vector3.up) * worldPos; // calculates the camera angle
             
-            // gets the flat position of the target 
-            Vector3 flatTargetPos = new Vector3(target.transform.position.x, 0f, target.transform.position.z);
-            Vector3 finalPos = flatTargetPos + camAngle; // the final camera position 
-            
-            // smoothly goes to the final position
-            this.transform.position = Vector3.Lerp(this.transform.position, finalPos, Time.deltaTime * 8f);
-            this.transform.eulerAngles = new Vector3(62f, angle , 0f);
+        // gets the flat position of the target 
+        Vector3 flatTargetPos = new Vector3(targetFollower.x, 0f, targetFollower.z);
+        this.transform.position = flatTargetPos + camAngle; // the final camera position 
+        this.transform.LookAt(targetFollower);
     }
 
     //limits the zoom
     private void ZoomCap()
     {
-            float minHeight = 3f; // the minimum height limit variable
-            float maxHeight = 11f; // the maximum height limit variable
+        float minHeight = 3f; // the minimum height limit variable
+        float maxHeight = 11f; // the maximum height limit variable
 
-            float minDistance = 0.5f; // the minimum distance limit variable
-            float maxDistance = 8.5f; // the maximum distance limit variable
+        float minDistance = 0.5f; // the minimum distance limit variable
+        float maxDistance = 8.5f; // the maximum distance limit variable
 
-            // the height limit
-            if(height < minHeight) // the minimum height limit
-            {
-                height = minHeight;
-            }
-            else if(height > maxHeight) // the maximum height limit
-            {
-                height = maxHeight;
-            }
+        // the height limit
+        if(height < minHeight) // the minimum height limit
+        {
+            height = minHeight;
+        }
+        else if(height > maxHeight) // the maximum height limit
+        {
+            height = maxHeight;
+        }
             
-            // the distance limit
-            if (distance < minDistance) // the minimum distance limit
-            {
-                distance = minDistance;
-            }
-            else if(distance > maxDistance) // the maximum distance limit
-            {
-                distance = maxDistance;
-            }
+        // the distance limit
+        if (distance < minDistance) // the minimum distance limit
+        {
+            distance = minDistance;
+        }
+        else if(distance > maxDistance) // the maximum distance limit
+        {
+            distance = maxDistance;
+        }
     }
 
 
