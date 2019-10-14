053d05761bf790491d5a56a585366fd241b3f192
Timothy Finucane <timfinucane@outlook.com>
2019-10-14 15:51:13 +1300

    Update player movement
    - Floating is now an animation, rather than being handled in code
    - movement uses input axis values for smoothing and input acceleration rather than rolling own
    - Code is simplified with preparation for

--- Assets/Scripts/HumanMovement.cs
+++ Assets/Scripts/HumanMovement.cs
@@ -2,23 +2,15 @@
 using Mirror;
 
 [RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(Animator))]
 public class HumanMovement : NetworkBehaviour
 {
+    // The base speed at which the given character can move
     [SyncVar]
-    private float gravity = 10f;
-    [SyncVar]
-    public float speed = 10f;
-    [SyncVar]
-    private float acceleration = 10f;
-    private float floatingStartTime;
-    private float bobbingDistance = 0.05f;
-    private float floatingSpeed = 5.0f;
-    public float floatingHeight = -1.987f;
-    private bool floating = false;
+    public float characterSpeed = 5f;
 
     private Animator characterAnimator;
     private CharacterController characterController;
-    private Vector2 currentDirection;
     private new Camera camera;
 
     private void Start()
@@ -30,52 +22,44 @@ public class HumanMovement : NetworkBehaviour
 
     void Update()
     {
+        // Must be the local player, or they cannot move
         if (!isLocalPlayer)
-        {
-            // exit from update if this is not the local player
             return;
-        }
         
-        var input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
-        currentDirection = Vector2.MoveTowards(currentDirection, input, acceleration * Time.deltaTime);
-        
+        // TODO: Get these values from the proper places they will be generated
+        bool hasGravity = true;
+        bool canGrabSomething = false;
+        // TODO: Check other methods of moving, e.g. jetpack
 
-        //characterAnimator.SetFloat("Speed", 1f);
-        if(currentDirection.magnitude > 0)
-        {
-            // Move relative to the camera
-            Transform relativeMovement = camera.transform;
-            if(relativeMovement != null)
+        Vector2 inputDirection = new Vector2();
+        if (hasGravity || canGrabSomething) {
+            // Calculate next movement
+            // The vector is not normalized to allow for the input having potential rise and fall times
+            float x = Input.GetAxis("Horizontal");
+            float y = Input.GetAxis("Vertical");
+
+            // The input direction ramps up to at most 1. The user accelerates based on input acceleration.
+            inputDirection.Set(x, y);
+            inputDirection.Normalize();
+            inputDirection *= Mathf.Min(Mathf.Max(Mathf.Abs(x), Mathf.Abs(y)), 1.0f);
+
+            // Move the player
+            if(inputDirection.magnitude > 0)
             {
-                var forward = Vector3.ProjectOnPlane(relativeMovement.forward, Vector3.up);
-                forward.Normalize();
-                var right = Vector3.ProjectOnPlane(relativeMovement.right, Vector3.up);
-                right.Normalize();
-                
-                var dir = right * currentDirection.x + forward * currentDirection.y;
-                characterController.Move(dir * Time.deltaTime * speed);
-                transform.rotation = Quaternion.LookRotation(dir);
+                // Determine the absolute movement by aligning input to the camera's looking direction
+                Vector3 absoluteMovement = inputDirection.y * Vector3.Cross(camera.transform.right, Vector3.up) + inputDirection.x * Vector3.Cross(Vector3.up, camera.transform.forward);
+                // Move (without gravity). Whenever we move we also readjust the player's direction to the direction they are running in.
+                characterController.Move(absoluteMovement * characterSpeed * Time.deltaTime);
+                transform.rotation = Quaternion.LookRotation(absoluteMovement);
             }
         }
-        if (characterController.transform.position.y > floatingHeight)
-        {
-            characterController.Move(Vector3.down * gravity * Time.deltaTime);
-            characterAnimator.SetFloat("Speed", new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).magnitude);
-            floating = false;
-        } 
         else
-        {            
-            if(!floating)
-            {
-                floating = true;
-                floatingStartTime = Time.time;
-                characterAnimator.SetFloat("Speed", Vector2.zero.magnitude);
-            }
-            var pos = characterController.transform.position;
-            var floatPos = new Vector3(characterController.transform.position.x,
-             floatingHeight - bobbingDistance - Mathf.Cos(Mathf.PI + (Time.time - floatingStartTime) * floatingSpeed) * bobbingDistance,
-             characterController.transform.position.z);
-            characterController.transform.position = floatPos;
+        {
+            inputDirection.Set(0, 0);
         }
+
+        // TODO: Might eventually want more animation options. E.g. when in 0-gravity and 'clambering' via a surface
+        characterAnimator.SetBool("Floating", !hasGravity); // Note: Player can be floating and still move
+        characterAnimator.SetFloat("Speed", inputDirection.magnitude);
     }
 }
