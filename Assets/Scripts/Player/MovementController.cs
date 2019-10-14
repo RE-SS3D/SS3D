ce3cadd8e9937453f56b312a09c0652501c95e82
Timothy Finucane <timfinucane@outlook.com>
2019-10-14 17:40:56 +1300

    Update movement code for running and walking

--- Assets/Scripts/Player/MovementController.cs
+++ Assets/Scripts/Player/MovementController.cs
@@ -5,13 +5,22 @@ using Mirror;
 [RequireComponent(typeof(Animator))]
 public class MovementController : NetworkBehaviour
 {
+    public const float ACCELERATION = 10f;
+
     // The base speed at which the given character can move
     [SyncVar]
-    public float characterSpeed = 5f;
+    public float runSpeed = 5f;
+    // The base speed for the character when walking. To disable walkSpeed, set it to runSpeed
+    [SyncVar]
+    public float walkSpeed = 2f;
 
     private Animator characterAnimator;
     private CharacterController characterController;
     private new Camera camera;
+
+    // Current movement the player is making.
+    private Vector2 currentMovement = new Vector2();
+    private bool isWalking = false;
 
     private void Start()
     {
@@ -25,41 +34,42 @@ public class MovementController : NetworkBehaviour
         // Must be the local player, or they cannot move
         if (!isLocalPlayer)
             return;
+
+        if(Input.GetButtonDown("Toggle Run"))
+            isWalking = !isWalking;
         
         // TODO: Get these values from the proper places they will be generated
         bool hasGravity = true;
         bool canGrabSomething = false;
         // TODO: Check other methods of moving, e.g. jetpack
 
-        Vector2 inputDirection = new Vector2();
         if (hasGravity || canGrabSomething) {
             // Calculate next movement
             // The vector is not normalized to allow for the input having potential rise and fall times
-            float x = Input.GetAxis("Horizontal");
-            float y = Input.GetAxis("Vertical");
+            float x = Input.GetAxisRaw("Horizontal");
+            float y = Input.GetAxisRaw("Vertical");
 
-            // The input direction ramps up to at most 1. The user accelerates based on input acceleration.
-            inputDirection.Set(x, y);
-            inputDirection.Normalize();
-            inputDirection *= Mathf.Min(Mathf.Max(Mathf.Abs(x), Mathf.Abs(y)), 1.0f);
+            // Smoothly transition to next intended movement
+            Vector2 intendedMovement = new Vector2(x, y).normalized * (isWalking ? walkSpeed : runSpeed);
+            currentMovement = Vector2.MoveTowards(currentMovement, intendedMovement, Time.deltaTime * ACCELERATION);
 
             // Move the player
-            if(inputDirection.magnitude > 0)
+            if (currentMovement.magnitude > 0)
             {
                 // Determine the absolute movement by aligning input to the camera's looking direction
-                Vector3 absoluteMovement = inputDirection.y * Vector3.Cross(camera.transform.right, Vector3.up) + inputDirection.x * Vector3.Cross(Vector3.up, camera.transform.forward);
+                Vector3 absoluteMovement = currentMovement.y * Vector3.Cross(camera.transform.right, Vector3.up) + currentMovement.x * Vector3.Cross(Vector3.up, camera.transform.forward);
                 // Move (without gravity). Whenever we move we also readjust the player's direction to the direction they are running in.
-                characterController.Move(absoluteMovement * characterSpeed * Time.deltaTime);
+                characterController.Move(absoluteMovement * Time.deltaTime);
                 transform.rotation = Quaternion.LookRotation(absoluteMovement);
             }
         }
         else
         {
-            inputDirection.Set(0, 0);
+            currentMovement.Set(0, 0);
         }
 
         // TODO: Might eventually want more animation options. E.g. when in 0-gravity and 'clambering' via a surface
         characterAnimator.SetBool("Floating", !hasGravity); // Note: Player can be floating and still move
-        characterAnimator.SetFloat("Speed", inputDirection.magnitude);
+        characterAnimator.SetFloat("Speed", currentMovement.magnitude / runSpeed); // animation Speed is a proportion of maximum runSpeed
     }
 }
