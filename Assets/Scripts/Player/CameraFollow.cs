98c75dc2dd4655dfd0510bd3b4615bc68c0c19a1
Timothy Finucane <timfinucane@outlook.com>
2019-10-18 12:02:58 +1300

    Smoothen camera snapping, uses single press only

--- Assets/Scripts/Player/CameraFollow.cs
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -6,6 +6,7 @@ public class CameraFollow : MonoBehaviour
 {
     // Sensitivities and Accelerations
     private const float DISTANCE_ACCELERATION = 15.0f; // How quickly distance changes
+    private const float ANGLE_ACCELERATION = 8f;
     private const float DISTANCE_SCALING = 1.18f; // The exponential effect of distance
 
     private const float HORIZONTAL_ROTATION_SENSITIVITY = 150f;
@@ -18,7 +19,7 @@ public class CameraFollow : MonoBehaviour
     private const float MIN_DISTANCE = 2f;
     private const float MAX_DISTANCE = 15f;
 
-    private const float DOUBLE_PRESS_TIME = 2.0f;
+    private const float CARDINAL_SNAP_TIME = 0.3f;
 
 
     // The object to follow
@@ -49,16 +50,15 @@ public class CameraFollow : MonoBehaviour
     {
         // Check for double tap
         if (Input.GetButtonDown("Camera Rotation"))
+            prevHorizontalAxisPress = Time.time;
+
+        // If a double tap actually works
+        // Round to closest 90 degree angle, going up or down based on whether axis is positive or negative
+        if (Input.GetButtonUp("Camera Rotation") && (Time.time - prevHorizontalAxisPress) < CARDINAL_SNAP_TIME)
         {
-            // If a double tap actually works
-            // Round to closest 90 degree angle, going up or down based on whether axis is positive or negative
-            if ((Time.time - prevHorizontalAxisPress) < DOUBLE_PRESS_TIME)
-            {
-                angle = (Input.GetAxis("Camera Rotation") > 0 ? Mathf.Ceil(angle / 90.0f) : Mathf.Floor(angle / 90.0f)) * 90.0f;
-                prevHorizontalAxisPress = 0.0f;
-                return;
-            }
-            prevHorizontalAxisPress = Time.time; // Set up the first tap
+            angle = (Input.GetAxis("Camera Rotation") > 0 ? Mathf.Ceil(angle / 90.0f) : Mathf.Floor(angle / 90.0f)) * 90.0f;
+            prevHorizontalAxisPress = 0.0f;
+            return;
         }
 
         // input handling
@@ -98,10 +98,11 @@ public class CameraFollow : MonoBehaviour
             return;
         }
         
-        // Smooth the distance before using it
+        // Smooth the distance and angle before using it
+        curHorizontalAngle = Mathf.LerpAngle(curHorizontalAngle, angle, Time.deltaTime * ANGLE_ACCELERATION);
         currentDistance = Mathf.MoveTowards(currentDistance, distance, Time.deltaTime * DISTANCE_ACCELERATION);
         // The position is determined by the orientation and the distance, where distance has an exponential effect.
-        Vector3 relativePosition = Quaternion.Euler(0, angle, vAngle) * new Vector3(Mathf.Pow(DISTANCE_SCALING, currentDistance), 0, 0);
+        Vector3 relativePosition = Quaternion.Euler(0, curHorizontalAngle, vAngle) * new Vector3(Mathf.Pow(DISTANCE_SCALING, currentDistance), 0, 0);
         // Determine the part of the target we want to follow
         Vector3 targetPosition = target.transform.position + playerOffset;
 
@@ -113,6 +114,7 @@ public class CameraFollow : MonoBehaviour
     // Previous button downs for left and right axis movement
     private float prevHorizontalAxisPress = 0.0f;
 
+    private float curHorizontalAngle;
     private float currentDistance;
 
     // Offset of target transform position to camera focus point.
