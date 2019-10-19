90822de241d4dcb34b455af61a4f6e1e1ba3717c
Daniel Thompson <d.kqgqn@gmail.com>
2019-10-20 00:06:25 +0100

    Adds base for Tiles, Turfs, and Connections

---
+++ Assets/Scripts/Turfs/Turf.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Mirror;
+using UnityEngine;
+
+// Turfs. Turfs relate to the combination of underplating (or open space), and the wall or floor above them.
+
+public class Turf : ScriptableObject
+{
+    TileManager tileManager;
+
+    public virtual void Initialize(TileManager parent)
+    {
+        tileManager = parent;
+    }
+
+    public virtual Mesh getMesh(Tile tile)
+    {
+        return new Mesh();
+    }
+}
---
+++ Assets/Scripts/Utilities/ConnectionManager.cs
@@ -0,0 +1,9 @@
+using System;
+using Mirror;
+using UnityEngine;
+
+public class ConnectionManager : Component
+{
+    GameObject parent;
+    int[] connections;
+}
