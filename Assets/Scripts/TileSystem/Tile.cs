69b7cddff7571363d082cd49c407ded46bae1998
Daniel Thompson <d.kqgqn@gmail.com>
2019-10-20 00:06:25 +0100

    Adds base for Tiles, Turfs, and Connections

---
+++ Assets/Scripts/TileSystem/Tile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Mirror;
+using UnityEngine;
+
+public class Tile : NetworkBehaviour
+{
+    TileManager tileManager;
+    MeshFilter meshFilter;
+
+    Turf turf;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/TileSystem/TileManager.cs
+++ Assets/Scripts/TileSystem/TileManager.cs
@@ -1,88 +1,106 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using Mirror;
 using UnityEngine;
 
-namespace Mirror.Examples.Basic
+public class TileManager : NetworkBehaviour
 {
-    public class TileManager : MonoBehaviour
+    public Vector3Int bounds;
+
+    public Tile[] grid;
+
+    // Direction bitflags used by the TileManager
+    [HideInInspector]
+    public const int NORTH = 1;
+
+    [HideInInspector]
+    public const int SOUTH = 2;
+
+    [HideInInspector]
+    public const int EAST = 4;
+
+    [HideInInspector]
+    public const int WEST = 8;
+
+    [HideInInspector]
+    public const int UP = 16;
+
+    [HideInInspector]
+    public const int DOWN = 32;
+
+    [HideInInspector]
+    public int[] CARDINALS = {
+        NORTH,
+        EAST,
+        SOUTH,
+        WEST
+    };
+
+    [HideInInspector]
+    public int[] ORDINALS = {
+        NORTH|EAST,
+        SOUTH|EAST,
+        SOUTH|WEST,
+        NORTH|WEST
+    };
+
+    // Vector3Int getCoordsFromGrid(int index)
+    // {
+    //     //return object coords in the index]
+    //     Vector3Int coord(1,1,1);
+    //     return ;
+    // }
+
+    int getLinearIndex(int x, int y, int z)
     {
+        return (bounds.y * bounds.x * z) + (bounds.x * y) + x;
+    }
 
-        public Vector3Int bounds;
+    int getLinearIndex(Vector3Int coords)
+    {
+        return getLinearIndex(coords.x, coords.y, coords.z);
+    }
 
-        public Object[] grid;
+    int getLinearIndex(Tile tile)
+    {
+        // return getLinearIndex(tile.x, tile.y, tile.z);
+        return 1;
+    }
 
-        // Direction bitflags used by the TileManager
-        [HideInInspector]
-        public const int NORTH = 1;
+    // void adjustBounds(Vector3Int newBounds)
+    // {
+    //     Tile[] newGrid;
+    //     for(int i = 0; i < grid.Length; i++)
+    //     {
+    //         // Vector3Int coords = getCoordsFromGrid(i);
+    //         // int newIndex = getLinearIndex()
+    //     }
+    // }
 
-        [HideInInspector]
-        public const int SOUTH = 2;
-
-        [HideInInspector]
-        public const int EAST = 4;
-
-        [HideInInspector]
-        public const int WEST = 8;
-
-        [HideInInspector]
-        public const int UP = 16;
-
-        [HideInInspector]
-        public const int DOWN = 32;
-
-        [HideInInspector]
-        public int[] CARDINALS = {
-            NORTH,
-            EAST,
-            SOUTH,
-            WEST
-        };
-
-        [HideInInspector]
-        public int[] ORDINALS = {
-            NORTH|EAST,
-            SOUTH|EAST,
-            SOUTH|WEST,
-            NORTH|WEST
-        };
-
-        // Vector3Int getCoordsFromGrid(int index)
-        // {
-        //     //return object coords in the index]
-        //     Vector3Int coord(1,1,1);
-        //     return ;
-        // }
-
-        int getLinearIndex(int x, int y, int z)
+    void generatePrefabMap()
+    {
+        for(int i = 0; i < bounds.x; i++)
         {
-            return (bounds.y * bounds.x * z) + (bounds.x * y) + x;
-        }
-
-        int getLinearIndex(Vector3Int coords)
-        {
-            return getLinearIndex(coords.x, coords.y, coords.z);
-        }
-
-        void adjustBounds(Vector3Int newBounds)
-        {
-            Object[] newGrid;
-            for(int i = 0; i < grid.Length; i++)
+            for(int j = 0; i < bounds.y; j++)
             {
-                // Vector3Int coords = getCoordsFromGrid(i);
-                // int newIndex = getLinearIndex()
+                Tile newTile = new Tile();
+                int index = getLinearIndex(newTile);
+                grid[index] = newTile;
             }
         }
+    }
 
-        // Start is called before the first frame update
-        void Start()
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(isServer)
         {
-            
+            generatePrefabMap();
         }
+    }
 
-        // Update is called once per frame
-        void Update()
-        {
-            
-        }
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
