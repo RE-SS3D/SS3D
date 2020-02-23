bea56b8cc340fac2fac2fbd51bdd1fe416c4acea
Markus Peter Långstedt <30968435+Markus-P-L@users.noreply.github.com>
2020-02-23 08:08:40 +0200

    Blob Shadows (#145)
    * BlobShadowProjector
    
    Early version of bloby shadows
    
    * BlobShadows
    
    * BlobShadows
    
    * Fixing unintended changes
    
    * Some more fixing
    
    * Update MovementController.cs
    
    * Update BlobShadowProjector.prefab
    
    * restored Human.prefab
    
    Now there shouldn't be any changes to orginial assets.
    
    * Update Human.prefab
    
    * Update BlobShadowProjector.cs
    
    -Put in namespace 'Effects'
    -Turn Shadows on on destruction
    -Code style changes.
    -removed unused namespaces
    
    * Update BlobShadowProjector.cs
    
    Refactor method for turning dynamic shadows on and off.
    
    * Update BlobShadowProjector.cs
    
    Make Shadow toggling method more readable and performant.

---
+++ Assets/Scripts/Effects/BlobShadowProjector.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Effects
+{
+
+    public class BlobShadowProjector : MonoBehaviour
+    {
+        private Transform parent = null;
+
+        void Start()
+        {
+            parent = this.transform.parent;
+
+            SetDynamicShadowsOnParent(false);
+        }
+
+        private void OnDestroy()
+        {
+            SetDynamicShadowsOnParent(true);
+        }
+
+        private void SetDynamicShadowsOnParent(bool enabled)
+        {
+            var mode = enabled ? ShadowCastingMode.On : ShadowCastingMode.Off;
+            foreach (Transform child in parent)
+            {
+                var renderer = child.GetComponent<Renderer>();
+                if (renderer)
+                {
+                    renderer.shadowCastingMode = mode;
+                }
+            }
+        }
+    }
+}
