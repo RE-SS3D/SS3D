9e870fdb8c2dd49c5a20ede46651e6d16b138a88
Timothy Finucane <timfinucane@outlook.com>
2020-03-05 00:15:16 +1300

    Simplify network and state transitions

--- Assets/Scripts/Structure/DoorAnimator.cs
+++ Assets/Scripts/Structure/DoorAnimator.cs
@@ -1,177 +1,118 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Mirror;
 using UnityEngine;
 
 public class DoorAnimator : NetworkBehaviour
 {
-    private List<GameObject> insidePlayers = new List<GameObject>();
-    [SerializeField] private float openSpeed = 3f;
+    private const float DOOR_WAIT_CLOSE_TIME = 1.0f;
+    private const float OPEN_SPEED = 1.5f;
+
     [SerializeField] private Transform leftPanel;
     [SerializeField] private Transform rightPanel;
+
+    [SerializeField] private AudioSource openSfx;
+    [SerializeField] private AudioSource closeSfx;
+
+    [SerializeField] private int doorTriggerLayers = -1;
+
+    // TODO: Use real animations and remove the open/closed left/right
+    [SerializeField] private Vector3 openLeft;
+    [SerializeField] private Vector3 openRight;
 
     private Vector3 closedLeft;
     private Vector3 closedRight;
 
-    [SerializeField] private Vector3 openLeft;
-    [SerializeField] private Vector3 openRight;
-    private float lerpTime;
-    [SyncVar]
-    private bool isOpening;
+    // Server Only
+    [SyncVar(hook = "SetDoorState")]
+    private bool openState;
 
-    private bool isOpeningLocal;
-    [SyncVar]
-    private bool isClosing;
-
-    private bool isClosingLocal;
-    [SyncVar]
-    private bool isOpened;
-
-    private int playerLayer;
+    // Server Only
     private int playersInTrigger;
+    private Coroutine closeTimer;
 
-    [SerializeField] private AudioSource openSfx;
-    [SerializeField] private AudioSource closeSfx;
-    
-
-    void Awake()
+    public void Awake()
     {
+        if (doorTriggerLayers != -1)
+            doorTriggerLayers = LayerMask.NameToLayer("Player");
+
         closedLeft = leftPanel.localPosition;
         closedRight = rightPanel.localPosition;
-        playerLayer = LayerMask.NameToLayer("Player");
     }
 
     public override void OnStartClient()
     {
         base.OnStartClient();
-        SyncInitialState();
-    }
-
-    void SyncInitialState()
-    {
-        if (isServer) return;
-        if (isOpened)
-        {
-            leftPanel.localPosition = openLeft;
-            rightPanel.localPosition = openRight;
-        }
+        
+        if (isClientOnly)
+            SetDoorState(openState);
     }
     
     public void OnTriggerEnter(Collider other)
     {
-        
-        if (other.gameObject.layer != playerLayer || !isServer) return;
-        
-        if (!insidePlayers.Contains(other.gameObject))
+        if ((other.gameObject.layer & doorTriggerLayers) == 0 || !isServer) return;
+
+        if (playersInTrigger == 0)
         {
-            insidePlayers.Add(other.gameObject);
+            if (closeTimer != null) StopCoroutine(closeTimer);
+            openState = true;
         }
+
+        playersInTrigger += 1;
     }
 
     public void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.layer != playerLayer || !isServer) return;
-        
-        if (insidePlayers.Contains(other.gameObject))
+        if ((other.gameObject.layer & doorTriggerLayers) == 0 || !isServer) return;
+
+        if (playersInTrigger == 1)
         {
-            insidePlayers.Remove(other.gameObject);
+            // Start the close timer (which may be stopped).
+            closeTimer = StartCoroutine(RunCloseEventually());
+        }
+
+        playersInTrigger = Math.Max(playersInTrigger - 1, 0);
+    }
+
+    private void SetDoorState(bool open)
+    {
+        if (openState == open)
+            return;
+        openState = open;
+
+        if (isClient)
+        {
+            (open ? openSfx : closeSfx).Play();
+            StartCoroutine(RunDoorAnim(open));
         }
     }
 
-    void OpenDoor()
+    /*
+     * Runs the animation.
+     * TODO: Replace this with a real animation.
+     */
+    private IEnumerator RunDoorAnim(bool open)
     {
-        if (isClosingLocal || isOpeningLocal) return;
-        
-        lerpTime = 0f;
-        StartCoroutine(OpenDoorAnim());
-        openSfx.Play();
-    }
+        float animTime = 0.0f;
 
-    void CloseDoor()
-    {
-        if (isClosingLocal || isOpeningLocal) return;
-        
-        lerpTime = 0f;
-        StartCoroutine(CloseDoorAnim());
-        closeSfx.Play();
-    }
+        while (animTime < 1f) {
+            animTime += Time.deltaTime * OPEN_SPEED;
 
-    IEnumerator OpenDoorAnim()
-    {
-        if(isServer) isOpening = true;
-        isOpeningLocal = true;
-        while (lerpTime < 1f)
-        {
-            lerpTime += Time.deltaTime * (openSpeed * 2f);
+            float lerp = open ? animTime : 1.0f - animTime;
 
-            leftPanel.localPosition = Vector3.Lerp(closedLeft, openLeft, lerpTime);
-            rightPanel.localPosition = Vector3.Lerp(closedRight, openRight, lerpTime);
+            leftPanel.localPosition = Vector3.Lerp(closedLeft, openLeft, lerp);
+            rightPanel.localPosition = Vector3.Lerp(closedRight, openRight, lerp);
+
             yield return new WaitForEndOfFrame();
         }
-        
+
         yield return new WaitForSeconds(1f);
-
-        if(isServer) isOpening = false;
-        isOpeningLocal = false;
-        if(isServer) isOpened = true;
     }
 
-    IEnumerator CloseDoorAnim()
+    private IEnumerator RunCloseEventually()
     {
-        if(isServer)  isClosing = true;
-        isClosingLocal = true;
-        
-        while (lerpTime < 1f)
-        {
-            lerpTime += Time.deltaTime * openSpeed;
-
-            leftPanel.localPosition = Vector3.Lerp(openLeft, closedLeft, lerpTime);
-            rightPanel.localPosition = Vector3.Lerp(openRight, closedRight, lerpTime);
-            yield return new WaitForEndOfFrame();
-        }
-        
-        yield return new WaitForSeconds(1f);
-        if(isServer) isClosing = false;
-        isClosingLocal = false;
-        if(isServer) isOpened = false;
-    }
-
-    void Update()
-    {
-        if (isServer)
-        {
-            ServerMonitorDoor();
-        }
-        else
-        {
-            ClientMonitorDoor();
-        }
-    }
-
-    void ServerMonitorDoor()
-    {
-        if (insidePlayers.Count > 0 && !isOpening && !isOpened)
-        {
-            OpenDoor();
-        }
-
-        if (insidePlayers.Count == 0 && isOpened)
-        {
-            CloseDoor();
-            isOpened = false;
-        }
-    }
-
-    void ClientMonitorDoor()
-    {
-        if (!isOpeningLocal && isOpening)
-        {
-            OpenDoor();
-        }
-
-        if (!isClosingLocal && isClosing)
-        {
-            CloseDoor();
-        }
+        yield return new WaitForSeconds(DOOR_WAIT_CLOSE_TIME);
+        openState = false;
     }
 }
