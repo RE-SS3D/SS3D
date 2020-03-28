ebc8cfc819431946a048560492036bd391bcf854
cosmiccoincidence <cosmiccoincidence1991@gmail.com>
2020-03-28 17:15:39 -0400

    Fixes X variant script bugs
    Also adds tile setup in the tile editor

--- Assets/Engine/Tile/Connections/AdvancedAdjacencyConnector.cs
+++ Assets/Engine/Tile/Connections/AdvancedAdjacencyConnector.cs
@@ -156,8 +156,8 @@ namespace SS3D.Engine.Tiles.Connections
                         mesh = xQuad;
                         break;
                     case 1:
-                        mesh = xSingle;
-                        rotation = DirectionHelper.AngleBetween(Direction.East, diagonals.GetOnlyNegative());
+                        mesh = xTriple;
+                        rotation = DirectionHelper.AngleBetween(Direction.West, diagonals.GetOnlyNegative());
                         break;
                     case 2:
                         if(diagonals.north == diagonals.south) {
@@ -166,11 +166,11 @@ namespace SS3D.Engine.Tiles.Connections
                         }
                         else {
                             mesh = xSide;
-                            rotation = DirectionHelper.AngleBetween(Direction.SouthEast, diagonals.GetCornerDirection());
+                            rotation = DirectionHelper.AngleBetween(Direction.SouthWest, diagonals.GetCornerDirection());
                         }
                         break;
                     case 3:
-                        mesh = xTriple;
+                        mesh = xSingle;
                         rotation = DirectionHelper.AngleBetween(Direction.East, diagonals.GetOnlyNegative());
                         break;
                     default:
