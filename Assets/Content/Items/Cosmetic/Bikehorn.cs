68e1df4dbbfb8955170db4433f6d1f7d5e2df689
LightVelox <veloxgods@gmail.com>
2020-05-14 14:05:09 -0300

    Honk
    Adds the honk

---
+++ Assets/Content/Items/Cosmetic/Bikehorn.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using Mirror;
+using SS3D.Engine.Interactions;
+
+namespace SS3D.Content.Items.Functional.Generic
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class Bikehorn : NetworkBehaviour, Interaction
+    {
+        [SerializeField] private AudioClip honkSound = null;
+        private AudioSource audioSource;
+
+        public InteractionEvent Event { get; set; }
+        public string Name => "Honk";
+
+        public bool CanInteract()
+        {
+            return Event.tool == gameObject;
+        }
+
+        public void Start()
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        public void Interact()
+        {
+            if (!audioSource.isPlaying) {
+                audioSource.PlayOneShot(honkSound);
+                RpcPlayHonk();
+            }
+        }
+
+        [ClientRpc]
+        private void RpcPlayHonk()
+        {
+            audioSource.PlayOneShot(honkSound);
+        }
+    }
+}
