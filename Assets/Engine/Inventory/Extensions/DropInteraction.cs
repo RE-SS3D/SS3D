e3764ed5a042e094c39e50fe42ce3d03e8cfa4c7
Alain Siegrist <alainsiegrist02@gmail.com>
2020-05-17 14:22:12 +0200

    Add drop/pickup interaction

---
+++ Assets/Engine/Inventory/Extensions/DropInteraction.cs
@@ -0,0 +1,42 @@
+﻿using SS3D.Engine.Interactions;
+
+namespace SS3D.Engine.Inventory.Extensions
+{
+    public class DropInteraction : IInteraction
+    {
+        public IClientInteraction CreateClient(InteractionEvent interactionEvent)
+        {
+            return null;
+        }
+
+        public string GetName(InteractionEvent interactionEvent)
+        {
+            return "Drop";
+        }
+
+        public bool CanInteract(InteractionEvent interactionEvent)
+        {
+            return interactionEvent.Source.Parent is Hands;
+        }
+
+        public bool Start(InteractionEvent interactionEvent, InteractionReference reference)
+        {
+            if (interactionEvent.Source.Parent is Hands hands)
+            {
+                hands.PlaceHeldItem(interactionEvent.Point);
+            }
+            
+            return false;
+        }
+
+        public bool Update(InteractionEvent interactionEvent, InteractionReference reference)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Cancel(InteractionEvent interactionEvent, InteractionReference reference)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Engine/Inventory/Extensions/PickupInteraction.cs
@@ -0,0 +1,61 @@
+﻿using SS3D.Engine.Interactions;
+using UnityEngine;
+
+namespace SS3D.Engine.Inventory.Extensions
+{
+    public class PickupInteraction : IInteraction
+    {
+        public float PickupDistance { get; set; } = 1.5f;
+
+        public IClientInteraction CreateClient(InteractionEvent interactionEvent)
+        {
+            return null;
+        }
+
+        public string GetName(InteractionEvent interactionEvent)
+        {
+            return "Pick up";
+        }
+
+        public bool CanInteract(InteractionEvent interactionEvent)
+        {
+            if (interactionEvent.Target is IGameObjectProvider targetBehaviour)
+            {
+                if (targetBehaviour.GameObject.GetComponent<Item>() == null)
+                {
+                    return false;
+                }
+
+                if (interactionEvent.Source is IGameObjectProvider sourceBehaviour &&
+                    Vector3.Distance(sourceBehaviour.GameObject.transform.position, targetBehaviour.GameObject.transform.position) >
+                    PickupDistance)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool Start(InteractionEvent interactionEvent, InteractionReference reference)
+        {
+            if (interactionEvent.Source is Hands hands && interactionEvent.Target is IGameObjectProvider target)
+            {
+                hands.Pickup(target.GameObject);
+            }
+            return false;
+        }
+
+        public bool Update(InteractionEvent interactionEvent, InteractionReference reference)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Cancel(InteractionEvent interactionEvent, InteractionReference reference)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
