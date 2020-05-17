5519f7835d5b69d503dce3cc9254a43b58b48221
Alain Siegrist <alainsiegrist02@gmail.com>
2020-05-17 14:30:23 +0200

    Add storage containers

---
+++ Assets/Content/Furniture/Openable.cs
@@ -0,0 +1,42 @@
+﻿using Mirror;
+using SS3D.Content.Systems.Interactions;
+using SS3D.Engine.Interactions;
+using UnityEngine;
+
+namespace SS3D.Content.Furniture
+{
+    [RequireComponent(typeof(Animator))]
+    public class Openable : InteractionTargetNetworkBehaviour
+    {
+        private Animator animator;
+        private static readonly int Open = Animator.StringToHash("Open");
+
+        public override IInteraction[] GenerateInteractions(InteractionEvent interactionEvent)
+        {
+            OpenInteraction openInteraction = new OpenInteraction();
+            openInteraction.OpenStateChange += OnOpenStateChange;
+            return new IInteraction[] { openInteraction };
+        }
+        
+        public bool IsOpen()
+        {
+            return animator.GetBool(Open);
+        }
+
+        private void OnOpenStateChange(object sender, bool e)
+        {
+            RpcSetOpenState(e);
+        }
+
+        protected virtual void Start()
+        {
+            animator = GetComponent<Animator>();
+        }
+        
+        [ClientRpc]
+        private void RpcSetOpenState(bool open)
+        {
+            animator.SetBool(Open, open);
+        }
+    }
+}
