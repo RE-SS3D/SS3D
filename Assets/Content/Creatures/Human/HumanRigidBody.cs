8654bfb47f263260efed38538b298b05bb7625e3
Alain Siegrist <alainsiegrist02@gmail.com>
2020-05-18 02:39:31 +0200

    Create player ragdoll

---
+++ Assets/Content/Creatures/Human/HumanRigidBody.cs
@@ -0,0 +1,64 @@
+﻿using Mirror;
+using SS3D.Content.Systems.Player;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace SS3D.Content.Creatures.Human
+{
+    [RequireComponent(typeof(Animator))]
+    public class HumanRigidBody : NetworkBehaviour
+    {
+        public Transform ArmatureRoot;
+        public bool BodyEnabled
+        {
+            get => bodyEnabled;
+            set => SetEnabled(value);
+        }
+
+        private bool bodyEnabled = false;
+    
+        void Start()
+        {
+            Assert.IsNotNull(ArmatureRoot);
+            SetEnabled(BodyEnabled);
+        }
+
+        public void SetEnabled(bool enabled)
+        {
+            bodyEnabled = enabled;
+            
+            foreach (Rigidbody body in ArmatureRoot.GetComponentsInChildren<Rigidbody>())
+            {
+                body.isKinematic = !enabled;
+            }
+
+            GetComponent<Animator>().enabled = !enabled;
+            
+            CharacterController characterController = GetComponent<CharacterController>();
+            if (characterController != null)
+            {
+                characterController.enabled = !enabled;
+            }
+            
+            MovementController movementController = GetComponent<MovementController>();
+            if (movementController != null)
+            {
+                movementController.enabled = !enabled;
+            }
+
+            if (isServer)
+            {
+                RpcSetEnabled(enabled);
+            }
+        }
+
+        [ClientRpc]
+        private void RpcSetEnabled(bool enabled)
+        {
+            if (!isServer)
+            {
+                SetEnabled(enabled);
+            }
+        }
+    }
+}
