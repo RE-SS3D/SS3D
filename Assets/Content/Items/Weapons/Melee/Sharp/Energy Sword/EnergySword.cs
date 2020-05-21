a3a87bd9b645b5f3f8c9b251bd56873af2a22981
João Buratto <46575243+joaoburatto@users.noreply.github.com>
2020-05-20 21:58:31 -0300

    Rotation fixes to holdable items (#306)
    * Item rotation and holding offsets
    
    This changes the rotation of every item to be standing up on the FBX.
    With this I also updated the prefabs and their holding offsets.
    
    * More Item fixing
    
    All items changed have Seteron's standard for rotation.
    Added a simple on/off for the Energy Sword aswell as reworked it's model and added a blue variant.
    Changed the glock to the enforcer model.
    
    * More model fixes
    
    Model rotation and mesh fixing.
    Material for glass and emission.
    Changes on TileMapEditor to have name and icon, aswell as appearing in an SS3D tab, visual help to see where the turf position will be.
    
    * Mising metal materials fix
    
    * Update welder.prefab
    
    * More holding fixes

---
+++ Assets/Content/Items/Weapons/Melee/Sharp/Energy Sword/EnergySword.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using Mirror;
+using SS3D.Engine.Interactions;
+using System.Collections;
+
+public class EnergySword : NetworkBehaviour, Interaction
+{
+    [SerializeField]
+    private Animator animator;
+    private bool on;
+
+    public InteractionEvent Event { get; set; }
+    public string Name => "Turn On/Off";
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool CanInteract()
+    {
+        return Event.tool == gameObject;
+    }
+
+    public void Interact()
+    {
+        on = !on;
+        animator.SetBool("On", on);
+
+        RpcSetBlade(on);
+    }
+
+    [ClientRpc]
+    private void RpcSetBlade(bool on)
+    {
+        animator.SetBool("On", on);
+    }
+}
