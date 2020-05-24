51357bd169487b36c02a17780d9b47852c8152e1
João Buratto <joaoburattoc@gmail.com>
2020-05-24 14:12:40 -0300

    Update Fork

--- Assets/Content/Items/Weapons/Melee/Sharp/Energy Sword/EnergySword.cs
+++ Assets/Content/Items/Weapons/Melee/Sharp/Energy Sword/EnergySword.cs
@@ -2,9 +2,16 @@
 using Mirror;
 using SS3D.Engine.Interactions;
 using System.Collections;
+<<<<<<< HEAD
 using System.Media;
+=======
+using System.Collections.Generic;
+using System.Linq;
+using SS3D.Content.Systems.Interactions;
+using SS3D.Engine.Inventory;
+>>>>>>> upstream/master
 
-public class EnergySword : NetworkBehaviour, Interaction
+public class EnergySword : Item, IToggleable
 {
     [SerializeField]
     private Animator animator;
@@ -20,39 +27,41 @@ public class EnergySword : NetworkBehaviour, Interaction
     private AudioClip soundOff;
 
     private bool on;
-
-    public InteractionEvent Event { get; set; }
-    public string Name => "Turn On/Off";
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public bool CanInteract()
-    {
-        return Event.tool == gameObject;
-    }
-
-    public void Interact()
-    {
-        if (audio.isPlaying && animator.IsInTransition(0))
-            return;
-
-        on = !on;
-
-        animator.SetBool("On", on);
-
-        audio.clip = on ? soundOn : soundOff;
-        audio.Play();
-
-        RpcSetBlade(on);
-    }
+    private static readonly int OnHash = Animator.StringToHash("On");
 
     [ClientRpc]
     private void RpcSetBlade(bool on)
     {
+        animator.SetBool(OnHash, on);
+    }
+
+    public bool GetState()
+    {
+        return on;
+    }
+
+    public void Toggle()
+    {
+        if (audio.isPlaying && animator.IsInTransition(0))
+            return;
+
+        on = !on;
+<<<<<<< HEAD
+
+        animator.SetBool("On", on);
+
+        audio.clip = on ? soundOn : soundOff;
+        audio.Play();
+
+=======
+        animator.SetBool(OnHash, on);
+>>>>>>> upstream/master
+        RpcSetBlade(on);
+    }
+
+    public override IInteraction[] GenerateInteractions(IInteractionTarget[] targets)
+    {
+<<<<<<< HEAD
         if (audio.isPlaying && animator.IsInTransition(0))
             return;
 
@@ -61,5 +70,13 @@ public class EnergySword : NetworkBehaviour, Interaction
 
         audio.clip = on ? soundOn : soundOff;
         audio.Play();
+=======
+        List<IInteraction> interactions = base.GenerateInteractions(targets).ToList();
+        interactions.Insert(0, new ToggleInteraction
+        {
+            OnName = "Turn off", OffName = "Turn on"
+        });
+        return interactions.ToArray();
+>>>>>>> upstream/master
     }
 }
