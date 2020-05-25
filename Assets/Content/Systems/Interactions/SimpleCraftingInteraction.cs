becf8365492524f1f3fce2f1df1b3a8a27b79307
Alain Siegrist <alainsiegrist02@gmail.com>
2020-05-25 20:42:00 +0200

    Add simple crafting

---
+++ Assets/Content/Systems/Interactions/SimpleCraftingInteraction.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using SS3D.Content.Systems.Interactions;
+using SS3D.Engine.Interactions;
+using SS3D.Engine.Interactions.Extensions;
+using SS3D.Engine.Inventory;
+using UnityEngine;
+
+public class SimpleCraftingInteraction : IInteraction
+{
+    /// <summary>
+    /// The id of the necessary item
+    /// </summary>
+    public string ItemId { get; set; }
+
+    /// <summary>
+    /// Should the source be consumed
+    /// </summary>
+    public bool Consume { get; set; }
+
+    /// <summary>
+    /// The objects which are created
+    /// </summary>
+    public CraftingResult[] ResultingObjects { get; set; }
+
+    /// <summary>
+    /// The name shown in the context menu
+    /// </summary>
+    public string Name { get; set; }
+
+
+    public IClientInteraction CreateClient(InteractionEvent interactionEvent)
+    {
+        return null;
+    }
+
+    public string GetName(InteractionEvent interactionEvent)
+    {
+        return Name;
+    }
+
+    public bool CanInteract(InteractionEvent interactionEvent)
+    {
+        Item item = interactionEvent.Source as Item;
+        if (item == null || item.ItemId != ItemId)
+        {
+            return false;
+        }
+
+        if (!InteractionExtensions.RangeCheck(interactionEvent))
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public bool Start(InteractionEvent interactionEvent, InteractionReference reference)
+    {
+        Item item = (Item) interactionEvent.Source;
+        Item targetItem = interactionEvent.Target.GetComponent<Item>();
+        if (Consume)
+        {
+            ItemHelpers.DestroyItem(item);
+        }
+
+        GameObject targetGameObject = targetItem.gameObject;
+
+
+        for (int index = 0; index < ResultingObjects.Length; index++)
+        {
+            CraftingResult result = ResultingObjects[index];
+            Item created = ItemHelpers.CreateItem(result.Prefab);
+            GameObject gameObject = created.gameObject;
+            gameObject.transform.position = targetGameObject.transform.position + targetGameObject.transform.rotation * result.Position;
+            gameObject.transform.eulerAngles = targetGameObject.transform.eulerAngles + result.Rotation;
+            
+            if (index == 0 && ResultingObjects.Length == 1)
+            {
+                ItemHelpers.ReplaceItem(targetItem, created);
+            }
+        }
+
+        if (ResultingObjects.Length > 1)
+        {
+            ItemHelpers.DestroyItem(targetItem);
+        }
+
+
+        return false;
+    }
+
+    public bool Update(InteractionEvent interactionEvent, InteractionReference reference)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Cancel(InteractionEvent interactionEvent, InteractionReference reference)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    [Serializable]
+    public struct CraftingResult
+    {
+        public GameObject Prefab;
+        public Vector3 Position;
+        public Vector3 Rotation;
+    }
+}
---
+++ Assets/Content/Systems/Interactions/SimpleCraftingTarget.cs
@@ -0,0 +1,33 @@
+﻿using SS3D.Engine.Interactions;
+using UnityEngine;
+
+namespace SS3D.Content.Systems.Interactions
+{
+    public class SimpleCraftingTarget : InteractionTargetBehaviour
+    {
+        /// <summary>
+        /// The name shown in the context menu
+        /// </summary>
+        public string Name;
+        /// <summary>
+        /// The id of the necessary item
+        /// </summary>
+        public string ItemId;
+        /// <summary>
+        /// Should the source be consumed
+        /// </summary>
+        public bool Consume;
+        /// <summary>
+        /// The objects which are created
+        /// </summary>
+        public SimpleCraftingInteraction.CraftingResult[] ResultingObjects;
+
+        public override IInteraction[] GenerateInteractions(InteractionEvent interactionEvent)
+        {
+            return new IInteraction[] { new SimpleCraftingInteraction
+            {
+                Name = Name, ItemId = ItemId, Consume = Consume, ResultingObjects = ResultingObjects
+            } };
+        }
+    }
+}
