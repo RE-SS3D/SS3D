e07ae3ac0481a9a94afc6344d5fef9bedb8114d0
Alain Siegrist <alainsiegrist02@gmail.com>
2020-05-25 02:15:43 +0200

    Bad banana (#292)
    * Create player ragdoll
    
    * Make banana peel slippery
    
    * Enable item - player interactions
    
    * Move banana peel to floor
    
    * Revert "Move banana peel to floor"
    
    This reverts commit b99b8f43c0137b53c55d0b61d41b60512302361d.
    
    * Update rigidbody
    
    * Make soap slippery
    
    * Keep walking velocity when rigidbody is activated
    
    * Add human head collider

---
+++ Assets/Content/Creatures/Human/RigidBodyInspector.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace SS3D.Content.Creatures.Human
+{
+    #if UNITY_EDITOR
+    [CustomEditor(typeof(HumanRigidBody))]
+    public class RigidBodyInspector : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            HumanRigidBody rigidBody = (HumanRigidBody) target;
+            DrawDefaultInspector();
+            if (GUILayout.Button(rigidBody.BodyEnabled ? "Disable body" : "Enable body"))
+            {
+                rigidBody.BodyEnabled = !rigidBody.BodyEnabled;
+            }
+        }
+    }
+    #endif
+}
---
+++ Assets/Content/Items/Slippery.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Mirror;
+using SS3D.Content.Creatures.Human;
+using UnityEngine;
+
+namespace SS3D.Content.Items
+{
+    public class Slippery : NetworkBehaviour
+    {
+        private Dictionary<HumanRigidBody, float> slippedBodies;
+
+        private void Start()
+        {
+            slippedBodies = new Dictionary<HumanRigidBody, float>();
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (!isServer)
+            {
+                return;
+            }
+            
+            HumanRigidBody humanRigidBody = other.transform.root.gameObject.GetComponent<HumanRigidBody>();
+            if (humanRigidBody != null && !humanRigidBody.BodyEnabled)
+            {
+                CleanBodyDictionary();
+                if (!slippedBodies.ContainsKey(humanRigidBody))
+                {
+                    humanRigidBody.BodyEnabled = true;
+                    slippedBodies.Add(humanRigidBody, Time.time);
+                    StartCoroutine(DisableBodyCoroutine(humanRigidBody));
+                }
+            }
+            
+        }
+
+        private void CleanBodyDictionary()
+        {
+            var keyValuePairs = slippedBodies.Where(x => x.Value + 4f < Time.time).ToList();
+            foreach (var pair in keyValuePairs)
+            {
+                slippedBodies.Remove(pair.Key);
+            }
+        }
+
+        private IEnumerator DisableBodyCoroutine(HumanRigidBody body)
+        {
+            yield return new WaitForSeconds(3);
+            body.BodyEnabled = false;
+        }
+    }
+}
