b52ee004597b3b20139b85a716c5285910dd0e8f
LightVelox <veloxgods@gmail.com>
2020-05-26 20:10:01 -0300

    Adds interactions for Boombox and Service Bell and changes the Audio Listener to player (#339)
    * Adds functionality to the Boombox and sounds to the Microwave
    
    The boombox can now play 3 different sounds, i also found a "hacky" way to go around the Interaction System's limitations and play music even after the Item has been disabled(PickUp disables the game object), It has 3 musics: Title1,2 and 3.
    The microwave now has sounds to show when it's "microwaving" and when it's done, also added Drinking and Eating sounds for future eating interactions
    
    * Fixes microwave sound way too low
    
    read the fucking title
    
    * Restarts music when changing clips
    
    Just so you can hear the musics from the beginning, also to maybe prevent musics that are larger than the others to bug the source
    
    * Removes the Audio Listener from the Camera
    
    and puts it on the Player, we'll need to make an issue for fixing 3D Audio for headsets tho
    
    * Adds the Service Bell sound
    
    * Fixes merge problem i think
    
    * Reduces music volume
    
    * Adjusts volumes of sounds
    
    - Re adds the missing microwaves files too
    
    * Improved audio falloff for radio
    
    * Organized Audio Files
    
    * Update noise settings on prefabs
    
    Co-authored-by: CosmicCoincidence <cosmiccoincidence1991@gmail.com>

---
+++ Assets/Content/Items/Cosmetic/ServiceBell.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Mirror;
+using SS3D.Content.Systems.Interactions;
+using SS3D.Engine.Interactions;
+using SS3D.Engine.Interactions.Extensions;
+using SS3D.Engine.Inventory;
+using UnityEngine;
+
+namespace SS3D.Content.Items.Cosmetic
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class ServiceBell : Item, IInteractionTarget
+    {
+        private class BellInteraction : IInteraction
+        {
+            public IClientInteraction CreateClient(InteractionEvent interactionEvent)
+            {
+                return null;
+            }
+
+            public string GetName(InteractionEvent interactionEvent)
+            {
+                return "Bell";
+            }
+
+            public bool CanInteract(InteractionEvent interactionEvent)
+            {
+                if (interactionEvent.Target is ServiceBell bell)
+                {
+                    if (!InteractionExtensions.RangeCheck(interactionEvent))
+                    {
+                        return false;
+                    }
+                    return true;
+                }
+                if (interactionEvent.Source is ServiceBell bell1)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+
+            public bool Start(InteractionEvent interactionEvent, InteractionReference reference)
+            {
+                if (interactionEvent.Target is ServiceBell bell)
+                {
+                    bell.Bell();
+                }
+                if (interactionEvent.Source is ServiceBell bell1)
+                {
+                    bell1.Bell();
+                }
+                return false;
+            }
+
+            public bool Update(InteractionEvent interactionEvent, InteractionReference reference)
+            {
+                throw new System.NotImplementedException();
+            }
+
+            public void Cancel(InteractionEvent interactionEvent, InteractionReference reference)
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+        
+        [SerializeField] private AudioClip bellSound = null;
+        private AudioSource audioSource;
+
+        public void Start()
+        {
+            audioSource = GetComponent<AudioSource>();
+            GenerateNewIcon(); 
+        }
+
+        [Server]
+        private void Bell()
+        {
+            audioSource.PlayOneShot(bellSound);
+            RpcPlayBell();
+        }
+
+        [ClientRpc]
+        private void RpcPlayBell()
+        {
+            audioSource.PlayOneShot(bellSound);
+        }
+
+        public override IInteraction[] GenerateInteractions(IInteractionTarget[] targets)
+        {
+            List<IInteraction> interactions = base.GenerateInteractions(targets).ToList();
+            interactions.Insert(0, new BellInteraction());
+            return interactions.ToArray();
+        }
+
+        public IInteraction[] GenerateInteractions(InteractionEvent interactionEvent)
+        {
+            return new IInteraction[]{new BellInteraction()};
+        }
+    }
+}
