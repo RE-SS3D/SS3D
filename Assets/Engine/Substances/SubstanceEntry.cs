eb937b2a6fc345090891b0abd3059274f982ae65
Alain Siegrist <alainsiegrist02@gmail.com>
2020-05-30 03:15:32 +0200

    Add substance system

---
+++ Assets/Engine/Substances/SubstanceEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SS3D.Engine.Substances
+{
+    [Serializable]
+    public struct SubstanceEntry
+    {
+        public Substance Substance;
+        public float Moles;
+
+        public SubstanceEntry(Substance substance, float moles)
+        {
+            Substance = substance;
+            Moles = moles;
+        }
+    }
+}
