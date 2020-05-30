2e4140a9a4103c9a195d4d5d4b6b3e530d480eed
Alain Siegrist <alainsiegrist02@gmail.com>
2020-05-30 14:30:23 +0200

    Sync container look

--- Assets/Content/Systems/Substances/SubstanceDisplay.cs
+++ Assets/Content/Systems/Substances/SubstanceDisplay.cs
@@ -1,15 +1,19 @@
-﻿using SS3D.Engine.Substances;
+﻿using Mirror;
+using SS3D.Engine.Substances;
 using UnityEngine;
 
 namespace SS3D.Content.Systems.Substances
 {
-    [RequireComponent(typeof(MeshRenderer))]
-    public class SubstanceDisplay : MonoBehaviour
+    public class SubstanceDisplay : NetworkBehaviour
     {
         /// <summary>
         /// The container to display
         /// </summary>
         public SubstanceContainer Container;
+        /// <summary>
+        /// The object displaying the fluid level
+        /// </summary>
+        public GameObject DisplayObject;
         /// <summary>
         /// The position of fill when empty
         /// </summary>
@@ -25,17 +29,23 @@ namespace SS3D.Content.Systems.Substances
 
         private void Start()
         {
-            meshRenderer = GetComponent<MeshRenderer>();
-            Container.ContentsChanged += container => UpdateDisplay();
-            UpdateDisplay();
+            meshRenderer = DisplayObject.GetComponent<MeshRenderer>();
+            if (isServer)
+            {
+                Container.ContentsChanged += container => UpdateDisplay();
+                UpdateDisplay();
+            }
         }
 
+        [Server]
         private void UpdateDisplay()
         {
             float relativeVolume = Container.CurrentVolume / Container.Volume;
-            transform.localPosition = Vector3.Lerp(EmptyPosition, FullPosition, Mathf.Min(relativeVolume, 1));
-            transform.localScale = new Vector3(ScaleX.Evaluate(relativeVolume), ScaleY.Evaluate(relativeVolume), ScaleZ.Evaluate(relativeVolume));
-            meshRenderer.material.color = CalculateColor();
+            Transform trans = DisplayObject.transform;
+            trans.localPosition = Vector3.Lerp(EmptyPosition, FullPosition, Mathf.Min(relativeVolume, 1));
+            trans.localScale = new Vector3(ScaleX.Evaluate(relativeVolume), ScaleY.Evaluate(relativeVolume), ScaleZ.Evaluate(relativeVolume));
+            Color color = meshRenderer.material.color = CalculateColor();
+            RpcUpdateDisplay(trans.localPosition, trans.localScale, color);
         }
 
         private Color CalculateColor()
@@ -49,6 +59,15 @@ namespace SS3D.Content.Systems.Substances
             }
 
             return color;
+        }
+
+        [ClientRpc]
+        private void RpcUpdateDisplay(Vector3 position, Vector3 scale, Color color)
+        {
+            Transform trans = DisplayObject.transform;
+            trans.localPosition = position;
+            trans.localScale = scale;
+            meshRenderer.material.color = color;
         }
     }
 }
