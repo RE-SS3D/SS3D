b7e0fcfb354b774447c844c0756c8665af5d8eee
BroodjeKipcorn <rutger_dezweef@hotmail.com>
2020-06-05 19:21:24 +0200

    Add valves

---
+++ Assets/Engine/Atmospherics/ValveObject.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SS3D.Engine.Atmospherics
+{
+    public class ValveObject : MonoBehaviour
+    {
+        public enum ValveType
+        {
+            Manual,
+            Digital
+        }
+
+        public ValveType valveType;
+        private PipeObject pipe;
+        public bool isEnabled;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            pipe = GetComponent<PipeObject>();
+            if (isEnabled)
+            {
+                pipe.SetBlocked(true);
+            }
+            else
+            {
+                pipe.SetBlocked(false);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (isEnabled)
+            {
+                pipe.SetBlocked(true);
+            }
+            else
+            {
+                pipe.SetBlocked(false);
+            }
+        }
+    }
+}
