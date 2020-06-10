f699cca0aa600f251e1557e490799471b29403e1
João Buratto <46575243+joaoburatto@users.noreply.github.com>
2020-06-10 19:19:35 -0300

    Networked prefabs (#389)
    * Network transoform on prefabs
    
    this adds the network transform into the prefabs
    
    * Cryotube model fix
    
    * Basic liquid shader
    
    * Liquid and Glass shader changes
    
    * Model and prefab fixes
    
    * Liquid and locker animation fixes
    
    * Canister resize and hold position on beakers
    
    * Fix canister shadows and bounds
    
    Co-authored-by: CosmicCoincidence <cosmiccoincidence1991@gmail.com>

--- Assets/Content/Systems/Substances/SubstanceDisplay.cs
+++ Assets/Content/Systems/Substances/SubstanceDisplay.cs
@@ -27,6 +27,21 @@ namespace SS3D.Content.Systems.Substances
         public AnimationCurve ScaleZ;
         private MeshRenderer meshRenderer;
 
+        // wobble shader stuff
+        Vector3 lastPos;
+        Vector3 velocity;
+        Vector3 lastRot;
+        Vector3 angularVelocity;
+        public float MaxWobble = 0.03f;
+        public float WobbleSpeed = 1f;
+        public float Recovery = 1f;
+        float wobbleAmountX;
+        float wobbleAmountZ;
+        float wobbleAmountToAddX;
+        float wobbleAmountToAddZ;
+        float pulse;
+        float time = 0.5f;
+
         private void Start()
         {
             meshRenderer = DisplayObject.GetComponent<MeshRenderer>();
@@ -37,15 +52,51 @@ namespace SS3D.Content.Systems.Substances
             }
         }
 
+        private void Update()
+        {
+            time += Time.deltaTime;
+            // decrease wobble over time
+            wobbleAmountToAddX = Mathf.Lerp(wobbleAmountToAddX, 0, Time.deltaTime * (Recovery));
+            wobbleAmountToAddZ = Mathf.Lerp(wobbleAmountToAddZ, 0, Time.deltaTime * (Recovery));
+
+            // make a sine wave of the decreasing wobble
+            pulse = 2 * Mathf.PI * WobbleSpeed;
+            wobbleAmountX = wobbleAmountToAddX * Mathf.Sin(pulse * time);
+            wobbleAmountZ = wobbleAmountToAddZ * Mathf.Sin(pulse * time);
+
+            // send it to the shader
+            meshRenderer.material.SetFloat("_WobbleX", wobbleAmountX);
+            meshRenderer.material.SetFloat("_WobbleZ", wobbleAmountZ);
+
+            // velocity
+            velocity = (lastPos - transform.position) / Time.deltaTime;
+            angularVelocity = transform.rotation.eulerAngles - lastRot;
+
+
+            // add clamped velocity to wobble
+            wobbleAmountToAddX += Mathf.Clamp((velocity.x + (angularVelocity.z * 0.2f)) * MaxWobble, -MaxWobble, MaxWobble);
+            wobbleAmountToAddZ += Mathf.Clamp((velocity.z + (angularVelocity.x * 0.2f)) * MaxWobble, -MaxWobble, MaxWobble);
+
+            // keep last position
+            lastPos = transform.position;
+            lastRot = transform.rotation.eulerAngles;
+        }
+
         [Server]
         private void UpdateDisplay()
         {
-            float relativeVolume = Container.CurrentVolume / Container.Volume;
+            float relativeVolume = (Container.CurrentVolume / Container.Volume);
             Transform trans = DisplayObject.transform;
-            trans.localPosition = Vector3.Lerp(EmptyPosition, FullPosition, Mathf.Min(relativeVolume, 1));
-            trans.localScale = new Vector3(ScaleX.Evaluate(relativeVolume), ScaleY.Evaluate(relativeVolume), ScaleZ.Evaluate(relativeVolume));
-            Color color = meshRenderer.material.color = CalculateColor();
-            RpcUpdateDisplay(trans.localPosition, trans.localScale, color);
+
+            Color newColor = CalculateColor();
+
+            meshRenderer.material.SetFloat("_FillAmount", relativeVolume);
+            meshRenderer.material.SetColor("_Tint", newColor);
+            meshRenderer.material.SetColor("_TopColor", newColor);
+
+            //trans.localPosition = Vector3.Lerp(EmptyPosition, FullPosition, Mathf.Min(relativeVolume, 1));
+            //trans.localScale = new Vector3(ScaleX.Evaluate(relativeVolume), ScaleY.Evaluate(relativeVolume), ScaleZ.Evaluate(relativeVolume));
+            RpcUpdateDisplay(trans.localPosition, trans.localScale, newColor);
         }
 
         private Color CalculateColor()
@@ -58,6 +109,7 @@ namespace SS3D.Content.Systems.Substances
                 color += entry.Substance.Color * relativeMoles;
             }
 
+            color.a = 0.5f;
             return color;
         }
 
