5dd01f6229e30c5eb085ecf2b514f360714db810
LightVelox <veloxgods@gmail.com>
2020-06-13 01:59:29 -0300

    Adds power cells and cell charger (#394)
    * Adds Power Cells
    
    * Revert "Adds Power Cells"
    
    This reverts commit d4305522f5467082b61d15a178a49ddafcd4558d.
    
    * Revert "Revert "Adds Power Cells""
    
    This reverts commit 2328f140fbda9c083397721eb86d63f28391c43b.
    
    * Does all suggested changes and now can only store Power Cells
    
    * Fixes everything
    
    * Fix cell charger collider
    
    * fix main scene conflict
    
    Co-authored-by: CosmicCoincidence <cosmiccoincidence1991@gmail.com>

---
+++ Assets/Engine/Inventory/IChargeable.cs
@@ -0,0 +1,9 @@
+﻿namespace SS3D.Engine.Inventory
+{
+    public interface IChargeable
+    {
+        void AddCharge(int amount);
+        int GetChargeRate();
+        float GetPowerPercentage();
+    }
+}
