b433661ed307c62c02d556158998a62502ed59d1
CosmicCoincidence <cosmiccoincidence1991@gmail.com>
2020-06-15 13:12:23 -0400

    New adjacency connectors and file renames

---
+++ Assets/Engine/Tile/Connections/PipesAdjacencyConnector.cs
@@ -0,0 +1,127 @@
+﻿using UnityEngine;
+using System.Collections;
+using SS3D.Engine.Tiles.Connections;
+using System;
+
+namespace SS3D.Engine.Tiles.Connections
+{
+    /**
+     * The pipes adjacency connector is...
+     */
+    [RequireComponent(typeof(MeshFilter))]
+    public class PipesAdjacencyConnector : MonoBehaviour, AdjacencyConnector
+    {
+        public enum TileLayer
+        {
+            Turf,
+            Fixture,
+        }
+
+        public FixtureLayers Layer { get; set; }
+
+        // Id that adjacent objects must be to count. If null, any id is accepted
+        public string type;
+
+
+        [Header("Meshes")]
+        [Tooltip("A mesh where no edges are connected")]
+        public Mesh o;
+        [Tooltip("A mesh where the east edge is connected")]
+        public Mesh c;
+        [Tooltip("A mesh where east and west edges are connected")]
+        public Mesh i;
+        [Tooltip("A mesh where the south and west edges are connected")]
+        public Mesh l;
+        [Tooltip("A mesh where the north, south, and east edge is connected")]
+        public Mesh t;
+        [Tooltip("A mesh where all edges are connected")]
+        public Mesh x;
+
+        /**
+         * When a single adjacent turf is updated
+         */
+        public void UpdateSingle(Direction direction, TileDefinition tile)
+        {
+            if (UpdateSingleConnection(direction, tile))
+                UpdateMeshAndDirection();
+        }
+
+        /**
+         * When all (or a significant number) of adjacent turfs update.
+         * Turfs are ordered by direction, i.e. North, NorthEast, East ... NorthWest
+         */
+        public void UpdateAll(TileDefinition[] tiles)
+        {
+            bool changed = false;
+            for (int i = 0; i < tiles.Length; i++)
+            {
+                changed |= UpdateSingleConnection((Direction)i, tiles[i]);
+            }
+
+            if (changed)
+                UpdateMeshAndDirection();
+        }
+
+        public void Awake() => filter = GetComponent<MeshFilter>();
+        public void OnEnable() => UpdateMeshAndDirection();
+
+        /**
+         * Adjusts the connections value based on the given new tile.
+         * Returns whether value changed.
+         */
+        private bool UpdateSingleConnection(Direction direction, TileDefinition tile)
+        {
+            int index = (int)Layer;
+
+            bool isConnected = (tile.turf && (tile.turf.genericType == type || type == null));
+            if (tile.fixtures != null)
+                isConnected = isConnected || (tile.fixtures[index] && (tile.fixtures[index].genericType == type || type == null));
+            return adjacents.UpdateDirection(direction, isConnected, true);
+        }
+
+        private void UpdateMeshAndDirection()
+        {
+            // Count number of connections along cardinal (which is all that we use atm)
+            var cardinalInfo = adjacents.GetCardinalInfo();
+
+            // Determine rotation and mesh specially for every single case.
+            float rotation = 0.0f;
+            Mesh mesh;
+
+            if (cardinalInfo.IsO())
+                mesh = o;
+            else if (cardinalInfo.IsC())
+            {
+                mesh = c;
+                rotation = DirectionHelper.AngleBetween(Direction.East, cardinalInfo.GetOnlyPositive());
+            }
+            else if (cardinalInfo.IsI())
+            {
+                mesh = i;
+                rotation = OrientationHelper.AngleBetween(Orientation.Horizontal, cardinalInfo.GetFirstOrientation());
+            }
+            else if (cardinalInfo.IsL())
+            {
+                mesh = l;
+                rotation = DirectionHelper.AngleBetween(Direction.SouthEast, cardinalInfo.GetCornerDirection());
+            }
+            else if (cardinalInfo.IsT())
+            {
+                mesh = t;
+                rotation = DirectionHelper.AngleBetween(Direction.West, cardinalInfo.GetOnlyNegative());
+            }
+            else // Must be X
+                mesh = x;
+
+            if (filter == null)
+                filter = GetComponent<MeshFilter>();
+
+            filter.mesh = mesh;
+            transform.localRotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, rotation, transform.localRotation.eulerAngles.z);
+        }
+
+        // A bitfield of connections. Total of 8 connections -> 8 bits, ascending order with direction.
+        private AdjacencyBitmap adjacents = new AdjacencyBitmap();
+        private MeshFilter filter;
+    }
+}
