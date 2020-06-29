95fd7e82e3812271d8a1f80c4842496f4529ae27
João Buratto <46575243+joaoburatto@users.noreply.github.com>
2020-06-29 15:32:56 -0300

    Skybox rotation (#441)
    * Skybox rotation
    
    This adds another skybox aswell as rotation for the skybox over time, the rotation rate can be bumped up in the main camera.
    
    * Updated main camera and import settings
    
    Updates the main camera prefab to have the skybox controller and updates all the export settings for better image quality
    
    * Update skybox materials
    
    * Rename skybox parts for easier searching
    
    Co-authored-by: CosmicCoincidence <cosmiccoincidence1991@gmail.com>

---
+++ Assets/SkyboxController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxController : MonoBehaviour
+{
+    private Skybox current;
+    
+    [Range(0.005f, 0.02f)]
+    public float rotationRate = 0.02f;
+
+    private void Start()
+    {
+        current = GetComponent<Skybox>();
+    }
+
+    void FixedUpdate()
+    {
+        current.material.SetFloat(("_Rotation"), current.material.GetFloat("_Rotation") + rotationRate);
+    }
+}
