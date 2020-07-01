934d7b1ee833264158469e4bf473cc31c90cc92a
João Buratto <joaoburattoc@gmail.com>
2020-07-01 10:48:27 -0300

    Basic pause menu UI

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    Animator animator;
+    
+    void Start() {    
+        animator = GetComponent<Animator>();
+    }
+    
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) 
+        {
+            animator.SetBool("Toggle", !animator.GetBool("Toggle"));
+        }
+    }
+}
