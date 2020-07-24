8e11a545dee9f2cf9057f1ca8870b5768ccf841e
Ashes2007 <owek16@gmail.com>
2020-07-23 21:47:20 -0500

    Changed - Flashlight.cs Changed - flashlight.prefab
    Added - flashlight_Off.mat
    Added - flashlight_Off.mat.meta
    
    The material of a flashlight bulb will now change when it's toggled.

--- Assets/Content/Items/Functional/Tools/Generic/Flashlight.cs
+++ Assets/Content/Items/Functional/Tools/Generic/Flashlight.cs
@@ -2,12 +2,15 @@
 using Mirror;
 using SS3D.Engine.Interactions;
 
-namespace SS3D.Content.Items.Functional.Tools
+namespace SS3D.Content.Items.Functional.Generic
 {
-    public class Flashlight : NetworkBehaviour, Interaction
+    public class Boombox : NetworkBehaviour, Interaction
     {
         [SerializeField]
         private new Light light = null;
+        private GameObject bulb = null;
+        private Material onmat = null;
+        private Material offmat = null;
 
         public InteractionEvent Event { get; set; }
         public string Name => "Turn On/Off";
@@ -26,12 +29,23 @@ namespace SS3D.Content.Items.Functional.Tools
         {
             light.enabled = !light.enabled;
             RpcSetLight(light.enabled);
+          
         }
 
         [ClientRpc]
         private void RpcSetLight(bool value)
         {
             light.enabled = value;
+            Material mat = null;
+            if (value)
+            {
+                mat = onmat;
+            }
+            else
+            {
+                mat = offmat;
+            }
+            bulb.GetComponent<MeshRenderer>().sharedMaterial = mat;
         }
     }
 }
