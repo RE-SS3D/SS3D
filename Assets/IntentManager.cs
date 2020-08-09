985881e7c7668c9cf65752ba85c2a3b3b7c4b268
João Buratto <joaoburattoc@gmail.com>
2020-08-09 11:57:06 -0300

    Small changes to the Intent Menu

--- Assets/IntentManager.cs
+++ Assets/IntentManager.cs
@@ -10,57 +10,26 @@ using UnityEngine.UI;
 public class IntentManager : MonoBehaviour
 {
     public IntentType selectedIntent;
+    
+    public Image intentImage;
+
+    public Sprite spriteHelp;
+    public Sprite spriteHarm;
+
+    public Color colorHarm;
+    public Color colorHelp;
 
     public enum IntentType
     {
         Help,
         Harm
     }
-
-    public Button[] intents;
-    
-    private void Start()
-    {
-        // Assign the colors to the buttonss
-        Setup();
-    }
-
     // Select the body part and deselect others
     public void SelectIntent(Button selected)
     {
-        selected.Select();
-        // disables the selected button
-        selected.interactable = false;
-        selectedIntent = SetIntent(selected.name);
-        foreach (Button intent in intents)
-        {
-            if (intent != selected)
-            {
-                if (intent.enabled)
-                {
-                    intent.OnDeselect(null);
-                    intent.interactable = true;
-                }
-            }
-        }
-    }
-
-    private IntentType SetIntent(string type)
-    {
-        return (IntentType) System.Enum.Parse(typeof(IntentType), type);
-    }
-    
-    // Assign color to the buttons, and I wanted it to assign the event, however it doesn't work so I did manually
-    // TODO: make onClick automatic
-    [ContextMenu("Setup buttons")]
-    public void Setup()
-    {
-        foreach (Button intent in intents)
-        {
-            intent.onClick.AddListener(delegate
-            {
-                SelectIntent(intent);
-            });
-        }
+        bool harm = selectedIntent == IntentType.Harm;
+        selectedIntent = harm ? IntentType.Help : IntentType.Harm;
+        intentImage.sprite = harm ? spriteHelp : spriteHarm;
+        intentImage.color = harm ? colorHelp : colorHarm;
     }
 }
