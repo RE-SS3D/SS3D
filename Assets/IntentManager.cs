6d19d7282e98d53203ef489743235519bfe15d1d
João Buratto <46575243+joaoburatto@users.noreply.github.com>
2020-08-17 15:11:22 -0300

    UI Rework (#557)
    * UI Rework
    
    * Bodypart selector and Intent UI
    
    * Small changes to the Intent Menu
    
    * Resize UI
    
    * Fix clothing menu offset
    
    Co-authored-by: Alain Siegrist <alainsiegrist02@gmail.com>

---
+++ Assets/IntentManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Basically a copy of HumanoidBOdyPartTargetSelector.cs
+/// This manages intent and it's done to easily support other intents
+/// </summary>
+public class IntentManager : MonoBehaviour
+{
+    public IntentType selectedIntent;
+    
+    public Image intentImage;
+
+    public Sprite spriteHelp;
+    public Sprite spriteHarm;
+
+    public Color colorHarm;
+    public Color colorHelp;
+
+    public enum IntentType
+    {
+        Help,
+        Harm
+    }
+    // Select the body part and deselect others
+    public void SelectIntent(Button selected)
+    {
+        bool harm = selectedIntent == IntentType.Harm;
+        selectedIntent = harm ? IntentType.Help : IntentType.Harm;
+        intentImage.sprite = harm ? spriteHelp : spriteHarm;
+        intentImage.color = harm ? colorHelp : colorHarm;
+    }
+}
