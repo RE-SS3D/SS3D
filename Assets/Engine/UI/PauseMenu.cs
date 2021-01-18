69f1d0a2e17fc9a395048971be12b3a168f4cc2e
João Buratto <46575243+joaoburatto@users.noreply.github.com>
2021-01-18 15:50:36 -0300

    Separating Lobby and Server (#575)
    * Simple Loading Screen
    
    This separates the main menu from the station scene aswell as adding a loading screen when you join a server. I also created a few images and added some wallpapers from our Google Drive.
    
    * Changed the lobby menu and added UI stuff
    
    Added a few windows presets for easy UI creation.
    Updated the Lobby prefab with the new windows and added a little settings for the host button, which is empty for now.
    Fixed a little problem with the loading screen.
    
    * Fixed Client loading screen not disabling
    
    * Fixed a bunch of bugs
    
    Fixed some scalling to support other resolutions.
    Fixed the disconnect button not working, now you can use it.
    Added OOC to the chat channels.
    Added Grimmie's blurred background.
    
    * Lobby menu stuff
    
    * Separate scenes and fix UI
    
    * UI animations and improvements
    
    * Player spawning
    
    * Spawning player
    
    * Round timer
    
    * Map selection
    
    * Temporary fix to the pause menu
    
    * Moving code from RoundManager to ServerLobbyUIHelper
    
    * Event fixes (Local only)
    
    * Attempt to network
    
    * Prefab Fixes
    
    * Embark countdown now works
    
    * Attempt to fix the camera bug (Pushing so I can ask for help lol)
    
    * Fix camera bug
    
    * Only unlock start round after map is loaded
    
    * Table prefab fixes
    
    * Fixed pause menu
    
    * Failling to connect doesn't lock your game
    
    * Pause menu no longer can be accessed via the lobby
    
    * Fixed clicking embark twice
    
    * Fixed camera bugs and added lobby error message
    
    * Networked scene unloading
    
    * Fixed weird animation on the character preview (I think)
    
    * Round restart and map selection fixes
    
    * Changed Pause menu UI and server settings UI additions

--- Assets/Engine/UI/PauseMenu.cs
+++ Assets/Engine/UI/PauseMenu.cs
@@ -1,20 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Mirror;
+using SS3D.Engine.Server.Round;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Networking.Types;
 
-public class PauseMenu : MonoBehaviour
+public class PauseMenu : NetworkBehaviour
 {
     [SerializeField] Animator animator;
+    LoginNetworkManager networkManager;
     
     void Start() 
     {
         if (animator == null)
-        animator = GetComponent<Animator>();
+            animator = GetComponent<Animator>();
+        networkManager = LoginNetworkManager.singleton;
+
+        // turned off for now, I need a better solution for that
+        //RoundManager.ServerRoundEnded += ForceToggleOff;
     }
     
     void Update() 
     {
-        if (Input.GetKeyDown(KeyCode.Escape)) 
+        if (Input.GetKeyDown(KeyCode.Escape) && !CameraManager.singleton.lobbyCamera.gameObject.activeSelf) 
         {
             Toggle();
         }
@@ -22,6 +31,37 @@ public class PauseMenu : MonoBehaviour
 
     public void Toggle()
     {
+        if (!animator.enabled)
+        { 
+            animator.enabled = true; 
+            return;
+        }
         animator.SetBool("Toggle", !animator.GetBool("Toggle"));
+    }
+
+    public void ForceToggleOff()
+    {
+        if (animator == null) return;
+        
+        if (!animator.enabled && animator.GetBool("Toggle"))
+        { 
+            animator.enabled = true;
+            animator.SetBool("Toggle", false); 
+        }
+    }
+
+    public void Disconnect()
+    {
+        if (isServer)
+        {
+            networkManager.StopHost();
+            Debug.Log("server");
+        }
+        if (isClient)
+        {
+            Debug.Log("client");
+            networkManager.StopClient();
+        }
+        
     }
 }
