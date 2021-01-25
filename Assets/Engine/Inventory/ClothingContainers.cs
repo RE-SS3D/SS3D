fa8bbc3d46af201adce09824f4d1cc16855d6f9e
Alain Siegrist <alainsiegrist02@gmail.com>
2021-01-25 15:40:14 +0100

    Fix broken suit storage

--- Assets/Engine/Inventory/ClothingContainers.cs
+++ Assets/Engine/Inventory/ClothingContainers.cs
@@ -15,9 +15,11 @@ namespace SS3D.Engine.Inventory
         public AttachedContainer Head => Containers["Head"];
         public AttachedContainer Shoes => Containers["Shoes"];
         public AttachedContainer Accessory => Containers["Accessory"];
+        // TODO: Replace with actual clothing storage
+        public AttachedContainer SuitStorage => Containers["Suit Storage"];
 
         [NonSerialized]
-        public static readonly string[] ClothingSlotNames = {"Ears", "Jumpsuit", "Exosuit", "Glasses", "Mask", "Gloves", "Head", "Shoes", "Accessory"};
+        public static readonly string[] ClothingSlotNames = {"Ears", "Jumpsuit", "Exosuit", "Glasses", "Mask", "Gloves", "Head", "Shoes", "Accessory", "Suit Storage"};
 
         [NonSerialized]
         public Dictionary<string, AttachedContainer> Containers = new Dictionary<string, AttachedContainer>();
