0059f5dacbdb569f1d4563ee1ec5eea09e36cf73
CrazyMalk <lucas_o__castro@hotmail.com>
2021-02-10 18:10:47 -0300

    Create PepperSpray class to handle interaction with the pepper spray (#604)
    * Create PepperSpray class to handle interaction
    
    Fixes #600, adds a class that plays the spray effect when the item is interacted with. Ideally, interacting with the item should cycle between power like in the normal game (in fact, as all spray items have this power feature, they should probably share an abstract class or interface), while clicking somewhere would fire in that direction, but no combat system so whatever.
    
    * Update pepper spray's particle system collision
    
    Updates the pepper spray's particle system so it doesnt collide with the player and disappears instantly on some occasions
    
    * Add spray sound to pepper spray

---
+++ Assets/Content/Items/Functional/Containers/Liquid/PepperSpray.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Mirror;
+using SS3D.Content.Systems.Interactions;
+using SS3D.Engine.Interactions;
+using SS3D.Engine.Interactions.Extensions;
+using SS3D.Engine.Inventory;
+using UnityEngine;
+
+namespace SS3D.Content.Items.Functional.Weapons
+{
+    public class PepperSpray: Item
+    {
+        private class SprayInteraction : IInteraction
+        {
+            public Sprite icon;
+
+            public IClientInteraction CreateClient(InteractionEvent interactionEvent)
+            {
+                return null;
+            }
+
+            public string GetName(InteractionEvent interactionEvent)
+            {
+                return "Spray";
+            }
+
+            public Sprite GetIcon(InteractionEvent interactionEvent)
+            {
+                return icon;
+            }
+
+            public bool CanInteract(InteractionEvent interactionEvent)
+            {
+                if (interactionEvent.Target is PepperSpray spray)
+                {
+                    return InteractionExtensions.RangeCheck(interactionEvent) && spray.CanSpray();
+                }
+                return false;
+            }
+
+            public bool Start(InteractionEvent interactionEvent, InteractionReference reference)
+            {
+                if (interactionEvent.Target is PepperSpray spray)
+                {
+                    spray.Spray();
+                }
+                return false;
+            }
+
+            public bool Update(InteractionEvent interactionEvent, InteractionReference reference)
+            {
+                throw new System.NotImplementedException();
+            }
+
+            public void Cancel(InteractionEvent interactionEvent, InteractionReference reference)
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+
+        [SerializeField] private AudioClip spraySound = null;
+        [SerializeField] private ParticleSystem particleSystem;
+        [SerializeField] private float cooldownInSeconds;
+
+        public Sprite useIcon;
+        private AudioSource audioSource;
+        private float lastSprayTime;
+        private bool justSprayed;
+
+        public override void Start()
+        {
+            base.Start();
+            audioSource = GetComponent<AudioSource>();
+            GenerateNewIcon();
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            if (justSprayed)
+            {
+                particleSystem.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+                justSprayed = false;
+            }
+        }
+
+        private bool CanSpray()
+        {
+            return Time.time - lastSprayTime >= cooldownInSeconds;
+        }
+
+        [Server]
+        private void Spray()
+        {
+            lastSprayTime = Time.time;
+            particleSystem.Play();
+            if (audioSource != null)
+            {
+                audioSource.PlayOneShot(spraySound);
+            }
+            justSprayed = true;
+            RpcSpray();
+        }
+
+        [ClientRpc]
+        private void RpcSpray()
+        {
+            lastSprayTime = Time.time;
+            particleSystem.Play();
+            if (audioSource != null)
+            {
+                audioSource.PlayOneShot(spraySound);
+            }
+            justSprayed = true;
+        }
+
+        public override IInteraction[] GenerateInteractions(InteractionEvent interactionEvent)
+        {
+            List<IInteraction> list = base.GenerateInteractions(interactionEvent).ToList();
+            list.Add(new SprayInteraction { icon = useIcon });
+            return list.ToArray();
+        }
+    }
+}
