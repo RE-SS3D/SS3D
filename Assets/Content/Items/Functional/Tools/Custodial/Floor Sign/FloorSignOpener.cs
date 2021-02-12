f89b42b4d913fa648c922d2dfb7596b161d1a222
CrazyMalk <lucas_o__castro@hotmail.com>
2021-02-12 07:39:35 -0300

    Update floor sign mesh and create custom openable class

---
+++ Assets/Content/Items/Functional/Tools/Custodial/Floor Sign/FloorSignOpener.cs
@@ -0,0 +1,35 @@
+﻿using SS3D.Engine.Inventory;
+using SS3D.Content.Furniture;
+
+namespace SS3D.Content.Items.Cosmetic
+{
+    public class FloorSignOpener : Openable
+    {
+        private bool lastFrameInContainer;
+        private Item item;
+
+        public override void Start()
+        {
+            base.Start();
+            item = GetComponent<Item>();
+
+            lastFrameInContainer = item.InContainer();
+            UpdateStatus(lastFrameInContainer);
+        }
+
+        private void Update()
+        {
+            bool newInContainer = item.InContainer();
+            if (lastFrameInContainer != newInContainer)
+            {
+                UpdateStatus(newInContainer);
+            }
+            lastFrameInContainer = newInContainer;
+        }
+
+        private void UpdateStatus(bool inContainer)
+        {
+            SetOpen(!inContainer);
+        }
+    }
+}
