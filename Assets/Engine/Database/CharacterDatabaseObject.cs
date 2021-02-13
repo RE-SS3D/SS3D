94c65ef0e8fcf3cc1d0165f040fce1776511f145
João Buratto <joaoburattoc@gmail.com>
2021-02-12 21:12:37 -0300

    Basic MySQL setup

---
+++ Assets/Engine/Database/CharacterDatabaseObject.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace SS3D.Engine.Database
+{
+    public class CharacterData : MonoBehaviour
+    {
+        [SerializeField] private string name;
+        [SerializeField] private string gender;
+    }
+}
---
+++ Assets/Engine/Database/DatabaseConnectionManager.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using UnityEngine;
+
+using MySql;
+using MySql.Data.MySqlClient;
+using Debug = UnityEngine.Debug;
+
+public class DatabaseConnectionManager : MonoBehaviour
+{
+    public static DatabaseConnectionManager singleton { get; private set; }
+    
+    [SerializeField] private string serverURL;
+    [SerializeField] private string user;
+    [SerializeField] private string database;
+    [SerializeField] private string port;
+    [SerializeField] private string password;
+
+    private string connectionString;
+
+    private void Awake()
+    {
+        if (singleton != null && singleton != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            singleton = this;
+        }
+    }
+    private void Start()
+    {
+        Connect();
+    }
+
+    [ContextMenu("Connect")]
+    public void Connect()
+    {
+        string connection =
+            "server=" + serverURL + ";" +
+            "user=" + user + ";" +
+            "database=" + database + ";" +
+            "port=" + port + ";" +
+            "password=" + password;
+
+        connectionString = connection;
+        MySqlConnection conn = new MySqlConnection(connection);
+        conn.Open();
+        
+        GetDatabaseState(conn);
+    }
+
+    public ConnectionState GetDatabaseState(MySqlConnection conn)
+    {
+        ConnectionState state = conn.State;
+
+        StartCoroutine(WaitUntilConnected(state));
+        Debug.Log("connecting to database: " + connectionString);
+
+        switch(state)
+        {
+            case ConnectionState.Open:
+                Debug.Log("connected");
+                break;
+            case ConnectionState.Closed:
+                Debug.Log("failed to connect");
+                break;
+            case ConnectionState.Broken:
+                Debug.Log("connection lost");
+                break;
+            case ConnectionState.Executing:
+                Debug.Log("database is executing a command");
+                break;
+            case ConnectionState.Fetching:
+                Debug.Log("database is fetching data");
+                break;
+        }
+            
+        return state;
+    }
+
+    public IEnumerator WaitUntilConnected(ConnectionState state)
+    {
+        yield return new WaitUntil(delegate { return state != ConnectionState.Connecting; });
+    }
+    
+}
