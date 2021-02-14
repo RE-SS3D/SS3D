a7aab33608fe02e9486adedf3651541faba45057
CrazyMalk <lucas_o__castro@hotmail.com>
2021-02-14 16:19:13 -0300

    Make floor sign openable/closeable  (#609)
    * Update floor sign mesh and create custom openable class
    
    * Streamline FloorSignOpener code
    
    * Add NetworkedOpenable component to floor signs

---
+++ Assets/Content/Items/Functional/Tools/Custodial/Floor Sign/FloorSignOpener.cs
@@ -0,0 +1,34 @@
+﻿using SS3D.Engine.Inventory;
+using SS3D.Content.Furniture;
+
+namespace SS3D.Content.Items.Cosmetic
+{
+    public class FloorSignOpener : Openable
+    {
+        private bool lastInContainer;
+        private Item item;
+
+        public override void Start()
+        {
+            base.Start();
+            item = GetComponent<Item>();
+
+            OnContainerUpdate(item.InContainer());
+        }
+
+        private void Update()
+        {
+            bool newInContainer = item.InContainer();
+            if (lastInContainer != newInContainer)
+            {
+                OnContainerUpdate(newInContainer);
+            }
+        }
+
+        private void OnContainerUpdate(bool inContainer)
+        {
+            SetOpen(!inContainer);
+            lastInContainer = inContainer;
+        }
+    }
+}
