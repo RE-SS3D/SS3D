068ccd8e2a54fe0fac3439a82a08fc0163cd5827
CrazyMalk <lucas_o__castro@hotmail.com>
2021-02-15 23:27:28 -0300

    Implement better lighter interaction

---
+++ Assets/Content/Items/Functional/Generic/Lighters/LighterOpenable.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using SS3D.Content.Furniture;
+
+namespace SS3D.Content.Items.Functional.Generic.Lighters
+{
+    public class LighterOpenable : NetworkedOpenable
+    {
+        [SerializeField]
+        private ParticleSystem fireParticle;
+
+        protected override void OnOpenStateChange(object sender, bool e)
+        {
+            base.OnOpenStateChange(sender, e);
+            SetFlame(e);
+        }
+
+        private void SetFlame(bool on)
+        {
+            if (on)
+            {
+                fireParticle.Play();
+            }
+            else
+            {
+                fireParticle.Stop();
+            }
+        }
+    }
+}
+
