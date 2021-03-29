679fbc3b9cb234291a90d0eb37d1dfc056184c7f
João Buratto <46575243+joaoburatto@users.noreply.github.com>
2021-03-29 15:55:02 -0300

    Update UserDatabaseObject.cs

--- Assets/Engine/Database/UserDatabaseObject.cs
+++ Assets/Engine/Database/UserDatabaseObject.cs
@@ -3,12 +3,15 @@ using UnityEngine;
 
 namespace SS3D.Engine.Database
 {
+    // Handles user database operations
     public class UserDatabaseObject : NetworkBehaviour
     {
+        // unique key to users, ckey stands for client key
         public string ckey;
 
         private void Start()
         {
+            // this updates at start cause we  start this when we connect, so we check if this user was here once or not
             UpdateCkey();
         }
         
@@ -16,7 +19,9 @@ namespace SS3D.Engine.Database
         [ContextMenu("Update CKey")]
         public void UpdateCkey()
         {
+            // this is just a test, we have to have a way to work on the localCkey, probably with the Hub
             ckey = LoginNetworkManager.singleton.localCkey;
+            // and we assign it to the local player manager which is just handy stuff
             LocalPlayerManager.singleton.ckey = ckey;
         }
 
