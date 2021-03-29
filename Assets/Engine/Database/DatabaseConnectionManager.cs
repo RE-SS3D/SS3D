1bb43f4eccfe83124c25fd0abdf9a845fcbd94de
João Buratto <46575243+joaoburatto@users.noreply.github.com>
2021-03-29 15:50:00 -0300

    Update DatabaseConnectionManager.cs

--- Assets/Engine/Database/DatabaseConnectionManager.cs
+++ Assets/Engine/Database/DatabaseConnectionManager.cs
@@ -9,16 +9,23 @@ using Debug = UnityEngine.Debug;
 
 namespace SS3D.Engine.Database
 {
+    // Handles the connection to the local database on the host
     public class DatabaseConnectionManager : NetworkBehaviour
     {
         public static DatabaseConnectionManager singleton { get; private set; }
 
+        // database ip
         [SerializeField] private string serverURL;
+        // database user
         [SerializeField] private string user;
+        // database name
         [SerializeField] private string database;
+        // database port
         [SerializeField] private string port;
+        // database pasword
         [SerializeField] private string password;
 
+        // full connection string
         private string connectionString;
         public MySqlConnection conn;
 
@@ -41,6 +48,7 @@ namespace SS3D.Engine.Database
 
         [Server]
         [ContextMenu("Connect")]
+        // Handles the database connection
         public void Connect()
         {
             string connection =
@@ -52,15 +60,18 @@ namespace SS3D.Engine.Database
 
             connectionString = connection;
             conn = new MySqlConnection(connection);
+            // actually connects
             conn.Open();
-
+            
+            // just to make sure lets do this
             GetDatabaseState(conn);
         }
 
         public ConnectionState GetDatabaseState(MySqlConnection conn)
         {
             ConnectionState state = conn.State;
-
+            
+            // We wait until we have a state that is not "connecting"
             StartCoroutine(WaitUntilConnected(state));
             Debug.Log("connecting to database: " + connectionString);
 
@@ -88,25 +99,32 @@ namespace SS3D.Engine.Database
 
         public IEnumerator WaitUntilConnected(ConnectionState state)
         {
+            // wait until we are not connecting
             yield return new WaitUntil(delegate { return state != ConnectionState.Connecting; });
         }
 
         [Server]
+        // handles any querry execution
         public void ExecuteQuerry(string sql)
         {
+            // we get the connection state to not try using a database that is offline
             ConnectionState state = GetDatabaseState(conn);
             
             Debug.Log("Executing new SQL query, database state: " + conn);
 
             if (state == ConnectionState.Closed || state == ConnectionState.Broken)
             {
+                // if we are disconnected from the database we try connecting again
                 Connect();
             }
             
+            // sets up the querry for the MySQL "framework"
             MySqlCommand cmd = new MySqlCommand(sql, conn);
 
+            // executes the querry and handles the result of the querry
             object result = cmd.ExecuteScalar();
 
+            // if theres a result we debug it, or just comment it out. we should have a log for this eventually
             if (result != null)
                 Debug.Log(cmd.ExecuteScalar().ToString());
         }
