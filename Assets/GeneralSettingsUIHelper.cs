712999d96c65dec3cd814c639d4d65e4e9c73bc5
João Buratto <46575243+joaoburatto@users.noreply.github.com>
2021-03-29 14:51:08 -0300

    Basic quality settings UI (#613)
    * Basic quality settings UI
    
    * Basic graphic settings

---
+++ Assets/GeneralSettingsUIHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class GeneralSettingsUIHelper : MonoBehaviour
+{
+    public Toggle toggleSSRT;
+
+    public Button[] graphicButtons;
+
+    public Button[] tabs;
+    
+    private void Start()
+    {
+        GeneralSettingsManager settings = GeneralSettingsManager.singleton;
+
+        toggleSSRT.isOn = settings.ssrt.enabled;
+
+        tabs[0].Select();
+        
+        graphicButtons[(int)settings.graphicSettings].Select();
+    }
+}
