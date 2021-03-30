292d14a94537a569a6c77f0fb3746055e7ae280b
João Buratto <46575243+joaoburatto@users.noreply.github.com>
2021-03-30 16:33:27 -0300

    Add a few comments

--- Assets/Engine/Inventory/Extensions/PickupInteraction.cs
+++ Assets/Engine/Inventory/Extensions/PickupInteraction.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 namespace SS3D.Engine.Inventory.Extensions
 {
+    // A pickup interaction is when you pick an item and
+    // add it into a container (in this case, the hands)
+    // you can only pick things that are not in a container
     public class PickupInteraction : IInteraction
     {
         public Sprite icon;
@@ -26,19 +29,25 @@ namespace SS3D.Engine.Inventory.Extensions
 
         public bool CanInteract(InteractionEvent interactionEvent)
         {
+            // if the target is whatever the hell Alain did
+            // and the part that matters, if the interaction source is a hand
             if (interactionEvent.Target is IGameObjectProvider targetBehaviour && interactionEvent.Source is Hands hands)
             {
+		// if the selected hand is not empty we return false
                 if (!hands.SelectedHandEmpty)
                 {
                     return false;
                 }
                 
+		// we try to get the Item component from the GameObject we just interacted with
+		// you can only pickup items (for now, TODO: we have to consider people too), which makes sense
                 Item item = targetBehaviour.GameObject.GetComponent<Item>();
                 if (item == null)
                 {
                     return false;
                 }
-
+		// then we just do a range check, to make sure we can interact
+		// and we check if the item is not in a container, you can only pick things that are not in a container
                 return InteractionExtensions.RangeCheck(interactionEvent) && !item.InContainer();
             }
 
@@ -47,8 +56,11 @@ namespace SS3D.Engine.Inventory.Extensions
 
         public bool Start(InteractionEvent interactionEvent, InteractionReference reference)
         {
+	    // remember that when we call this Start, we are starting the interaction per see
+	    // so we check if the source of the interaction is a Hand, and if the target is an Item
             if (interactionEvent.Source is Hands hands && interactionEvent.Target is Item target)
             {
+		// and then we run the function that adds it to the container
                 hands.Pickup(target);
             }
             return false;
