c22781afb96422413d20c775974f1376118fd9bf
Michael Both <mjboth@earthlink.net>
2021-04-06 03:09:28 -0500

    remove completed TODO

--- Assets/Content/Furniture/Machines/Kitchen/Microwave/Microwave.cs
+++ Assets/Content/Furniture/Machines/Kitchen/Microwave/Microwave.cs
@@ -12,7 +12,6 @@ using Mirror;
 using UnityEngine.Assertions;
 
 // Handles the microwave object
-// TODO: IMPORTANT, rename the SND stuff to Sound
 [RequireComponent(typeof(AudioSource))]
 public class Microwave : InteractionTargetNetworkBehaviour
 {
