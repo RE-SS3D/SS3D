8d5e8a0ab0e4c89bb235fd6c80550fd0e1695360
CosmicCoincidence <cosmiccoincidence1991@gmail.com>
2021-05-21 18:16:31 -0400

    Revert "Move menu managers & some assets a root folder"
    This reverts commit ea3126c52c2af39c175b477fdc9c60b92b5aecc1.

---
+++ Assets/Content/Systems/Managers/LoadingScreenManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using SS3D;
+using SS3D.Engine.Tiles;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Tilemaps;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class LoadingScreenManager : MonoBehaviour
+{
+    [SerializeField] private Sprite[] wallpapers;
+    [SerializeField] private Animator animator;
+
+    [SerializeField]
+    private Image image;
+
+    private void Start()
+    {
+        SceneLoaderManager.mapLoaded += delegate { Toggle(true); };
+        TileManager.tileManagerLoaded += delegate { Toggle(false); };
+    }
+
+    public void Toggle(bool state)
+    {
+        if (!animator.enabled) animator.enabled = true;
+        animator.SetBool("Toggle", state);
+    }
+    private void OnEnable()
+    {
+        image.sprite = wallpapers[Random.Range(0, wallpapers.Length - 1)];
+    }
+}
