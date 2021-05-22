bbdf7765da40cbe1435a8506fb34ae02f1ef9f1d
ryanputh <ryanputh@gmail.com>
2021-05-22 22:47:54 +1000

    Commenced implementation of IExamineRequirement, which provides CanExamine() method.

---
+++ Assets/Engine/Examine/ReqMaxRange.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace SS3D.Engine.Examine
+{
+    public class ReqMaxRange : AbstractRequirementDecorator
+    {
+		
+		private Range;
+		
+		public ReqMaxRange(IExamineRequirement wrapped, float range)
+		{
+			DecoratedObject = wrapped;
+			Range = range;
+		}
+		
+		public bool MeetsRequirement(GameObject examinator)
+		{
+			GameObject go = GetBaseObject();
+			Vector2 target = new Vector2(go.transform.position.x, go.transform.position.z);
+			Vector2 source = new Vector2(examinator.transform.position.x, examinator.transform.position.z);
+			return Vector2.Distance(target, source) <= Range;
+		}
+    }
+}
