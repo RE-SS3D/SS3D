bbe3b18b3ab29c49ccad9ad180bb4b31120110f5
stilnat <nathan.raynal.castang@gmail.com>
2021-06-16 03:43:56 +0200

    add name of containers to the containerUI
    See issue #701 . The name of the container is now displayed at the top of it.

--- Assets/Engine/Inventory/UI/ContainerUi.cs
+++ Assets/Engine/Inventory/UI/ContainerUi.cs
@@ -46,6 +46,16 @@ namespace SS3D.Engine.Inventory.UI
             float height = rectTransform.offsetMin.y + Math.Abs(rectTransform.offsetMax.y) + gridDimensions.y;
             var rect = transform.GetChild(0).GetComponent<RectTransform>();
             rect.sizeDelta = new Vector2(width, height);
+
+            // Set the text inside the containerUI to be the name of the container
+            GameObject containerName = GameObject.Find("Container name");
+            Text containerNameText = containerName.GetComponent<Text>(); 
+            containerNameText.text = attachedContainer.name; 
+
+            // Position the text correctly inside the UI.
+            Vector3[] v = new Vector3[4];
+            rect.GetLocalCorners(v); 
+            containerName.transform.localPosition = v[1] + new Vector3(0.03f * width, -0.02f * height, 0);
         }
     }
 }
