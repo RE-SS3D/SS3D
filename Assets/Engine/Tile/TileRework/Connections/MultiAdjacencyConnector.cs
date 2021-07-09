fde88288bea770b15beea56083c32ae2d0ab3bc7
BroodjeKipcorn <rutger_dezweef@hotmail.com>
2021-07-09 23:52:38 +0200

    Improve the multi adjacency connector

--- Assets/Engine/Tile/TileRework/Connections/MultiAdjacencyConnector.cs
+++ Assets/Engine/Tile/TileRework/Connections/MultiAdjacencyConnector.cs
@@ -6,21 +6,9 @@ namespace SS3D.Engine.Tiles.Connections
 {
     public class MultiAdjacencyConnector : AbstractAdjacencyConnector
     {
-        [SerializeField] private AdjacencyType adjacencyType;
-
-        [Header("Simple Meshes")]
-        [Tooltip("A mesh where no edges are connected")]
-        public Mesh o;
-        [Tooltip("A mesh where the north edge is connected")]
-        public Mesh c;
-        [Tooltip("A mesh where the north & south edges are connected")]
-        public Mesh i;
-        [Tooltip("A mesh where the north & east edges are connected")]
-        public Mesh l;
-        [Tooltip("A mesh where the north, east, and west edges are connected")]
-        public Mesh t;
-        [Tooltip("A mesh where all edges are connected")]
-        public Mesh x;
+        public AdjacencyType selectedAdjacencyType;
+        [SerializeField] private SimpleAdjacency simpleAdjacency;
+        [SerializeField] private AdvancedAdjacency advancedAdjacency;
 
         public override void UpdateAll(PlacedTileObject[] neighbourObjects)
         {
@@ -44,49 +32,29 @@ namespace SS3D.Engine.Tiles.Connections
 
         private bool UpdateSingleConnection(Direction dir, PlacedTileObject placedObject)
         {
-            bool isConnected = (placedObject && placedObject.HasAdjacencyConnector());
+            bool isConnected = (placedObject && placedObject.HasAdjacencyConnector() && (placedObject.GetGenericType() == type || type == null));
             return adjacents.UpdateDirection(dir, isConnected, true);
         }
 
         protected override void UpdateMeshAndDirection()
         {
-            // Count number of connections along cardinal (which is all that we use atm)
-            var cardinalInfo = adjacents.GetCardinalInfo();
-
-            // Determine rotation and mesh specially for every single case.
-            float rotation = 0.0f;
-            Mesh mesh;
-
-            if (cardinalInfo.IsO())
-                mesh = o;
-            else if (cardinalInfo.IsC())
+            MeshDirectionInfo info = new MeshDirectionInfo();
+            switch (selectedAdjacencyType)
             {
-                mesh = c;
-                rotation = TileHelper.AngleBetween(Direction.North, cardinalInfo.GetOnlyPositive());
+                case AdjacencyType.Simple:
+                    info = simpleAdjacency.GetMeshAndDirection(adjacents);
+                    break;
+                case AdjacencyType.Advanced:
+                    info = advancedAdjacency.GetMeshAndDirection(adjacents);
+                    break;
             }
-            else if (cardinalInfo.IsI())
-            {
-                mesh = i;
-                rotation = TileHelper.AngleBetween(Orientation.Vertical, cardinalInfo.GetFirstOrientation());
-            }
-            else if (cardinalInfo.IsL())
-            {
-                mesh = l;
-                rotation = TileHelper.AngleBetween(Direction.NorthEast, cardinalInfo.GetCornerDirection());
-            }
-            else if (cardinalInfo.IsT())
-            {
-                mesh = t;
-                rotation = TileHelper.AngleBetween(Direction.South, cardinalInfo.GetOnlyNegative());
-            }
-            else // Must be X
-                mesh = x;
+            
 
             if (filter == null)
                 filter = GetComponent<MeshFilter>();
 
-            filter.mesh = mesh;
-            transform.localRotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, rotation, transform.localRotation.eulerAngles.z);
+            filter.mesh = info.mesh;
+            transform.localRotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, info.rotation, transform.localRotation.eulerAngles.z);
         }
     }
 }
