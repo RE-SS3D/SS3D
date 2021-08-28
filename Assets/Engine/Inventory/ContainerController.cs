481905ce6c8518cecc7a727fc211ec3cdcad8ab9
stilnat <nathan.raynal.castang@gmail.com>
2021-08-28 19:15:41 +0200

    Add Controls for containers

--- Assets/Engine/Inventory/ContainerController.cs
+++ Assets/Engine/Inventory/ContainerController.cs
@@ -1,6 +1,7 @@
 
 using UnityEngine;
 using SS3D.Content.Furniture.Storage;
+using System.Collections.Generic;
 
 namespace SS3D.Engine.Inventory
 {
@@ -17,73 +18,156 @@ namespace SS3D.Engine.Inventory
     /// </summary>
     public class ContainerController : MonoBehaviour
     {
-        // can either be Openable or Storage, not both.
-        public bool isOpenable;
-        public bool addedContainer = false;
-        public bool hasAttachedContainer => gameObject.GetComponent<AttachedContainer>() != null;
-        public bool hasOpenableContainer => gameObject.GetComponent<OpenableContainer>() != null;
+        public List<ContainerDescriptor> ContainerDescriptors = new List<ContainerDescriptor>();
 
         // if is normal, has an UI, if Pile or Hidden, has no UI.
         // Visible script only attached on container with an UI. 
         // Warning, Pile container can have an UI when there is multiple containers on the same game object, in the context menu. 
         public ContainerType containerType;
 
-        public Vector2Int Size;
-
-
-        public void Start()
+        public void AddBaseComponent()
         {
-           /* AttachedContainer attachedContainer = gameObject.AddComponent<AttachedContainer>();
-            gameObject.AddComponent<AttachedContainerGenerator>().initialize(attachedContainer, Size);
-            gameObject.AddComponent<ContainerSync>();
+            ContainerDescriptor ContainerDescriptor = new ContainerDescriptor();
 
-            if (isOpenable)
+            AttachedContainer AttachedContainer = gameObject.AddComponent<AttachedContainer>();
+            ContainerDescriptor.AttachedContainer = AttachedContainer;
+
+            AttachedContainerGenerator AttachedContainerGenerator = gameObject.AddComponent<AttachedContainerGenerator>();
+            AttachedContainerGenerator.AttachedContainer = AttachedContainer;
+            ContainerDescriptor.AttachedContainerGenerator = AttachedContainerGenerator;
+            
+            if (ContainerDescriptors.Count == 0)
             {
-                gameObject.AddComponent<OpenableContainer>();
+                ContainerSync ContainerSync = gameObject.AddComponent<ContainerSync>();
+                ContainerDescriptor.ContainerSync = ContainerSync;
             }
             else
             {
-                gameObject.AddComponent<StorageContainer>();
+                ContainerDescriptor.ContainerSync = gameObject.GetComponent<ContainerSync>();
             }
 
-            if (containerType == ContainerType.Normal | containerType == ContainerType.Pile)
+            ContainerDescriptor.gameObject = gameObject;
+            ContainerDescriptors.Add(ContainerDescriptor);
+        }
+
+        public int FindIndexOf(AttachedContainer container)
+        {
+            int index = 0;
+            foreach (ContainerDescriptor containerDescriptor in ContainerDescriptors)
             {
-                gameObject.AddComponent<VisibleContainer>(); 
-            } */
+                if (container == containerDescriptor.AttachedContainer)
+                {
+                    return index;
+                }
+                index += 1;
+            }
+            Debug.Log("this container is not part of the list");
+            return 0;
         }
 
-        public void AddBaseComponent()
+        public void RemoveContainer(AttachedContainer container)
         {
-            AttachedContainer attachedContainer = gameObject.AddComponent<AttachedContainer>();
-            AttachedContainerGenerator attachedContainerGenerator = gameObject.AddComponent<AttachedContainerGenerator>();
-            ContainerSync containerSync = gameObject.AddComponent<ContainerSync>();
+            RemoveContainer(FindIndexOf(container));
         }
 
-        public void RemoveBaseComponent()
+        public void RemoveContainer(int IndexOf)
         {
-            DestroyImmediate(GetComponent(typeof(AttachedContainer)));
-            DestroyImmediate(GetComponent(typeof(AttachedContainerGenerator)));
-            DestroyImmediate(GetComponent(typeof(ContainerSync)));
+            DestroyImmediate(ContainerDescriptors[IndexOf].AttachedContainer);
+            DestroyImmediate(ContainerDescriptors[IndexOf].AttachedContainerGenerator);
+            if(ContainerDescriptors.Count == 1)
+            {
+                DestroyImmediate(ContainerDescriptors[IndexOf].ContainerSync);
+            }
+            DestroyImmediate(ContainerDescriptors[IndexOf].OpenableContainer);
+            DestroyImmediate(ContainerDescriptors[IndexOf].StorageContainer);
+            ContainerDescriptors.RemoveAt(IndexOf);
         }
 
-        public void AddAttachedContainer()
+        public void RemoveAllContainers()
         {
-            AttachedContainer attachedContainer = gameObject.AddComponent<AttachedContainer>();   
-        }
-        public void RemoveAttachedContainer()
-        {
-                 DestroyImmediate(GetComponent(typeof(AttachedContainer)));
-        }
-        public void AddOpenableContainer()
-        {
-            OpenableContainer attachedContainer = gameObject.AddComponent<OpenableContainer>();
-        }
-        public void RemoveOpenableContainer()
-        {
-            DestroyImmediate(GetComponent(typeof(OpenableContainer)));
+            foreach(ContainerDescriptor containerDescriptor in ContainerDescriptors)
+            {
+                DestroyImmediate(containerDescriptor.AttachedContainer);
+                DestroyImmediate(containerDescriptor.AttachedContainerGenerator);
+                DestroyImmediate(containerDescriptor.ContainerSync);
+                DestroyImmediate(containerDescriptor.OpenableContainer);
+                DestroyImmediate(containerDescriptor.StorageContainer);
+            }
+
+            ContainerDescriptors.Clear();
         }
 
+        public class ContainerDescriptor
+        {
+            public GameObject gameObject;
+            public AttachedContainer AttachedContainer;
+            public AttachedContainerGenerator AttachedContainerGenerator;
+            public ContainerSync ContainerSync;
+            public OpenableContainer OpenableContainer;
+            public StorageContainer StorageContainer;
 
+            private ContainerType ContainerType;
+            public ContainerType HasContainerType
+            {
+                get { return ContainerType;}
+                set { ContainerType = value; }
+            }
+
+            public bool IsOpenable
+            {
+                get { return OpenableContainer != null; }
+                set
+                {
+                    if (value && OpenableContainer == null)
+                    {
+                        AddOpenable();
+                    }
+                    else if (!value && OpenableContainer != null) RemoveOpenable();
+                }
+            }
+            public bool IsStorage
+            {
+                get { return StorageContainer != null; }
+                set
+                {
+                    if (value && StorageContainer == null)
+                    {
+                        AddStorage();
+                    }
+                    else if (!value && StorageContainer != null) RemoveStorage();
+                }
+            }
+
+            public void AddOpenable()
+            {
+                OpenableContainer = gameObject.AddComponent<OpenableContainer>();
+                if (IsStorage)
+                {
+                    DestroyImmediate(StorageContainer);
+                } 
+            }
+
+            public void RemoveOpenable()
+            {
+                DestroyImmediate(OpenableContainer);
+            }
+
+            public void AddStorage()
+            {
+                StorageContainer = gameObject.AddComponent<StorageContainer>();
+                if (IsOpenable)
+                {
+                    DestroyImmediate(OpenableContainer);
+                }
+            }
+
+            public void RemoveStorage()
+            {
+                DestroyImmediate(StorageContainer);
+            }
+        }
 
     }
+   
+
 }
--- Assets/Engine/Inventory/ContainerControllerEditor.cs
+++ Assets/Engine/Inventory/ContainerControllerEditor.cs
@@ -7,51 +7,48 @@ using SS3D.Engine.Inventory;
 public class ContainerControllerEditor : Editor
 {
 
-	private string labelText;
-	private bool addContainer = false;
-	private bool buttonPressed = false;
-	public override void OnInspectorGUI()
+    public void OnEnable()
+    {
+        // put here graphical stuff
+    }
+
+    public override void OnInspectorGUI()
 	{
-		
 		ContainerController containerController = (ContainerController)target;
-		
+
 		if (GUILayout.Button("Add Container", GUILayout.Width(250)))
 		{
+			containerController.AddBaseComponent();
+		}
+		int count = containerController.ContainerDescriptors.Count;
+		for (int i = count-1; i >=0; i--)
+        {
+			GUILayout.Label("Container number " + (count -i).ToString());
+			ContainerController.ContainerDescriptor containerDescriptor = containerController.ContainerDescriptors[i];
+			containerDescriptor.IsOpenable = EditorGUILayout.Toggle("isOpenable", containerDescriptor.IsOpenable);
+			containerDescriptor.IsStorage = EditorGUILayout.Toggle("isStorage", containerDescriptor.IsStorage);
+			containerDescriptor.HasContainerType = (ContainerType) EditorGUILayout.EnumPopup(containerDescriptor.HasContainerType);
 
-			containerController.addedContainer = !containerController.addedContainer;
-			Debug.Log("add container clicked");
+			containerDescriptor.AttachedContainerGenerator.Size = EditorGUILayout.Vector2IntField("Size", containerDescriptor.AttachedContainerGenerator.Size);
+			containerDescriptor.AttachedContainerGenerator.StartFilter = (Filter) EditorGUILayout.ObjectField("Filter",containerDescriptor.AttachedContainerGenerator.StartFilter, typeof(Filter), true);
 
-			if (!containerController.hasAttachedContainer)
+			containerDescriptor.AttachedContainer.HideItems = EditorGUILayout.Toggle("Hide Items", containerDescriptor.AttachedContainer.HideItems);
+			containerDescriptor.AttachedContainer.AttachItems = EditorGUILayout.Toggle("Attach Items", containerDescriptor.AttachedContainer.AttachItems);
+
+			if (GUILayout.Button("Remove Container", GUILayout.Width(250)))
 			{
-				containerController.AddBaseComponent();
-			}
-			else
-			{
-				containerController.RemoveBaseComponent();
+				containerController.RemoveContainer(containerDescriptor.AttachedContainer);
 			}
 		}
 
-		if (containerController.hasAttachedContainer)
-        {
-			containerController.isOpenable = EditorGUILayout.Toggle("isOpenable", containerController.isOpenable);
+
+		if (GUILayout.Button("Remove All Containers", GUILayout.Width(250)))
+		{
+			containerController.RemoveAllContainers();
 		}
 
-		if (containerController.isOpenable)
-        {
-			Debug.Log("container is openable");
-			if (!containerController.hasOpenableContainer)
-            {
-				containerController.AddOpenableContainer();
-			}
-        }
-        else
-        {
-			if (containerController.hasOpenableContainer)
-			{
-				containerController.RemoveOpenableContainer();
-			}
-		}
 
-		
 	}
+
+	
 }
