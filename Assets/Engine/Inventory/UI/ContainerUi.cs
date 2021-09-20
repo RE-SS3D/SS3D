92cc94aceb7733ea6a95c5cef3596acc9d90fcf9
stilnat <nathan.raynal.castang@gmail.com>
2021-09-20 10:10:18 +0200

    displays in text the substances inside containers

--- Assets/Engine/Inventory/UI/ContainerUi.cs
+++ Assets/Engine/Inventory/UI/ContainerUi.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using UnityEngine;
 using UnityEngine.UI;
+using SS3D.Content.Systems.Substances;
+using SS3D.Engine.Substances;
 
 namespace SS3D.Engine.Inventory.UI
 {
@@ -27,6 +29,7 @@ namespace SS3D.Engine.Inventory.UI
 
         private AttachedContainer attachedContainer;
         public Text containerName;
+        public Text substanceDisplay;
 
         public void Close()
         {
@@ -48,16 +51,43 @@ namespace SS3D.Engine.Inventory.UI
             Vector2 gridDimensions = Grid.GetGridDimensions();
             float width = rectTransform.offsetMin.x + Math.Abs(rectTransform.offsetMax.x) + gridDimensions.x + 1;
             float height = rectTransform.offsetMin.y + Math.Abs(rectTransform.offsetMax.y) + gridDimensions.y;
+            float gridHeight = height;
+            if (AttachedContainer.containerDescriptor.canHoldSubstances)
+            {
+                height += AttachedContainer.containerDescriptor.substanceContainer.Substances.Count * 40;
+            }
             var rect = transform.GetChild(0).GetComponent<RectTransform>();
             rect.sizeDelta = new Vector2(width, height);
 
             // Set the text inside the containerUI to be the name of the container
             containerName.text = attachedContainer.GetName();
+            
 
             // Position the text correctly inside the UI.
             Vector3[] v = new Vector3[4];
             rect.GetLocalCorners(v); 
             containerName.transform.localPosition = v[1] + new Vector3(0.03f * width, -0.02f * height, 0);
+
+            DisplaySubstances();
+            substanceDisplay.transform.localPosition = v[1] + new Vector3(0.04f * width, -gridHeight - 0.02f * height, 0); ;
+            
+
+        }
+
+        private void DisplaySubstances()
+        {
+            if (AttachedContainer.containerDescriptor.canHoldSubstances)
+            {
+                substanceDisplay.text = "";
+                foreach (SubstanceEntry substance in AttachedContainer.containerDescriptor.substanceContainer.Substances)
+                {
+                    substanceDisplay.text += substance.Substance.name + ": " + substance.Moles * substance.Substance.MillilitersPerMole;
+                }
+            }
+            else
+            {
+                substanceDisplay.text = "";
+            }
         }
     }
 }
