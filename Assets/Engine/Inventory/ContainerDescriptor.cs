38ab43570d48325b98f77d79121526ed11ff876b
stilnat <nathan.raynal.castang@gmail.com>
2021-09-26 01:41:30 +0200

    Fix a bug where the client can't grab items
    The hand containers were not updating the list of observers on them since they had no UI. The condition hasUi is removed so the list is correctly observed.

--- Assets/Engine/Inventory/ContainerDescriptor.cs
+++ Assets/Engine/Inventory/ContainerDescriptor.cs
@@ -114,11 +114,8 @@ namespace SS3D.Engine.Inventory
 
         public void Update()
         {
-            // if the container has an UI
-            if(hasUi)
-            {
+                // this should be executed only for some kind of containers, not sure which one. It needs some conditions.
                 UpdateObservers();
-            }
         }
 
         private void UpdateObservers()
