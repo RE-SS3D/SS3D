c63d426ca5a522428aed4773be2a855685b9ccf1
EikoBiko <53350053+EikoBiko@users.noreply.github.com>
2022-03-30 16:44:29 -0400

    Adds 14 types of airlocks (#887)
    Does what the title says!
    
    Co-authored-by: CosmicCoincidence <cosmiccoincidence1991@gmail.com>

---
+++ Assets/Engine/Utilities/MaterialColorer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MaterialColorer : MonoBehaviour
+{
+    private string state = "black";
+    private Color currentColor;
+    [Tooltip("A list of objects that will be recolored when the event is called.")]
+    public MeshRenderer[] objectsToRecolor;
+    [Tooltip("Which material slot in the Mesh Renderer should be recolored?")]
+    public int materialIndex = 0;
+    
+    public void ChangeMaterialState(string changeStateTo)
+    {
+        switch (changeStateTo)
+        {
+            case "off":
+            case "black":
+            case "null":
+            case "no power":
+            case "neutral":
+            case "default":
+                currentColor = Color.black;
+                break;
+            case "on":
+            case "green":
+            case "approve":
+            case "accept":
+            case "positive":
+            case "yes":
+                currentColor = new Color(.07f, 1f, .32f);
+                break;
+            case "deny":
+            case "red":
+            case "error":
+            case "negative":
+            case "no":
+                currentColor = new Color(1, 0.18f, .2f);
+                break;
+        }
+        ChangeColors(currentColor);
+    }
+
+    private void ChangeColors(Color newColor)
+    {
+        foreach (var item in objectsToRecolor)
+        {
+            item.materials[materialIndex].color = newColor;
+        }
+    }
+}
