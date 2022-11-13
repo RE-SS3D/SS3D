5589eee22c787c041f8d42835008651ffacac7cf
TaylorNAlbarnaz <veloxgods@gmail.com>
2022-11-13 11:26:20 -0300

    Added Nuke Card

---
+++ Assets/Scripts/SS3D/Systems/Items/NukeCard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FishNet.Object;
+using SS3D.Interactions;
+using SS3D.Interactions.Interfaces;
+using SS3D.Systems.Storage.Items;
+using UnityEngine;
+
+namespace SS3D.Systems.Items
+{
+    /// <summary>
+    /// The syndies love to use this to blow up the place
+    /// </summary>
+    public class NukeCard : Item
+    {        
+        public override IInteraction[] CreateTargetInteractions(InteractionEvent interactionEvent)
+        {
+            List<IInteraction> interactions = base.CreateTargetInteractions(interactionEvent).ToList();
+
+            return interactions.ToArray();
+        }
+    }
+}
