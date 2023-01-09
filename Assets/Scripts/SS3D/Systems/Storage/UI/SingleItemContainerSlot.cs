740406564beab3af984a7d7a4df032aa9d7c8832
stilnat <nathan.raynal.castang@gmail.com>
2023-01-09 19:32:43 +0100

    Add some comment to the container UI system to make it a bit less obscur (#967)

--- Assets/Scripts/SS3D/Systems/Storage/UI/SingleItemContainerSlot.cs
+++ Assets/Scripts/SS3D/Systems/Storage/UI/SingleItemContainerSlot.cs
@@ -10,11 +10,17 @@ using UnityEngine.EventSystems;
 namespace SS3D.Systems.Storage.UI
 {
     /// <summary>
-    /// A ui element to modify a container that contains one item
+    /// SingleItemContainerSlot allows displaying the content of a container that contain a single item in the UI.
+    /// It handles updating the inventory when an item is dropped on it, and it changes the displayed sprite accordingly.
+    /// As of now, it's only use is displaying the content of the containers on the hands of the player in the UI slots.
     /// </summary>
     public class SingleItemContainerSlot : InventoryDisplayElement, IPointerClickHandler, ISlotProvider
     {
         public ItemDisplay ItemDisplay;
+
+        /// <summary>
+        /// The container displayed by this slot.
+        /// </summary>
         private AttachedContainer _container;
 
         public AttachedContainer Container
@@ -32,6 +38,11 @@ namespace SS3D.Systems.Storage.UI
             }
         }
         
+        /// <summary>
+        /// When dragging and dropping an item sprite over this slot, update the inventory
+        /// and the displayed sprite inside the slot.
+        /// Does nothing if the slot already has an item.
+        /// </summary>
         public override void OnItemDisplayDrop(ItemDisplay display)
         {
             if (!_container.Container.Empty)
@@ -44,11 +55,17 @@ namespace SS3D.Systems.Storage.UI
             Inventory.ClientTransferItem(ItemDisplay.Item, Vector2Int.zero, Container);
         }
 
+        /// <summary>
+        /// Change the displayed sprite inside the slot.
+        /// </summary>
         private void UpdateDisplay()
         {
             ItemDisplay.Item = _container.Container.Items.FirstOrDefault();
         }
 
+        /// <summary>
+        /// UpdateContainer modify the container that this slot display, replacing the old one with newContainer.
+        /// </summary>
         private void UpdateContainer(AttachedContainer newContainer)
         {
             if (_container == newContainer)
