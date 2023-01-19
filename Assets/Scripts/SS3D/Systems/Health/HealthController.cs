32820c40cfec5ba11cf576d3dcb67b6df3f1f861
stilnat <nathan.raynal.castang@gmail.com>
2023-01-19 22:11:02 +0100

    Maek working Ghosts

--- Assets/Scripts/SS3D/Systems/Health/HealthController.cs
+++ Assets/Scripts/SS3D/Systems/Health/HealthController.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using FishNet.Object;
 using SS3D.Systems.Entities;
+using SS3D.Systems.Entities.Humanoid;
+using Coimbra;
+using SS3D.Systems.Health;
+using FishNet.Connection;
+using SS3D.Core;
 
 public class HealthController : NetworkBehaviour
 {
@@ -13,26 +18,34 @@ public class HealthController : NetworkBehaviour
     private void BecomeGhost(GameObject player, GameObject ghost)
     {
         Entity originEntity = player.GetComponent<Entity>();
-        Entity targetEntity = ghost.GetComponent<Entity>();
+        Entity ghostEntity = ghost.GetComponent<Entity>();
 
         Mind originMind = originEntity.Mind;
 
-        targetEntity.SetMind(originMind);
-        originEntity.SetMind(Mind.Empty);
+        ghostEntity.SetMind(originMind);
+        destroyObjects(originEntity, ghostEntity);
     }
 
+    [ObserversRpc]
+    private void destroyObjects(Entity originEntity, Entity ghostEntity)
+    {
+        originEntity.gameObject.GetComponent<StaminaController>().Destroy();
+        originEntity.gameObject.GetComponent<HumanoidController>().Destroy();
+        originEntity.gameObject.transform.Rotate(new Vector3(90, 0, 0));
+        ghostEntity.transform.position = originEntity.transform.position;
+        ghostEntity.transform.rotation = originEntity.transform.rotation;
+    }
+
+    [Client]
     public void ClientKill()
     {
-        CmdKill(Ghost, transform);
+        CmdKill(Ghost);
     }
 
-    [ServerRpc]
-    public void CmdKill(GameObject obj, Transform playerTransform)
+    [ServerRpc(RequireOwnership = false)]
+    public void CmdKill(GameObject obj)
     {
         GameObject ghost = Instantiate(obj);
-        gameObject.transform.Rotate(new Vector3(90, 0, 0));
-        ghost.transform.position = playerTransform.position;
-        ghost.transform.rotation = playerTransform.rotation;
         ServerManager.Spawn(ghost);
         BecomeGhost(gameObject, ghost);
     }
