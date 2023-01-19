4c52399cc33f2e7a508c7e33e6ae7d4be0c74233
stilnat <nathan.raynal.castang@gmail.com>
2023-01-19 13:42:55 +0100

    Add base health controller
    Basically can just kill player for now.

---
+++ Assets/Scripts/SS3D/Systems/Health/HealthController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FishNet.Object;
+using SS3D.Systems.Entities;
+
+public class HealthController : NetworkBehaviour
+{
+
+    public GameObject Ghost;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            ClientKill();
+        }
+    }
+
+    [Server]
+    private void BecomeGhost(GameObject player, GameObject ghost)
+    {
+        Entity originEntity = player.GetComponent<Entity>();
+        Entity targetEntity = ghost.GetComponent<Entity>();
+
+        Mind originMind = originEntity.Mind;
+
+        targetEntity.SetMind(originMind);
+        originEntity.SetMind(Mind.Empty);
+    }
+
+    public void ClientKill()
+    {
+        CmdKill(Ghost, transform);
+    }
+
+    [ServerRpc]
+    public void CmdKill(GameObject obj, Transform playerTransform)
+    {
+        GameObject ghost = Instantiate(obj);
+        gameObject.transform.Rotate(new Vector3(90, 0, 0));
+        ghost.transform.position = playerTransform.position;
+        ghost.transform.rotation = playerTransform.rotation;
+        ServerManager.Spawn(ghost);
+        BecomeGhost(gameObject, ghost);
+    }
+}
+
