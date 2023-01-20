3e0995c9c1d7de988091457333c82c63a12ccb92
TaylorNAlbarnaz <veloxgods@gmail.com>
2023-01-20 09:47:23 -0300

    Security Locker and Security ID for testing

--- Assets/Scripts/SS3D/Systems/Furniture/Locker.cs
+++ Assets/Scripts/SS3D/Systems/Furniture/Locker.cs
@@ -15,31 +15,6 @@ namespace SS3D.Systems.Furniture
     {
         public IDPermission permissionToOpen;
         public bool Locked { private set; get; } = true;
-        private bool Open = false;
-
-        public bool CanOpenDoor()
-        {
-            return (!Locked && !Open);
-        }
-
-        public bool CanCloseDoor()
-        {
-            return (!Locked && Open);
-        }
-
-        public bool OpenDoor()
-        {
-            Debug.Log("Opened!");
-            Open = true;
-            return true;
-        }
-
-        public bool CloseDoor()
-        {
-            Debug.Log("Closed!");
-            Open = false;
-            return true;
-        }
 
         public void SetLocked(bool locked)
         {
@@ -48,14 +23,6 @@ namespace SS3D.Systems.Furniture
 
         IInteraction[] IInteractionTarget.CreateTargetInteractions(InteractionEvent interactionEvent)
         {
-            SimpleInteraction openInteraction =
-                new SimpleInteraction("Open", AssetData.Get(InteractionIcons.Open),
-                    CanOpenDoor, OpenDoor);
-
-            SimpleInteraction closeInteraction =
-                new SimpleInteraction("Close", AssetData.Get(InteractionIcons.Open),
-                    CanCloseDoor, CloseDoor);
-
             UnlockLockerInteraction unlockLockerInteraction = new UnlockLockerInteraction();
             unlockLockerInteraction.permissionToOpen = permissionToOpen;
             unlockLockerInteraction.locker = this;
@@ -64,8 +31,7 @@ namespace SS3D.Systems.Furniture
             lockLockerInteraction.permissionToOpen = permissionToOpen;
             lockLockerInteraction.locker = this;
 
-            return new IInteraction[] { openInteraction, closeInteraction,
-                unlockLockerInteraction, lockLockerInteraction };
+            return new IInteraction[] { unlockLockerInteraction, lockLockerInteraction };
         }
     }
 }
