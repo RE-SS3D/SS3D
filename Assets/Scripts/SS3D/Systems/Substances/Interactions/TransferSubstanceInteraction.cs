167afa7e5aaf3f41688d37651a3798a6ee6fc71d
stilnat <nathan.raynal.castang@gmail.com>
2023-01-23 11:42:26 +0100

    Add substances back

---
+++ Assets/Scripts/SS3D/Systems/Substances/Interactions/TransferSubstanceInteraction.cs
@@ -0,0 +1,93 @@
+using SS3D.Interactions.Extensions;
+using SS3D.Interactions.Interfaces;
+using SS3D.Interactions;
+using SS3D.Substances;
+using System;
+using UnityEngine;
+
+namespace SS3D.Substances
+{
+    public class TransferSubstanceInteraction : Interaction
+    {
+        /// <summary>
+        /// Checks if the interaction should be possible
+        /// </summary>
+        public Predicate<InteractionEvent> CanInteractCallback { get; set; } = _ => true;
+
+        public override IClientInteraction CreateClient(InteractionEvent interactionEvent)
+        {
+            return null;
+        }
+
+        public override string GetName(InteractionEvent interactionEvent)
+        {
+            return "Transfer";
+        }
+
+        public override Sprite GetIcon(InteractionEvent interactionEvent)
+        {
+            return null;
+        }
+
+        public override bool CanInteract(InteractionEvent interactionEvent)
+        {
+            if (!InteractionExtensions.RangeCheck(interactionEvent))
+            {
+                return false;
+            }
+
+            var provider = interactionEvent.Source as IGameObjectProvider;
+            if (provider == null)
+            {
+                return false;
+            }
+
+            var container = provider.GameObject.GetComponent<SubstanceContainer>();
+            if (container == null)
+            {
+                return false;
+            }
+
+            if (container.Locked)
+            {
+                return false;
+            }
+
+            if (container.IsEmpty())
+            {
+                return false;
+            }
+
+            return CanInteractCallback.Invoke(interactionEvent);
+        }
+
+        public override bool Start(InteractionEvent interactionEvent, InteractionReference reference)
+        {
+            if (interactionEvent.Source is IGameObjectProvider provider)
+            {
+                SubstanceContainer container = provider.GameObject.GetComponent<SubstanceContainer>();
+                if (container != null)
+                {
+                    var targetContainer = interactionEvent.Target.GetComponent<SubstanceContainer>();
+                    container.TransferVolume(targetContainer, 25);
+                    container.MarkDirty();
+                    targetContainer.MarkDirty();
+                }
+            }
+
+            return false;
+        }
+
+        public override bool Update(InteractionEvent interactionEvent, InteractionReference reference)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Cancel(InteractionEvent interactionEvent, InteractionReference reference)
+        {
+            throw new System.NotImplementedException();
+        }
+
+       
+    }
+}
