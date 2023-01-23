7152818e305a3ddb6a8a2533a8e1d3af59a7bd2d
stilnat <nathan.raynal.castang@gmail.com>
2023-01-24 00:11:49 +0100

    Make Biological layer inherit from body layer

--- Assets/Scripts/SS3D/Systems/Health/BiologicalLayer.cs
+++ Assets/Scripts/SS3D/Systems/Health/BiologicalLayer.cs
@@ -1,77 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
+using System;
 
-public abstract class BodyLayer
+/// <summary>
+/// Biological layers take damage if not enough oxygen is provided.
+/// </summary>
+public abstract class BiologicalLayer : BodyLayer
 {
-    public List<DamageTypeQuantity> DamageTypeQuantities;
-    public float TotalDamage => DamageTypeQuantities.Sum(x => x.quantity);
+    public abstract float OxygenConsumptionRate { get; }
 
+    public BiologicalLayer(BodyPart bodyPart) : base(bodyPart)
+    {
+
+    }
     /// <summary>
-    /// Minimum amount of damage to do to make any actual damage.
+    /// Get oxygen from CirculatoryLayer if there's one. 
+    /// If not enough oxygen is present, inflict OXY type damage to the layer.
     /// </summary>
-    public List<DamageTypeQuantity> resistance;
-
-    /// <summary>
-    /// Add damage without going above 100 for any given type.
-    /// </summary>
-    /// <param name="damageQuantity"></param>
-    public void InflictDamage(DamageTypeQuantity damageQuantity)
+    public virtual void ConsumeOxygen()
     {
-        int damageTypeIndex = DamageTypeQuantities.FindIndex(x => x.damageType == damageQuantity.damageType);
-        if (damageTypeIndex == -1)
-        {
-            if (damageQuantity.quantity > 100)
-            {
-                damageQuantity.quantity = 100;
-            }
-            DamageTypeQuantities.Add(damageQuantity);
-        }
-        else
-        {
-            float newDamageQuantity = damageQuantity.quantity + DamageTypeQuantities[damageTypeIndex].quantity;
-            if (newDamageQuantity > 100)
-            {
-                DamageTypeQuantities[damageTypeIndex].quantity = 100;
-            }
-            else
-            {
-                DamageTypeQuantities[damageTypeIndex].quantity = newDamageQuantity;
-            }
-        }
+        throw new NotImplementedException();
     }
-
-    public void HealDamage(DamageTypeQuantity damageQuantity)
-    {
-        int damageTypeIndex = DamageTypeQuantities.FindIndex(x => x.damageType == damageQuantity.damageType);
-        if (damageTypeIndex == -1)
-        {
-            return;
-        }
-        else
-        {
-            float newDamageQuantity = DamageTypeQuantities[damageTypeIndex].quantity - damageQuantity.quantity;
-            if (newDamageQuantity < 0)
-            {
-                DamageTypeQuantities[damageTypeIndex].quantity = 0;
-            }
-            else
-            {
-                DamageTypeQuantities[damageTypeIndex].quantity = newDamageQuantity;
-            }
-        }
-    }
-
-    public float GetDamageTypeQuantity(DamageType damageType)
-    {
-        int damageTypeIndex = DamageTypeQuantities.FindIndex(x => x.damageType == damageType);
-        return damageTypeIndex == -1 ? 0 : DamageTypeQuantities[damageTypeIndex].quantity;
-    }
-
-    public bool IsDestroyed()
-    {
-        return TotalDamage >= 100;
-    }
-
 }
