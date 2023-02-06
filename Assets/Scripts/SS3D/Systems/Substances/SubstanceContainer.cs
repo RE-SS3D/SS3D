62dace28242ec9576a7c01aa7e39860e6e2f9807
stilnat <nathan.raynal.castang@gmail.com>
2023-02-06 17:55:27 +0100

    adds back processcontainer method

--- Assets/Scripts/SS3D/Systems/Substances/SubstanceContainer.cs
+++ Assets/Scripts/SS3D/Systems/Substances/SubstanceContainer.cs
@@ -340,5 +340,114 @@ namespace SS3D.Substances
                 new TransferSubstanceInteraction()
             };
         }
+
+        /// <summary>
+        /// Processes substances in a container
+        /// </summary>
+        public void ProcessContainer(SubstanceContainer container)
+        {
+            var registry = SystemLocator.Get<SubstancesSystem>();
+            float temperature = container.Temperature;
+
+            // Process recipes
+            // TODO : Highly inefficient as most recipes won't be achievable given the substances in the substance container.
+            // Instead, find a good data structure able to search through a small set of recipes, based on number of 
+            // ingredients or even ingredient types.
+            foreach (Recipe recipe in registry.Recipes)
+            {
+                // Check temperature limits
+                if (temperature < recipe.MinimalTemperature || temperature > recipe.MaximalTemperature)
+                {
+                    continue;
+                }
+
+                // Gather the mole amount of every substance
+                float[] moles = new float[recipe.Ingredients.Length];
+                bool ingredientsPresent = true;
+                for (var i = 0; i < recipe.Ingredients.Length; i++)
+                {
+                    ingredientsPresent = false;
+                    foreach (var entry in container.Substances)
+                    {
+                        if (entry.Substance.Type == recipe.Ingredients[i].Type)
+                        {
+                            moles[i] = entry.Moles;
+                            ingredientsPresent = true;
+                            break;
+                        }
+                    }
+
+                    if (!ingredientsPresent)
+                    {
+                        break;
+                    }
+                }
+
+                // Substance missing
+                if (!ingredientsPresent)
+                {
+                    continue;
+                }
+
+                // Calculate the maximum amount of ingredients
+                float totalIngredients = recipe.Ingredients.Sum(x => x.RelativeAmount);
+                float maxConversion = float.MaxValue;
+                for (var i = 0; i < moles.Length; i++)
+                {
+                    float relativeAmount = recipe.Ingredients[i].RelativeAmount;
+                    float part = relativeAmount / totalIngredients;
+                    float maxProduced = moles[i] / part;
+                    if (maxProduced < maxConversion)
+                    {
+                        maxConversion = maxProduced;
+                    }
+                }
+
+                // Calculate relative volume of ingredients
+                Substance[] ingredientSubstances = new Substance[moles.Length];
+                float ingredientsToVolume = 0;
+                for (var i = 0; i < moles.Length; i++)
+                {
+                    var component = recipe.Ingredients[i];
+                    var substance = ingredientSubstances[i] = registry.FromType(component.Type);
+                    ingredientsToVolume += substance.MillilitersPerMole * component.RelativeAmount;
+                }
+                ingredientsToVolume /= totalIngredients;
+
+                // Calculate relative volume of results
+                float totalResults = recipe.Results.Sum(x => x.RelativeAmount);
+                Substance[] resultSubstances = new Substance[recipe.Results.Length];
+                float resultsToVolume = 0;
+                for (var i = 0; i < recipe.Results.Length; i++)
+                {
+                    var component = recipe.Results[i];
+                    var substance = resultSubstances[i] = registry.FromType(component.Type);
+                    resultsToVolume += substance.MillilitersPerMole * component.RelativeAmount;
+                }
+                resultsToVolume /= totalResults;
+
+                // Limit the reaction to prevent spill
+                // TODO: Spill container into surroundings
+                float remainingVolume = container.RemainingVolume;
+                if (maxConversion * totalResults * resultsToVolume - maxConversion * ingredientsToVolume > remainingVolume)
+                {
+                    maxConversion = remainingVolume / resultsToVolume / totalResults;
+                }
+
+                // Remove ingredients
+                for (var i = 0; i < moles.Length; i++)
+                {
+                    container.RemoveSubstance(ingredientSubstances[i], maxConversion * (recipe.Ingredients[i].RelativeAmount / totalIngredients));
+                }
+
+                // Add results
+                foreach (var component in recipe.Results)
+                {
+                    container.AddSubstance(registry.FromType(component.Type), maxConversion * (component.RelativeAmount / totalResults));
+                }
+
+                container.MarkDirty();
+            }
+        }
     }
 }
