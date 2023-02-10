a55edcb4239845149ba091bbf824dc02b0cf4c39
stilnat <nathan.raynal.castang@gmail.com>
2023-02-10 02:50:10 +0100

    Add substances back (#986)
    * Add substances back
    
    * Adds some substances assets
    
    * Adds substance display script
    
    * Adds substanceDispenser script
    
    * Adds dispense substance interaction
    
    * Modify susbtance dispenser to fit the rework
    
    * Update substancedispenseInteraction to fit new rework
    
    * Replace string by enum, change list by dictionnary for performance.
    
    * add content for a mug and booze dispenser
    
    * Edit substances scriptable objects so their type match
    
    * Replace substanceregistry with substancesSystem
    
    * Remaining volume should be a simple property not a syncVar
    
    * update substance dispenser so it gets substance system instead of registry
    
    * adds a mug and a booze dispenser
    
    * some updates on mug and dispenser prefabs
    
    * adds back processcontainer method
    
    * add a second mug
    
    * implement cancel and update method for dispense and transfer interactions
    
    * remove a bunch of currently useless assets and the old substance registry prefab
    
    * rename Palette05Emission
    
    * rename Functionial folder to Reagent
    
    * remove some mesh file
    
    * remove some mesh file
    
    * move boozeDispenser into right folder
    
    * moves mugs.fbx and mugs.prefab to the right folders
    
    * move Palette files to world folder
    
    * removes some more useless stuff for now
    
    * Add some folders
    
    * delete reagent folder

---
+++ Assets/Scripts/SS3D/Systems/Substances/SubstanceDispenser.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using SS3D.Interactions;
+using SS3D.Substances;
+using SS3D.Interactions.Interfaces;
+using UnityEngine;
+using SS3D.Core;
+using SS3D.Systems.Screens;
+using static UnityEngine.GraphicsBuffer;
+
+namespace SS3D.Content.Furniture.Generic
+{
+    // This handles dispensing substances into substance containers
+    public class SubstanceDispenser : InteractionTargetBehaviour
+    {
+        /// <summary>
+        /// The name of the interaction
+        /// </summary>
+        public string InteractionName;
+        /// <summary>
+        /// What should be dispensed
+        /// </summary>
+        public SubstanceType[] substances;
+        /// <summary>
+        /// How much should be dispensed
+        /// </summary>
+        public float amount;
+
+        /// <summary>
+        /// Whether the amount is in milliliters, or moles.
+        /// </summary>
+        public bool useMillilitres = false;
+
+        private SubstancesSystem registry;
+
+        private void Start()
+        {
+            registry = SystemLocator.Get<SubstancesSystem>();
+            if (registry == null)
+            {
+                Debug.LogError("SubstanceRegistry not found. Substances will be disabled.");
+            }
+        }
+
+        public override IInteraction[] CreateTargetInteractions(InteractionEvent interactionEvent)
+        {
+            if (registry == null) return null;
+
+            // Create a separate interaction for each possible substance to dispense
+            IInteraction[] interactions = new IInteraction[substances.Length];
+            for (int i = 0; i <substances.Length; i++)
+            {
+
+                // Retrieve substance from the Registry
+                Substance substance = registry.FromType(substances[i]);
+
+                // Ensure the substance was successfully retrieved.
+                if (substance == null)
+                {
+                    // If it isn't, let them know what it is!
+                    Debug.LogWarning("No substance in Registry for " + substances[i] + ". Add it.");
+                    return null;
+                }
+
+                // Determine how many moles to dispense
+                float moles;
+                if (useMillilitres)
+                {
+                    moles = amount / substance.MillilitersPerMole;
+                }
+                else
+                {
+                    moles = amount;
+                }
+
+                // Add the specific dispence interaction to the list.
+                interactions[i] = new DispenseSubstanceInteraction
+                {
+                    RangeCheck = true,
+                    Substance = new SubstanceEntry(substance, moles),
+                    Name = String.IsNullOrWhiteSpace(InteractionName) ? "Fill with " + substances[i] : InteractionName + " " + substances[i]
+                };
+
+            }
+
+            return interactions;
+        }
+    }
+}
