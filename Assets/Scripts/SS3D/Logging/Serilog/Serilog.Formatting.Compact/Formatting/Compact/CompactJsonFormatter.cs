25f37e8cb1943b96799e990b83a725a834ef39bf
stilnat <nathan.raynal.castang@gmail.com>
2023-02-19 17:01:34 +0100

    Add serilog compact formatting

---
+++ Assets/Scripts/SS3D/Logging/Serilog/Serilog.Formatting.Compact/Formatting/Compact/CompactJsonFormatter.cs
@@ -0,0 +1,121 @@
+﻿// Copyright 2016 Serilog Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.IO;
+using System.Linq;
+using Serilog.Events;
+using Serilog.Formatting.Json;
+using Serilog.Parsing;
+
+namespace Serilog.Formatting.Compact
+{
+    /// <summary>
+    /// An <see cref="ITextFormatter"/> that writes events in a compact JSON format.
+    /// </summary>
+    public class CompactJsonFormatter: ITextFormatter
+    {
+        readonly JsonValueFormatter _valueFormatter;
+
+        /// <summary>
+        /// Construct a <see cref="CompactJsonFormatter"/>, optionally supplying a formatter for
+        /// <see cref="LogEventPropertyValue"/>s on the event.
+        /// </summary>
+        /// <param name="valueFormatter">A value formatter, or null.</param>
+        public CompactJsonFormatter(JsonValueFormatter valueFormatter = null)
+        {
+            _valueFormatter = valueFormatter ?? new JsonValueFormatter(typeTagName: "$type");
+        }
+
+        /// <summary>
+        /// Format the log event into the output. Subsequent events will be newline-delimited.
+        /// </summary>
+        /// <param name="logEvent">The event to format.</param>
+        /// <param name="output">The output.</param>
+        public void Format(LogEvent logEvent, TextWriter output)
+        {
+            FormatEvent(logEvent, output, _valueFormatter);
+            output.WriteLine();
+        }
+
+        /// <summary>
+        /// Format the log event into the output.
+        /// </summary>
+        /// <param name="logEvent">The event to format.</param>
+        /// <param name="output">The output.</param>
+        /// <param name="valueFormatter">A value formatter for <see cref="LogEventPropertyValue"/>s on the event.</param>
+        public static void FormatEvent(LogEvent logEvent, TextWriter output, JsonValueFormatter valueFormatter)
+        {
+            if (logEvent == null) throw new ArgumentNullException(nameof(logEvent));
+            if (output == null) throw new ArgumentNullException(nameof(output));
+            if (valueFormatter == null) throw new ArgumentNullException(nameof(valueFormatter));
+
+            output.Write("{\"@t\":\"");
+            output.Write(logEvent.Timestamp.UtcDateTime.ToString("O"));
+            output.Write("\",\"@mt\":");
+            JsonValueFormatter.WriteQuotedJsonString(logEvent.MessageTemplate.Text, output);
+
+            var tokensWithFormat = logEvent.MessageTemplate.Tokens
+                .OfType<PropertyToken>()
+                .Where(pt => pt.Format != null);
+
+            // Better not to allocate an array in the 99.9% of cases where this is false
+            // ReSharper disable once PossibleMultipleEnumeration
+            if (tokensWithFormat.Any())
+            {
+                output.Write(",\"@r\":[");
+                var delim = "";
+                foreach (var r in tokensWithFormat)
+                {
+                    output.Write(delim);
+                    delim = ",";
+                    var space = new StringWriter();
+                    r.Render(logEvent.Properties, space);
+                    JsonValueFormatter.WriteQuotedJsonString(space.ToString(), output);
+                }
+                output.Write(']');
+            }
+
+            if (logEvent.Level != LogEventLevel.Information)
+            {
+                output.Write(",\"@l\":\"");
+                output.Write(logEvent.Level);
+                output.Write('\"');
+            }
+
+            if (logEvent.Exception != null)
+            {
+                output.Write(",\"@x\":");
+                JsonValueFormatter.WriteQuotedJsonString(logEvent.Exception.ToString(), output);
+            }
+
+            foreach (var property in logEvent.Properties)
+            {
+                var name = property.Key;
+                if (name.Length > 0 && name[0] == '@')
+                {
+                    // Escape first '@' by doubling
+                    name = '@' + name;
+                }
+
+                output.Write(',');
+                JsonValueFormatter.WriteQuotedJsonString(name, output);
+                output.Write(':');
+                valueFormatter.Format(property.Value, output);
+            }
+
+            output.Write('}');
+        }
+    }
+}
---
+++ Assets/Scripts/SS3D/Logging/Serilog/Serilog.Formatting.Compact/Formatting/Compact/RenderedCompactJsonFormatter.cs
@@ -0,0 +1,104 @@
+﻿// Copyright 2016 Serilog Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.IO;
+using Serilog.Events;
+using Serilog.Formatting.Json;
+
+namespace Serilog.Formatting.Compact
+{
+    /// <summary>
+    /// An <see cref="ITextFormatter"/> that writes events in a compact JSON format, for consumption in environments 
+    /// without message template support. Message templates are rendered into text and a hashed event id is included.
+    /// </summary>
+    public class RenderedCompactJsonFormatter : ITextFormatter
+    {
+        readonly JsonValueFormatter _valueFormatter;
+
+        /// <summary>
+        /// Construct a <see cref="CompactJsonFormatter"/>, optionally supplying a formatter for
+        /// <see cref="LogEventPropertyValue"/>s on the event.
+        /// </summary>
+        /// <param name="valueFormatter">A value formatter, or null.</param>
+        public RenderedCompactJsonFormatter(JsonValueFormatter valueFormatter = null)
+        {
+            _valueFormatter = valueFormatter ?? new JsonValueFormatter(typeTagName: "$type");
+        }
+
+        /// <summary>
+        /// Format the log event into the output. Subsequent events will be newline-delimited.
+        /// </summary>
+        /// <param name="logEvent">The event to format.</param>
+        /// <param name="output">The output.</param>
+        public void Format(LogEvent logEvent, TextWriter output)
+        {
+            FormatEvent(logEvent, output, _valueFormatter);
+            output.WriteLine();
+        }
+
+        /// <summary>
+        /// Format the log event into the output.
+        /// </summary>
+        /// <param name="logEvent">The event to format.</param>
+        /// <param name="output">The output.</param>
+        /// <param name="valueFormatter">A value formatter for <see cref="LogEventPropertyValue"/>s on the event.</param>
+        public static void FormatEvent(LogEvent logEvent, TextWriter output, JsonValueFormatter valueFormatter)
+        {
+            if (logEvent == null) throw new ArgumentNullException(nameof(logEvent));
+            if (output == null) throw new ArgumentNullException(nameof(output));
+            if (valueFormatter == null) throw new ArgumentNullException(nameof(valueFormatter));
+
+            output.Write("{\"@t\":\"");
+            output.Write(logEvent.Timestamp.UtcDateTime.ToString("O"));
+            output.Write("\",\"@m\":");
+            var message = logEvent.MessageTemplate.Render(logEvent.Properties);
+            JsonValueFormatter.WriteQuotedJsonString(message, output);
+            output.Write(",\"@i\":\"");
+            var id = EventIdHash.Compute(logEvent.MessageTemplate.Text);
+            output.Write(id.ToString("x8"));
+            output.Write('"');
+
+            if (logEvent.Level != LogEventLevel.Information)
+            {
+                output.Write(",\"@l\":\"");
+                output.Write(logEvent.Level);
+                output.Write('\"');
+            }
+
+            if (logEvent.Exception != null)
+            {
+                output.Write(",\"@x\":");
+                JsonValueFormatter.WriteQuotedJsonString(logEvent.Exception.ToString(), output);
+            }
+
+            foreach (var property in logEvent.Properties)
+            {
+                var name = property.Key;
+                if (name.Length > 0 && name[0] == '@')
+                {
+                    // Escape first '@' by doubling
+                    name = '@' + name;
+                }
+
+                output.Write(',');
+                JsonValueFormatter.WriteQuotedJsonString(name, output);
+                output.Write(':');
+                valueFormatter.Format(property.Value, output);
+            }
+
+            output.Write('}');
+        }
+    }
+}
