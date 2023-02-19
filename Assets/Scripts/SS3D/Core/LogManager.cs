7c8d2b91cb3f690b38bb84ad6eb6c91670737462
stilnat <nathan.raynal.castang@gmail.com>
2023-02-19 17:00:39 +0100

    rename SSLog in LogManager

--- Assets/Scripts/SS3D/Core/LogManager.cs
+++ Assets/Scripts/SS3D/Core/LogManager.cs
@@ -6,12 +6,21 @@ using Serilog.Sinks.Unity3D;
 using SS3D.Core.Behaviours;
 using Serilog.Core;
 using Serilog.Events;
+using Serilog.Formatting.Json;
+using Serilog.Formatting.Compact;
 using UnityEngine;
+using Serilog.Configuration;
+using Serilog.Formatting;
+using Serilog.Sinks.File;
+using System.Runtime.Serialization;
+using System.Text;
+using System;
+using UnityEditor;
 
 namespace SS3D.Core
 {
     // Maybe add property for local client ?
-    public class SSLog : NetworkSystem
+    public class LogManager : NetworkSystem
     {
 
         private static LoggingLevelSwitch _levelSwitch;
@@ -21,27 +30,23 @@ namespace SS3D.Core
             _levelSwitch= new LoggingLevelSwitch();
             _levelSwitch.MinimumLevel = LogEventLevel.Warning;
         }
-
         public override void OnStartClient()
         {
+            public static LoggerConfiguration File(this LoggerSinkConfiguration sinkConfiguration, string path, LogEventLevel restrictedToMinimumLevel = LogEventLevel.Verbose, string outputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}", IFormatProvider? formatProvider = null, long? fileSizeLimitBytes = 1073741824L, LoggingLevelSwitch? levelSwitch = null, bool buffered = false, bool shared = false, TimeSpan? flushToDiskInterval = null, RollingInterval rollingInterval = RollingInterval.Infinite, bool rollOnFileSizeLimit = false, int? retainedFileCountLimit = 31, Encoding? encoding = null, FileLifecycleHooks? hooks = null, TimeSpan? retainedFileTimeLimit = null)
 
-            base.OnStartClient();
-
-            if (IsHost)
-            {
-                return;
-            }
-
-            Log.Logger = new LoggerConfiguration()
+                base.OnStartClient();
+                if (IsHost)
+                {
+                    return;
+                }
+                Log.Logger = new LoggerConfiguration()
                 .Enrich.With(new ClientIdEnricher())
                 .WriteTo.Unity3D()
-                .WriteTo.File("C:/Users/Nat/Documents/GitHub/StilnatSS3DMain/Logs/LogClient" + ClientManager.Connection.ClientId + ".txt"
-                , outputTemplate: "{Timestamp:HH:mm} [{Level}] [ID = {ClientId}] {Message}{NewLine}{Exception}"
-                , shared: true)
+                .WriteTo.File( new CompactJsonFormatter()
+                , "C:/Users/Nat/Documents/GitHub/StilnatSS3DMain/Logs/LogClient" + ClientManager.Connection.ClientId + ".txt")
                 .CreateLogger();
-
-            Log.Information("##########  CLIENT STARTING !  ##########");
-        }
+                Log.Information("##########  CLIENT STARTING !  ##########");
+            }
 
         public override void OnStartServer()
         {
@@ -49,10 +54,10 @@ namespace SS3D.Core
             if (IsServerOnly)
             {
                 Log.Logger = new LoggerConfiguration()
-                                .WriteTo.Unity3D()
-                                .WriteTo.File("C:/Users/Nat/Documents/GitHub/StilnatSS3DMain/Logs/LogServer.txt"
-                                , outputTemplate: "{Timestamp:HH:mm} [{Level}] [ID = SERVER] {Message}{NewLine}{Exception}"
-                                , shared: true)
+                .WriteTo.Unity3D()
+                .WriteTo.File("C:/Users/Nat/Documents/GitHub/StilnatSS3DMain/Logs/LogServer.txt"
+                , outputTemplate: "{Timestamp:HH:mm} [{Level}] [ID = SERVER] {Message}{NewLine}{Exception}"
+                , shared: true)
                                 .CreateLogger();
             }
             else
