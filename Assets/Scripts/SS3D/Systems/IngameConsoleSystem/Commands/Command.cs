69d0062a979bea7beb92e8974678e03d66baa152
Mechar418 <77565416+Mechar418@users.noreply.github.com>
2023-03-05 19:46:03 +0300

    Split CommandContainer into separated classes (#1064)
    * Split CommandContainer into separated classes
    
    * Add command suffix, move readme info to Command comment, improve tabulation

---
+++ Assets/Scripts/SS3D/Systems/IngameConsoleSystem/Commands/Command.cs
@@ -0,0 +1,20 @@
+﻿using SS3D.Systems.Permissions;
+
+namespace SS3D.Systems.IngameConsoleSystem.Commands
+{
+    // All commands, that inherit Command will be added to CommandsController and have to contain Command suffix
+    public abstract class Command
+    {
+        protected struct CheckArgsResponse
+        {
+            public bool IsValid;
+            public string InvalidArgs;
+        }
+        public abstract string ShortDescription { get; }
+        public abstract string LongDescription { get; }
+        public abstract ServerRoleTypes AccessLevel { get; }
+        public abstract string Perform(string[] args);
+        protected abstract CheckArgsResponse CheckArgs(string[] args);
+        protected const string WrongArgsText = "Wrong args. Type \"(command) help\"";
+    }
+}
---
+++ Assets/Scripts/SS3D/Systems/IngameConsoleSystem/Commands/ReconnectCommand.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using SS3D.Systems.Permissions;
+using UnityEngine.Device;
+
+namespace SS3D.Systems.IngameConsoleSystem.Commands
+{
+    public class ReconnectCommand: Command
+    {
+        public override string LongDescription => "Restart app";
+        public override string ShortDescription => "Restart app";
+        public override ServerRoleTypes AccessLevel => ServerRoleTypes.User;
+        public override string Perform(string[] args)
+        {
+            CheckArgsResponse checkArgsResponse = CheckArgs(args);
+            if (checkArgsResponse.IsValid == false)
+                return checkArgsResponse.InvalidArgs;
+            
+            Process.Start(Application.dataPath.Replace("_Data", ".exe"));
+            Application.Quit();
+            return "Done";
+        }
+        protected override CheckArgsResponse CheckArgs(string[] args)
+        {
+            CheckArgsResponse response = new CheckArgsResponse();
+            if (args.Length != 0)
+            {
+                response.IsValid = false;
+                response.InvalidArgs = "Invalid number of arguments";
+                return response;
+            }
+            
+            response.IsValid = true;
+            return response;
+        }
+    }
+}
