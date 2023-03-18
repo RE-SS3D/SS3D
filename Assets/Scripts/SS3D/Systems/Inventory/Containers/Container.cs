765af21b81fb8ba254bdfcecef278bb87c2eab02
João Buratto <46575243+joaoburatto@users.noreply.github.com>
2023-03-18 19:16:07 -0300

    Fix missing imports

--- Assets/Scripts/SS3D/Systems/Inventory/Containers/Container.cs
+++ Assets/Scripts/SS3D/Systems/Inventory/Containers/Container.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using FishNet.Object.Synchronizing;
-using SS3D.Core.Behaviours;
-using SS3D.Logging;
+using JetBrains.Annotations;
 using SS3D.Systems.Entities;
 using SS3D.Systems.Inventory.Items;
 using UnityEngine;
