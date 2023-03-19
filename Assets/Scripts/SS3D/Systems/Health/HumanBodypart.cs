40461ee3d159542f38fafe33fdd6df603897bda4
stilnat <nathan.raynal.castang@gmail.com>
2023-03-19 18:46:50 +0100

    change human body part constructor and add some doc

--- Assets/Scripts/SS3D/Systems/Health/HumanBodypart.cs
+++ Assets/Scripts/SS3D/Systems/Health/HumanBodypart.cs
@@ -6,23 +6,35 @@ using System.Linq;
 using Unity.Collections;
 using UnityEngine;
 
+/// <summary>
+/// Represent a generic body part, containing muscles, bones, nerves and veins.
+/// Can be used for arms, foot, legs ... Should not be used for organs.
+/// </summary>
 public class HumanBodypart : BodyPart
 {
 
-    public HumanBodypart(bool hasCentralNervousSystem) : base() 
+    public HumanBodypart(string name = "") : base(name) 
     {
         BodyLayers.Add(new MuscleLayer(this));
         BodyLayers.Add(new BoneLayer(this));
         BodyLayers.Add(new CirculatoryLayer(this));
-        BodyLayers.Add(new NerveLayer(this, hasCentralNervousSystem));
+        BodyLayers.Add(new NerveLayer(this, false));
     }
 
-    public HumanBodypart(BodyPartBehaviour bodyPartBehaviour, bool hasCentralNervousSystem) : base(bodyPartBehaviour)
+    public HumanBodypart(BodyPartBehaviour bodyPartBehaviour, string name = "") : base(bodyPartBehaviour, name)
     {
         BodyLayers.Add(new MuscleLayer(this));
         BodyLayers.Add(new BoneLayer(this));
         BodyLayers.Add(new CirculatoryLayer(this));
-        BodyLayers.Add(new NerveLayer(this, hasCentralNervousSystem));
+        BodyLayers.Add(new NerveLayer(this, false));
+    }
+
+    public HumanBodypart(BodyPart parent,string name = "") : base(parent, name)
+    {
+        BodyLayers.Add(new MuscleLayer(this));
+        BodyLayers.Add(new BoneLayer(this));
+        BodyLayers.Add(new CirculatoryLayer(this));
+        BodyLayers.Add(new NerveLayer(this, false));
     }
 
     private void OnNerveDamaged(object sender, DamageEventArgs nerveDamageEventArgs)
