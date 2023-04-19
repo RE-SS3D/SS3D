161a9374f79a466f2db64837ce01e808d0e9da6e
João Buratto <46575243+joaoburatto@users.noreply.github.com>
2023-04-19 18:18:59 -0300

    Add LauncherSystem summary

--- Assets/Scripts/SS3D/Launcher/LauncherSystem.cs
+++ Assets/Scripts/SS3D/Launcher/LauncherSystem.cs
@@ -2,14 +2,23 @@
 using SS3D.Core.Settings;
 using SS3D.Data.Enums;
 using SS3D.SceneManagement;
-using SS3D.Utils;
 using System;
 using UnityEngine;
 
 namespace SS3D.Launcher
 {
+	/// <summary>
+	/// System used to control launcher functions.
+	/// </summary>
 	public sealed class LauncherSystem : Core.Behaviours.System
 	{
+		/// <summary>
+		/// Launches the game.
+		/// </summary>
+		/// <param name="networkType">The network mode used on the game.</param>
+		/// <param name="ckey">The ckey used on the game.</param>
+		/// <param name="ip">The IP address to connect to.</param>
+		/// <param name="port">The port number to use.</param>
 		public void LaunchGame(NetworkType networkType, string ckey, string ip, string port)
 		{
 			if (!Application.isEditor)
