d6a2f6099935b0f9f028e137dbd985f974523fb8
Ryan Puth <64360820+Ryan089@users.noreply.github.com>
2023-04-21 01:21:29 +1000

    Additional edit mode tests for Containers. (#1183)

--- Assets/Scripts/Tests/Edit Mode/ContainerTests.cs
+++ Assets/Scripts/Tests/Edit Mode/ContainerTests.cs
@@ -25,7 +25,110 @@ namespace EditorTests
             container.AddItem(item);
 
             // ASSERT
+            Assert.True(container.ItemCount == 1);
             Assert.True(container.Items.Contains(item));
+        }
+
+        /// <summary>
+        /// Container should not be able to store an item that is too large for it
+        /// </summary>
+        [Test]
+        public void ShouldNotBeAbleToStoreOversizeItem()
+        {
+            // ARRANGE
+            Vector2Int containerSize = new Vector2Int(2, 2);
+            Container container = new Container(containerSize);
+            Item item = createItem(xSize:1, ySize:3);           // Technically smaller than container, but impossible to fit.
+
+            // ACT
+            container.AddItem(item);
+
+            // ASSERT
+            Assert.True(container.ItemCount == 0);
+            Assert.False(container.Items.Contains(item));
+        }
+
+        /// <summary>
+        /// Container should no longer be able to accept new items when it has been filled up.
+        /// </summary>
+        [Test]
+        public void ShouldRunOutOfSpaceAsItemsAreAdded()
+        {
+            // ARRANGE
+            Vector2Int containerSize = new Vector2Int(3, 3);  // Container is only large enough for three of the items.
+            Container container = new Container(containerSize);
+            Item item1 = createItem(xSize: 1, ySize: 3);
+            Item item2 = createItem(xSize: 1, ySize: 3);
+            Item item3 = createItem(xSize: 1, ySize: 3);
+            Item item4 = createItem(xSize: 1, ySize: 3);
+
+            // ACT
+            container.AddItem(item1);
+            container.AddItem(item2);
+            container.AddItem(item3);
+            container.AddItem(item4);
+
+
+            // ASSERT
+            Assert.True(container.ItemCount == 3);
+            Assert.True(container.Items.Contains(item1));
+            Assert.True(container.Items.Contains(item2));
+            Assert.True(container.Items.Contains(item3));
+            Assert.False(container.Items.Contains(item4));
+        }
+
+        /// <summary>
+        /// Removing items from container should free up space.
+        /// </summary>
+        [Test]
+        public void RemovingItemFromContainerShouldFreeUpSpaceForOtherItems()
+        {
+            // ARRANGE
+            Vector2Int containerSize = new Vector2Int(3, 3);  // Container is only large enough for one of the items.
+            Container container = new Container(containerSize);
+            Item item1 = createItem(xSize: 3, ySize: 3);
+            Item item2 = createItem(xSize: 3, ySize: 3);
+            
+            // Preload container with first item, and confirm that it cannot accept the second item (because it is full after the first)
+            container.AddItem(item1);
+            container.AddItem(item2);
+            Assert.True(container.Items.Contains(item1));
+            Assert.False(container.Items.Contains(item2));
+
+            // ACT
+            container.RemoveItem(item1);
+            container.AddItem(item2);
+
+            // ASSERT
+            Assert.True(container.ItemCount == 1);
+            Assert.False(container.Items.Contains(item1));
+            Assert.True(container.Items.Contains(item2));
+        }
+
+        /// <summary>
+        /// Specific placement of items should be able to prevent other items from fitting in the container, despite the container
+        /// being technically large enough to accommodate them if efficiently packed.
+        /// </summary>
+        [Test]
+        public void AddingItemAtSpecificPositionCanBlockOtherItems()
+        {
+            // ARRANGE
+            Vector2Int containerSize = new Vector2Int(4, 4);  // Container is large enough to hold four 2x2 items (efficiently packed).
+            Container container = new Container(containerSize);
+            Vector2Int pos = new Vector2Int(1, 1);            
+            Item item1 = createItem(xSize: 2, ySize: 2);
+            Item item2 = createItem(xSize: 2, ySize: 2);      
+
+            // ACT
+            container.AddItemPosition(item1, pos);           // Top left corner at (1, 1) => 2x2 item is centrally placed in 4x4 container
+            container.AddItem(item2);                        // Should not be able to fit - no 2x2 area available.
+            Assert.True(container.Items.Contains(item1));
+            Assert.False(container.Items.Contains(item2));
+
+            // ASSERT
+            Assert.True(container.ItemCount == 1);
+            Assert.True(container.Items.Contains(item1));
+            Assert.False(container.Items.Contains(item2));
         }
 
         /// <summary>
@@ -107,13 +210,15 @@ namespace EditorTests
         /// </summary>
         /// <param name="traits"></param>
         /// <returns></returns>
-        private static Item createItem()
+        private static Item createItem(string name = "TestItem", float weight = 1f, int xSize = 1, int ySize = 1)
         {
             var go = new GameObject();
             var item = go.AddComponent<Item>();
-            item.Init("TestItem", 1f, Vector2Int.one, new List<Trait>());
+            var size = new Vector2Int(xSize, ySize);
+            item.Init(name, weight, size, new List<Trait>());
             return item;
         }
+
         #endregion
     }
 }
