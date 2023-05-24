8f1af2625814ff6e6c715e8e78a78451c5df98c7
stilnat <nathan.raynal.castang@gmail.com>
2023-05-24 09:18:58 +0200

    fix value in container type to have non overlapping bits

--- Assets/Scripts/SS3D/Systems/Inventory/Containers/ContainerType.cs
+++ Assets/Scripts/SS3D/Systems/Inventory/Containers/ContainerType.cs
@@ -8,21 +8,22 @@ namespace SS3D.Systems.Inventory.Containers
     /// Make sure all the "clothes" type container are at the end of this enum (starting from Shoes..).
     /// This is necessary for differentiating between clothes and non clothes containers in some methods. 
     /// </summary>
+    [SerializeField]
     public enum ContainerType
     {
-        None,
-        Pocket,
-        Hand,
-        Bag,
-        Identification,
-        Shoes,
-        Gloves,
-        Glasses,
-        Mask,
-        Ears,
-        Head,
-        Jumpsuit,
-        ExoSuit,
+        None = 0,
+        Pocket = 1 << 0,
+        Hand = 1 << 1,
+        Shoes = 1 << 2,
+        Bag = 1 << 3,
+        Identification = 1 << 4,
+        Gloves = 1 << 5,
+        Glasses = 1 << 6,
+        Mask = 1 << 7,
+        Ears = 1 << 8,
+        Head = 1 << 9,
+        Jumpsuit = 1 << 10,
+        ExoSuit = 1 << 11,
     }
 }
 
