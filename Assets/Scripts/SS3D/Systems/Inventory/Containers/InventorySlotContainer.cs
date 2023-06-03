629e3226883bd64e1aaefa00e3c153465f7e2e02
stilnat <nathan.raynal.castang@gmail.com>
2023-06-03 17:58:03 +0200

    add inventory slot container

---
+++ Assets/Scripts/SS3D/Systems/Inventory/Containers/InventorySlotContainer.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.Serialization;
+using SS3D.Systems.Inventory.Items;
+using JetBrains.Annotations;
+using FishNet.Object.Synchronizing;
+using System.Linq;
+using FishNet.Object;
+
+namespace SS3D.Systems.Inventory.Containers
+{
+
+    public class InventorySlotContainer : AttachedContainer
+    {
+
+    }
+
+}
