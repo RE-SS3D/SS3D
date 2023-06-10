9b0cfcfd685ef85a8caa5c7f352747018e1503a8
stilnat <nathan.raynal.castang@gmail.com>
2023-06-10 17:21:29 +0200

    rename, document inventoryview

--- Assets/Scripts/SS3D/Systems/Inventory/Containers/InventoryView.cs
+++ Assets/Scripts/SS3D/Systems/Inventory/Containers/InventoryView.cs
@@ -18,58 +18,69 @@ using FishNet;
 namespace SS3D.Systems.Inventory.UI
 {
     /// <summary>
-    /// This handles displaying, removing, adding the container slots in the inventory of the player.
+    /// This handles displaying the container slots in the UI of the player.
     /// </summary>
     public class InventoryView : View
     {
-
+        // A reference to the Inventory it displays
         private HumanInventory Inventory;
 
-        // Slots in the horizontal layout
-        public GameObject HorizontalLayout;
-        public GameObject PocketPrefab;
-        public GameObject IDSlotPrefab;
-        public GameObject HandLeftPrefab;
-        public GameObject HandRightPrefab;
-        public GameObject BagPrefab;
-        public GameObject Divisor;
+        [SerializeField] private GameObject HorizontalLayout;
 
+        // Slots prefab in the horizontal layout
+        [SerializeField] private GameObject PocketPrefab;
+        [SerializeField] private GameObject IDSlotPrefab;
+        [SerializeField] private GameObject HandLeftPrefab;
+        [SerializeField] private GameObject HandRightPrefab;
+        [SerializeField] private GameObject BagPrefab;
+        [SerializeField] private GameObject Divisor;
+
+        /// <summary>
+        /// The order in which each containerType in the Horizontal layout should display.
+        /// </summary>
         [SerializeField]
         private List<ContainerType> HorizontalSlotOrder;
 
+        [SerializeField] private GameObject ClothingLayout;
 
-        public GameObject ClothingLayout;
-        public GameObject ShoeLeftPrefab;
-        public GameObject ShoeRightPrefab;
-        public GameObject GloveLeftPrefab;
-        public GameObject GloveRightPrefab;
-        public GameObject GlassesPrefab;
-        public GameObject MaskPrefab;
-        public GameObject HeadPrefab;
-        public GameObject JumpsuitPrefab;
-        public GameObject ExoSuitPrefab;
-        public GameObject DummyPrefab;
-        public GameObject EarLeftPrefab;
-        public GameObject EarRightPrefab;
+        // Slots prefab for the Clothing grid.
+        [SerializeField] private GameObject ShoeLeftPrefab;
+        [SerializeField] private GameObject ShoeRightPrefab;
+        [SerializeField] private GameObject GloveLeftPrefab;
+        [SerializeField] private GameObject GloveRightPrefab;
+        [SerializeField] private GameObject GlassesPrefab;
+        [SerializeField] private GameObject MaskPrefab;
+        [SerializeField] private GameObject HeadPrefab;
+        [SerializeField] private GameObject JumpsuitPrefab;
+        [SerializeField] private GameObject ExoSuitPrefab;
+        [SerializeField] private GameObject EarLeftPrefab;
+        [SerializeField] private GameObject EarRightPrefab;
+        [SerializeField] private GameObject DummyPrefab;
 
-
+        /// <summary>
+        /// The order in which clothing slots appear in the grid by container type,
+        /// from top left, to bottom right.
+        /// </summary>
         [SerializeField]
         private List<ContainerType> ClothingSlotPosition;
 
+        // All the slots present in the UI.
         private List<SingleItemContainerSlot> Slots = new();
 
+        // The number of Hand slots.
         public int CountHandsSlots => Slots.Where(x => x.ContainerType == ContainerType.Hand).Count();
 
         public void Setup(HumanInventory inventory)
         {
             FillClothingLayoutWithDummySlots();
             Inventory = inventory;
-            inventory.OnInventoryContainerAdded += OnInventoryContainerAdded;
-            inventory.OnInventoryContainerRemoved += OnInventoryContainerRemoved;
-            
-
+            inventory.OnInventoryContainerAdded += HandleInventoryContainerAdded;
+            inventory.OnInventoryContainerRemoved += HandleInventoryContainerRemoved;
         }
 
+        /// <summary>
+        /// Necessary method to set up the clothing slot grid. It fills the whole grid with invisible dummy slots.
+        /// </summary>
         [Client]
         private void FillClothingLayoutWithDummySlots()
         {
@@ -81,8 +92,11 @@ namespace SS3D.Systems.Inventory.UI
             }
         }
 
+        /// <summary>
+        /// Handle adding the right slot to the right place when a container is added to the inventory.
+        /// </summary>
         [Client]
-        void OnInventoryContainerAdded(AttachedContainer container)
+        private void HandleInventoryContainerAdded(AttachedContainer container)
         {
             SingleItemContainerSlot slot;
             switch (container.Type)
@@ -158,11 +172,17 @@ namespace SS3D.Systems.Inventory.UI
             Slots.Add(slot);
         }
 
+        /// <summary>
+        /// For a given container type in the Horizontal slot, give it's order from left to right. 
+        /// </summary>
         private int OrderOfType(ContainerType type) 
         {
             return HorizontalSlotOrder.FindIndex(0, x => x == type);
         }
 
+        /// <summary>
+        /// Hand slots need a special treatment, as there's left and right hand.
+        /// </summary>
         private SingleItemContainerSlot AddHandSlot()
         {
             if (CountHandsSlots % 2 == 0)
@@ -173,10 +193,9 @@ namespace SS3D.Systems.Inventory.UI
         }
 
         /// <summary>
-        /// This place a slot in the horizontal layout according to the order defined by element order in the HorizontalSlotOrder List of containerType.
+        /// This place a slot in the horizontal layout according to the order defined 
+        /// by element order in the HorizontalSlotOrder List of containerType.
         /// </summary>
-        /// <param name="type"></param>
-        /// <returns></returns>
         private int PlaceSlot(ContainerType type)
         {
             for (int i = 0; i < HorizontalLayout.transform.childCount; i++)
@@ -191,7 +210,8 @@ namespace SS3D.Systems.Inventory.UI
         }
 
         /// <summary>
-        /// Returns the game object sibling index of the ith game object being a SingleContainerSlot and having a given ContainerType in the horizontal layout.
+        /// Returns the game object sibling index of the ith game object being a 
+        /// SingleContainerSlot and having a given ContainerType in the horizontal layout.
         /// </summary>
         /// <returns> Index 0 if no slot with container type given is found, otherwise the ith index slot of the given type.</returns>
         private int IndexSlotOfType(ContainerType type, int number)
@@ -213,7 +233,8 @@ namespace SS3D.Systems.Inventory.UI
         }
 
         /// <summary>
-        /// Returns the game object sibling index of the last game object being a SingleContainerSlot and having a given ContainerType in the horizontal Layout.
+        /// Returns the game object sibling index of the last game object being a
+        /// SingleContainerSlot and having a given ContainerType in the horizontal Layout.
         /// </summary>
         /// <returns> Index 0 if no slot with container type given is found, otherwise the last index slot of the given type.</returns>
         private int LastIndexSlotOfType(ContainerType type, int number)
@@ -233,6 +254,10 @@ namespace SS3D.Systems.Inventory.UI
                 return slotsOfType.IndexOf(slotsOfType.Last());
         }
 
+        /// <summary>
+        /// Add clothing slot, which are slots in the left/down part of the screen, organised in a grid.
+        /// The order of apparition in the grid is from top left to bottom right, and is defined by the ClothingSlotPosition list.
+        /// </summary>
         private SingleItemContainerSlot AddClothingSlot(GameObject prefabToInstantiate)
         {
             GameObject clothingSlot = Instantiate(prefabToInstantiate, transform);
@@ -246,11 +271,13 @@ namespace SS3D.Systems.Inventory.UI
             return slot;
         }
 
+        /// <summary>
+        /// Add slot in the horizontal layout, such as bag, hand, id ...
+        /// </summary>
         private SingleItemContainerSlot AddHorizontalLayoutSlot(GameObject prefab, ContainerType type)
         {
             GameObject slot = Instantiate(prefab, transform);
             slot.transform.SetParent(HorizontalLayout.transform, false);
-            // Pocket go  to the far right of the UI.
             slot.transform.SetSiblingIndex(PlaceSlot(type));
             return slot.GetComponent<SingleItemContainerSlot>();
         }
@@ -282,8 +309,10 @@ namespace SS3D.Systems.Inventory.UI
             return HorizontalLayout.transform.GetChild(index+childIndex);
         }
 
-
-        void OnInventoryContainerRemoved(AttachedContainer container)
+        /// <summary>
+        /// Remove the corresponding slot when a container is removed from the inventory.
+        /// </summary>
+        private void HandleInventoryContainerRemoved(AttachedContainer container)
         {
             int indexToRemove = Slots.FindIndex(slot => slot.Container == container);
             Slots[indexToRemove].gameObject.Dispose(true);
