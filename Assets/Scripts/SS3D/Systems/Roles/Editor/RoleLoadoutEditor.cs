458ece489b8e402fa59f7ebe48ef9c3a4515d719
stilnat <nathan.raynal.castang@gmail.com>
2023-07-22 14:42:54 +0200

    add spawning with clothes

--- Assets/Scripts/SS3D/Systems/Roles/Editor/RoleLoadoutEditor.cs
+++ Assets/Scripts/SS3D/Systems/Roles/Editor/RoleLoadoutEditor.cs
@@ -1,4 +1,4 @@
-using SS3D.Data.Enums;
+﻿using SS3D.Data.Enums;
 using SS3D.Systems.Roles;
 using UnityEditor;
 
@@ -22,8 +22,17 @@ public class RoleLoadoutEditor : Editor
         roleLoadout.RightHand = EditorGUILayout.Toggle("Right Hand", roleLoadout.RightHand);
         roleLoadout.LeftPocket = EditorGUILayout.Toggle("Left Pocket", roleLoadout.LeftPocket);
         roleLoadout.RightPocket = EditorGUILayout.Toggle("Right Pocket", roleLoadout.RightPocket);
+		roleLoadout.LeftGlove = EditorGUILayout.Toggle("Left Glove", roleLoadout.LeftGlove);
+		roleLoadout.RightGlove = EditorGUILayout.Toggle("Right Glove", roleLoadout.RightGlove);
+		roleLoadout.LeftShoe = EditorGUILayout.Toggle("Left Shoe", roleLoadout.LeftShoe);
+		roleLoadout.RightShoe = EditorGUILayout.Toggle("Right Shoe", roleLoadout.RightShoe);
+		roleLoadout.Jumpsuit = EditorGUILayout.Toggle("Jumpsuit", roleLoadout.Jumpsuit);
+		roleLoadout.Glasses = EditorGUILayout.Toggle("Glasses", roleLoadout.Glasses);
+		roleLoadout.LeftEar = EditorGUILayout.Toggle("LeftEar", roleLoadout.LeftEar);
+		roleLoadout.RightEar = EditorGUILayout.Toggle("RightEar", roleLoadout.RightEar);
+		roleLoadout.Hat = EditorGUILayout.Toggle("Hat", roleLoadout.Hat);
 
-        if (roleLoadout.LeftHand)
+		if (roleLoadout.LeftHand)
         {
             roleLoadout.LeftHandItem = (ItemId)EditorGUILayout.
                 EnumPopup("Left Hand Item:", roleLoadout.LeftHandItem);
@@ -46,5 +55,60 @@ public class RoleLoadoutEditor : Editor
             roleLoadout.RightPocketItem = (ItemId)EditorGUILayout.
                 EnumPopup("Right Pocket Item:", roleLoadout.RightPocketItem);
         }
-    }
+
+		if (roleLoadout.LeftGlove)
+		{
+			roleLoadout.LeftGloveItem = (ItemId)EditorGUILayout.
+				EnumPopup("Left Glove Item:", roleLoadout.LeftGloveItem);
+		}
+
+		if (roleLoadout.LeftShoe)
+		{
+			roleLoadout.LeftShoeItem = (ItemId)EditorGUILayout.
+				EnumPopup("Left Shoe Item:", roleLoadout.LeftShoeItem);
+		}
+
+		if (roleLoadout.RightShoe)
+		{
+			roleLoadout.RightShoeItem = (ItemId)EditorGUILayout.
+				EnumPopup("Right Shoe Item:", roleLoadout.RightShoeItem);
+		}
+
+		if (roleLoadout.RightGlove)
+		{
+			roleLoadout.RightGloveItem = (ItemId)EditorGUILayout.
+				EnumPopup("Right Glove Item:", roleLoadout.RightGloveItem);
+		}
+
+
+		if (roleLoadout.LeftEar)
+		{
+			roleLoadout.LeftEarItem = (ItemId)EditorGUILayout.
+				EnumPopup("Left Ear Item:", roleLoadout.LeftEarItem);
+		}
+
+		if (roleLoadout.RightEar)
+		{
+			roleLoadout.RightEarItem = (ItemId)EditorGUILayout.
+				EnumPopup("Right Ear Item:", roleLoadout.RightEarItem);
+		}
+
+		if (roleLoadout.Jumpsuit)
+		{
+			roleLoadout.JumpsuitItem = (ItemId)EditorGUILayout.
+				EnumPopup("Jumpsuit Item:", roleLoadout.JumpsuitItem);
+		}
+
+		if (roleLoadout.Hat)
+		{
+			roleLoadout.HatItem = (ItemId)EditorGUILayout.
+				EnumPopup("Hat Item:", roleLoadout.HatItem);
+		}
+
+		if (roleLoadout.Glasses)
+		{
+			roleLoadout.GlassesItem = (ItemId)EditorGUILayout.
+				EnumPopup("Glasses Item:", roleLoadout.GlassesItem);
+		}
+	}
 }
