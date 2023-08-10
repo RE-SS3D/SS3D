4a219d8eb24d9676d10e71187f1589faae1beb0a
stilnat <nathan.raynal.castang@gmail.com>
2023-08-10 16:30:52 +0200

    fix handcommand

--- Assets/Scripts/SS3D/Systems/IngameConsoleSystem/Commands/AddHandCommand.cs
+++ Assets/Scripts/SS3D/Systems/IngameConsoleSystem/Commands/AddHandCommand.cs
@@ -23,15 +23,43 @@ namespace SS3D.Systems.IngameConsoleSystem.Commands
 		public override string ShortDescription => "add hand to user";
 		public override ServerRoleTypes AccessLevel => ServerRoleTypes.Administrator;
 
+		public override CommandType Type => CommandType.Server;
+
 		public GameObject hand;
 
-		public override string Perform(string[] args)
+		public override string Perform(string[] args, NetworkConnection conn)
 		{
 			CheckArgsResponse checkArgsResponse = CheckArgs(args);
 			if (checkArgsResponse.IsValid == false)
 				return checkArgsResponse.InvalidArgs;
+
 			string ckey = args[0];
-			CmdPerform(args);
+
+			// default transform for hand.
+			Vector3 position = new Vector3(0.5f, 0.7f, 0);
+			Vector3 rotation = new Vector3(-50, -270, 90);
+
+			if (args.Length > 1)
+			{
+				position = new Vector3(float.Parse(args[1]), float.Parse(args[2]), float.Parse(args[3]));
+				rotation = new Vector3(float.Parse(args[4]), float.Parse(args[5]), float.Parse(args[6]));
+			}
+
+			Player Player = Subsystems.Get<PlayerSystem>().GetPlayer(ckey);
+			Entity entity = Subsystems.Get<EntitySystem>().GetSpawnedEntity(Player);
+
+			GameObject leftHandPrefab = Assets.Get<GameObject>((int)AssetDatabases.BodyParts, (int)BodyPartsIds.HumanHandLeft);
+			GameObject leftHandObject = GameObject.Instantiate(leftHandPrefab, entity.transform);
+			leftHandObject.transform.localPosition = position;
+			leftHandObject.transform.localEulerAngles = rotation;
+
+			Hand leftHand = leftHandObject.GetComponent<Hand>();
+			InstanceFinder.ServerManager.Spawn(leftHandObject, Player.Owner);
+
+			Hands hands = entity.GetComponent<Hands>();
+			HumanInventory inventory = entity.GetComponent<HumanInventory>();
+			inventory.TryAddContainer(leftHandObject.GetComponent<AttachedContainer>());
+			hands.AddHand(leftHand);
 
 			return "hand added";
 		}
@@ -67,15 +95,6 @@ namespace SS3D.Systems.IngameConsoleSystem.Commands
 		private void CmdPerform(string[] args, NetworkConnection conn = null)
 		{
 			string ckey = args[0];
-			if(!Subsystems.Get<PermissionSystem>().TryGetUserRole(args[0], out ServerRoleTypes userPermission))
-			{
-				return;
-			}
-
-			if(userPermission < AccessLevel)
-			{
-				return;
-			}
 
 			// default transform for hand.
 			Vector3 position = new Vector3(0.5f, 0.7f, 0);
