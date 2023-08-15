9e98cd96199bfa3834716f82915303393e45993f
stilnat <nathan.raynal.castang@gmail.com>
2023-08-15 12:45:35 +0200

    fix kill interaction

--- Assets/Scripts/SS3D/Interactions/Extensions/InteractionTargetExtensions.cs
+++ Assets/Scripts/SS3D/Interactions/Extensions/InteractionTargetExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using SS3D.Interactions.Interfaces;
+using UnityEngine;
 
 namespace SS3D.Interactions.Extensions
 {
@@ -13,5 +14,30 @@ namespace SS3D.Interactions.Extensions
 
             return null;
         }
-    }
+
+		public static T GetComponentInParent<T>(this IInteractionTarget target) where T : class
+		{
+			GameObject go;
+			if (target is IGameObjectProvider provider)
+			{
+				go = provider.GameObject;
+			}
+			else
+			{
+				return null;
+			}
+
+			while (go != null)
+			{
+				var component = go.gameObject.GetComponent<T>();
+				if (component != null)
+				{
+					return component;
+				}
+				go = go.transform.parent.gameObject;
+			}
+
+			return null;
+		}
+	}
 }
---
+++ Assets/Scripts/SS3D/Systems/Health/Interactions/KillInteraction.cs
@@ -0,0 +1,55 @@
+using System;
+using SS3D.Data;
+using SS3D.Data.Enums;
+using SS3D.Interactions;
+using SS3D.Interactions.Extensions;
+using SS3D.Systems.Entities;
+using SS3D.Systems.Inventory.Containers;
+using UnityEngine;
+
+namespace SS3D.Systems.Health
+{
+    // a drop interaction is when we remove an item from the hand
+    [Serializable]
+    public class KillInteraction : Interaction
+    {
+        public override string GetName(InteractionEvent interactionEvent)
+        {
+            return "Kill";
+        }
+
+        public override Sprite GetIcon(InteractionEvent interactionEvent)
+        {
+            return Icon != null ? Icon : Assets.Get(InteractionIcons.Discard);
+        }
+
+        public override bool CanInteract(InteractionEvent interactionEvent)
+        {
+            // if the interaction source's parent is not a hand we return false
+            if (interactionEvent.Source.GetRootSource() is not Hands)
+            {
+                return false;
+            }
+
+            // and we do a range check just in case
+            return InteractionExtensions.RangeCheck(interactionEvent);
+        }
+
+        public override bool Start(InteractionEvent interactionEvent, InteractionReference reference)
+        {
+            // we check if the source of the interaction is a hand
+            if (interactionEvent.Source.GetRootSource() is Hands hands)
+            {
+                var entity = interactionEvent.Target.GetComponentInParent<Entity>();
+                if(entity == null) 
+                {
+                    return false;
+                }
+
+                Debug.Log("Kill the target entity " + entity.Ckey);
+            }
+
+            return false;
+        }
+    }
+}
