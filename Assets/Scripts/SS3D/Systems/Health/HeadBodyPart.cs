ce9c8aa607e29b88cd444822325aff0bf9cf4603
stilnat <nathan.raynal.castang@gmail.com>
2023-08-20 10:09:15 +0200

    add head body part

---
+++ Assets/Scripts/SS3D/Systems/Health/HeadBodyPart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadBodyPart : BodyPart
+{ 
+	public override void Init(BodyPart parent)
+	{
+		base.Init(parent);
+	}
+
+	protected override void AddInitialLayers()
+	{
+		TryAddBodyLayer(new MuscleLayer(this));
+		TryAddBodyLayer(new BoneLayer(this));
+		TryAddBodyLayer(new CirculatoryLayer(this));
+		TryAddBodyLayer(new NerveLayer(this));
+	}
+}
