505f1d06584119f632072eeeb6d049d0a127b21e
stilnat <nathan.raynal.castang@gmail.com>
2023-08-23 20:48:58 +0200

    simple script to check who owns a given go

---
+++ Assets/Scripts/SS3D/Hacks/WhoIsTheOwner.cs
@@ -0,0 +1,26 @@
+﻿using FishNet.Object;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhoIsTheOwner : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+		if (!Input.GetKeyDown(KeyCode.F))
+		{
+			return;
+		}
+
+		Debug.Log("Owner of gameobject" + gameObject.name + " is " + Owner);
+
+	}
+}
