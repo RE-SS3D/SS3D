cf719cee5a3401a64233e452e07fd6eda7b60036
stilnat <nathan.raynal.castang@gmail.com>
2023-08-27 15:35:53 +0200

    add hit interaction, for a combat basis

---
+++ Assets/Scripts/SS3D/Systems/Combat/Interactions/HandHit.cs
@@ -0,0 +1,23 @@
+﻿using SS3D.Interactions;
+using SS3D.Interactions.Interfaces;
+using SS3D.Systems.Combat.Interactions;
+using SS3D.Systems.Inventory.Containers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandHit : MonoBehaviour, IInteractionSourceExtension
+{
+	public void GetSourceInteractions(IInteractionTarget[] targets, List<InteractionEntry> interactions)
+	{
+		var interaction = new HitInteraction();
+		
+		foreach(IInteractionTarget target in targets)
+		{
+			if(interaction.CanInteract(new InteractionEvent(gameObject.GetComponent<Hand>(), target)))
+			{
+				interactions.Add(new InteractionEntry(target, interaction));
+			}
+		}
+	}
+}
