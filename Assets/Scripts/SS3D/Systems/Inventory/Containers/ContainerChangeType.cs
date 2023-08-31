655c243bf08361011bbde495d69286afe45c9dbf
stilnat <nathan.raynal.castang@gmail.com>
2023-08-31 10:29:18 +0200

    some comments

--- Assets/Scripts/SS3D/Systems/Inventory/Containers/ContainerChangeType.cs
+++ Assets/Scripts/SS3D/Systems/Inventory/Containers/ContainerChangeType.cs
@@ -1,5 +1,10 @@
 ﻿namespace SS3D.Systems.Inventory.Containers
 {
+	/// <summary>
+	/// Add is for when you add an item to a container.
+	/// Remove is for when you remove an item from a container.
+	/// Move is when you move an item inside its current container.
+	/// </summary>
     public enum ContainerChangeType
     {
 		None,
