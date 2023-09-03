0b2963ba05b3184008f461fdc936080eb1fe1c8c
stilnat <nathan.raynal.castang@gmail.com>
2023-09-03 23:33:36 +0200

    Add consumer system
    consumer system registers Oxygen consumers and periodically call Consume oxygen on all of them.

---
+++ Assets/Scripts/SS3D/Systems/Health/OxygenConsumerSystem.cs
@@ -0,0 +1,38 @@
+﻿using SS3D.Core.Behaviours;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OxygenConsumerSystem : NetworkSystem
+{
+    private readonly List<IOxygenConsumer> consumerList = new List<IOxygenConsumer>();
+    private float _timer = 0f;
+    private float _timeBeforeConsuming = 1f;
+
+    void Update()
+    {
+        if (!IsServer) return;
+
+        _timer += Time.deltaTime;
+
+        if (_timer > _timeBeforeConsuming)
+        {
+            _timer = 0f;
+            foreach (IOxygenConsumer consumer in consumerList)
+            {
+                consumer.ConsumeOxygen();
+            }
+        }
+    }
+
+    public void RegisterConsumer(IOxygenConsumer consumer)
+    {
+        consumerList.Add(consumer);
+    }
+
+    public void UnregisterConsumer(IOxygenConsumer consumer)
+    {
+        consumerList.Remove(consumer);
+    }
+}
