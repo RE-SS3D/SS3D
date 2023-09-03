24e5569f793b4f252611c55fbea9a4cc31c09f80
stilnat <nathan.raynal.castang@gmail.com>
2023-09-03 23:32:17 +0200

    add interface for oxygen consuming and needing

---
+++ Assets/Scripts/SS3D/Systems/Health/IOxygenConsumer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IOxygenConsumer
+{
+    public void RegisterToOxygenConsumerSystem();
+
+    public void ConsumeOxygen();
+}
---
+++ Assets/Scripts/SS3D/Systems/Health/IOxygenNeeder.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IOxygenNeeder
+{
+    /// <summary>
+    /// Return the amount in moles of needed oxygen
+    /// </summary>
+    public double GetOxygenNeeded();
+}
