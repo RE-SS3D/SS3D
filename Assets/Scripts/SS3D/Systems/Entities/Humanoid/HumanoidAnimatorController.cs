18b4a446014a4a416090e489e4ef86bebe0ef02b
stilnat <nathan.raynal.castang@gmail.com>
2023-09-04 23:53:08 +0200

    renaming

--- Assets/Scripts/SS3D/Systems/Entities/Humanoid/HumanoidAnimatorController.cs
+++ Assets/Scripts/SS3D/Systems/Entities/Humanoid/HumanoidAnimatorController.cs
@@ -1,4 +1,4 @@
-using SS3D.Core.Behaviours;
+﻿using SS3D.Core.Behaviours;
 using SS3D.Systems.Entities.Data;
 using UnityEngine;
 
@@ -25,12 +25,12 @@ namespace SS3D.Systems.Entities.Humanoid
 
         private void SubscribeToEvents()
         {
-            _movementController.SpeedChangeEvent += UpdateMovement;
+            _movementController.OnSpeedChangeEvent += UpdateMovement;
         }
 
         private void UnsubscribeFromEvents()
         {
-            _movementController.SpeedChangeEvent -= UpdateMovement;
+            _movementController.OnSpeedChangeEvent -= UpdateMovement;
         }
 
         private void UpdateMovement(float speed)
