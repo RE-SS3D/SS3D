e3347660a17ce46f89a9511e2756d0d94b83e815
stilnat <nathan.raynal.castang@gmail.com>
2023-09-14 13:44:47 +0200

    add substance container display for debugging

---
+++ Assets/Scripts/SS3D/Systems/Substances/EditorDisplay/SubstanceContainerDisplay.cs
@@ -0,0 +1,29 @@
+﻿using SS3D.Attributes;
+using SS3D.Substances;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+/// <summary>
+/// Put it next to a substance container and set up the reference to see at run time
+/// the actual amount of different substances present in the substance container
+/// </summary>
+
+namespace SS3D.Substances
+{
+    public class SubstanceContainerDisplay : MonoBehaviour
+    {
+
+        [SerializeField] private SubstanceContainer _container;
+
+        [ReadOnly] public List<SubstanceEntry> Substances;
+
+        // Update is called once per frame
+        void Update()
+        {
+            Substances = _container.Substances.ToList();
+        }
+    }
+}
+
