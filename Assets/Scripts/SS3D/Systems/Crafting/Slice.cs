40865f332283a0167408ee7150591beb48610170
stilnat <nathan.raynal.castang@gmail.com>
2023-09-18 21:26:37 +0200

    Add slice source interaction extension

---
+++ Assets/Scripts/SS3D/Systems/Crafting/Slice.cs
@@ -0,0 +1,25 @@
+﻿using SS3D.Interactions;
+using SS3D.Interactions.Interfaces;
+using SS3D.Systems.Combat.Interactions;
+using SS3D.Systems.Crafting;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SS3D.Systems.Crafting
+{
+    public class Slice : MonoBehaviour, IInteractionSourceExtension
+    {
+        public void GetSourceInteractions(IInteractionTarget[] targets, List<InteractionEntry> interactions)
+        {
+            var interaction = new SliceInteraction();
+
+            foreach (IInteractionTarget target in targets)
+            {
+                interactions.Add(new InteractionEntry(target, interaction));
+            }
+        }
+    }
+}
+
+
