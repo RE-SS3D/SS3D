75d2ac5863cf1e1868039fdc08b9238cf83504cf
stilnat <nathan.raynal.castang@gmail.com>
2023-09-19 14:54:06 +0200

    correct sline name

--- Assets/Scripts/SS3D/Systems/Crafting/SliceInteraction.cs
+++ Assets/Scripts/SS3D/Systems/Crafting/SliceInteraction.cs
@@ -4,6 +4,7 @@ using SS3D.Core;
 using SS3D.Data;
 using SS3D.Data.Enums;
 using SS3D.Interactions;
+using SS3D.Interactions.Extensions;
 using SS3D.Logging;
 using SS3D.Systems.Inventory.Containers;
 using SS3D.Systems.Inventory.Items;
@@ -29,7 +30,7 @@ namespace SS3D.Systems.Crafting
 
         public override string GetName(InteractionEvent interactionEvent)
         {
-            return GetGenericName() + " " + interactionEvent.Target.ToString();
+            return GetGenericName() + " " + interactionEvent.Target.GetGameObject().name.Split("(")[0];
         } 
     }
 }
