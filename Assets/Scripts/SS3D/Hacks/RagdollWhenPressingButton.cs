f19dcd1eac7f2b7bcf9f7e1cc49adf51b22ed285
stilnat <nathan.raynal.castang@gmail.com>
2023-10-08 15:45:31 +0200

    adding ragdoll hack when pressing button

---
+++ Assets/Scripts/SS3D/Hacks/RagdollWhenPressingButton.cs
@@ -0,0 +1,35 @@
+﻿using SS3D.Core;
+using SS3D.Systems.Entities.Humanoid;
+using SS3D.Systems.Inputs;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using InputSystem = SS3D.Systems.Inputs.InputSystem;
+
+namespace SS3D.Hacks
+{
+    /// <summary>
+    /// Currenytly using button Y to ragdoll, might change in the future.
+    /// </summary>
+    public class RagdollWhenPressingButton : MonoBehaviour
+    {
+        [SerializeField]
+        private Ragdoll _ragdoll;
+
+        [SerializeField]
+        private float _timeRagdolled;
+
+        private Controls.OtherActions _controls;
+
+        private void Start()
+        {
+            _controls = Subsystems.Get<InputSystem>().Inputs.Other;
+            _controls.Ragdoll.performed += HandleKnockdown;
+        }
+
+        private void HandleKnockdown(InputAction.CallbackContext context)
+        {
+            _ragdoll.Knockdown(_timeRagdolled);
+        }
+    }
+}
+
