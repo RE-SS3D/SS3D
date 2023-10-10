4e6e89cf161a2eb92223592dba33ad54fcfe0e41
João Buratto <46575243+joaoburatto@users.noreply.github.com>
2023-10-10 15:00:42 -0300

    Fix recipes

--- Assets/Scripts/SS3D/Systems/Crafting/CraftingRecipe.cs
+++ Assets/Scripts/SS3D/Systems/Crafting/CraftingRecipe.cs
@@ -1,5 +1,7 @@
-﻿using SS3D.Data.Enums;
+﻿using Coimbra;
+using SS3D.Data.Enums;
 using SS3D.Systems.Inventory.Items;
+using SS3D.Utils;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -8,123 +10,53 @@ using UnityEngine;
 
 namespace SS3D.Systems.Crafting
 {
-    /// <summary>
-    /// Crafting recipes allow to replace a bunch of item by another, using a specific interaction.
-    /// </summary>
-    [CreateAssetMenu(fileName = "Recipe", menuName = "SS3D/Crafting/Recipe")]
-    public class CraftingRecipe : ScriptableObject, ISerializationCallbackReceiver
-    {
-        private Dictionary<Item, int> _elements = new();
+	/// <summary>
+	/// Crafting recipes allow to replace a bunch of item by another, using a specific interaction.
+	/// </summary>
+	[CreateAssetMenu(fileName = "Recipe", menuName = "SS3D/Crafting/Recipe")]
+	public class CraftingRecipe : ScriptableObject
+	{
+		[SerializeField]
+		private float _executionTime;
 
-        [SerializeField]
-        private float _executionTime;
+		[SerializeField]
+		private string _interactionName;
 
-        [SerializeField]
-        private string _interactionName;
+		[SerializeField]
+		private Item _target;
 
-        [SerializeField]
-        private Item _target;
+		[SerializeField]
+		private List<Item> _result;
 
-        [SerializeField]
-        private List<Item> _result;
+		[SerializeField]
+		private SerializableDictionary<Item, int> _elements = new();
 
-        /// <summary>
-        /// The items and their respective numbers necessary for the recipe.
-        /// </summary>
-        public Dictionary<Item, int> Elements => _elements;
+		/// <summary>
+		/// The items and their respective numbers necessary for the recipe.
+		/// </summary>
+		public Dictionary<Item, int> Elements => _elements;
 
-        /// <summary>
-        /// Time it takes in second for the crafting to finish.
-        /// </summary>
-        public float ExecutionTime => _executionTime;
+		/// <summary>
+		/// Time it takes in second for the crafting to finish.
+		/// </summary>
+		public float ExecutionTime => _executionTime;
 
-        /// <summary>
-        /// Name of the necessary interaction to perform the recipe.
-        /// </summary>
-        public string InteractionName => _interactionName;
+		/// <summary>
+		/// Name of the necessary interaction to perform the recipe.
+		/// </summary>
+		public string InteractionName => _interactionName;
 
-        /// <summary>
-        /// The target of the recipe, which is the item on which the player must click to get the crafting interactions.
-        /// </summary>
-        public Item Target => _target;
+		/// <summary>
+		/// The target of the recipe, which is the item on which the player must click to get the crafting interactions.
+		/// </summary>
+		public Item Target => _target;
 
-        /// <summary>
-        /// The result of the crafting.
-        /// </summary>
-        public List<Item> Result => _result;
+		/// <summary>
+		/// The result of the crafting.
+		/// </summary>
+		public List<Item> Result => _result;
 
-        public int ElementsNumber => _elements.Sum(x => x.Value);
-
-#if UNITY_EDITOR
-        /// <summary>
-        /// Necessary to be able to edit in editor recipe elements.
-        /// Not straightforward since they are in a Hashset.
-        /// </summary>
-        [SerializeField]
-        private List<RecipeElement> _recipeElements;
-#endif
-
-        public void OnAfterDeserialize()
-        {
-#if UNITY_EDITOR
-            // just transfer things from the list to the dictionnary.
-            _elements = new();
-            foreach (RecipeElement item in _recipeElements)
-            {
-	            _elements.TryAdd(item.ItemId, item.Count);
-            }
-            _recipeElements = null;
-#endif
-        }
-
-        public void OnBeforeSerialize()
-        {
-#if UNITY_EDITOR
-            // just transfer things from the dictionnary to the list.
-            if (_elements == null)
-            {
-	            return;
-            }
-
-            _recipeElements = new List<RecipeElement>();
-            foreach (Item id in _elements.Keys)
-            {
-                _recipeElements.Add(new RecipeElement(id, _elements[id]));
-            }
-#endif
-        }
-
-#if UNITY_EDITOR
-        /// <summary>
-        /// A recipe element is simply describing an item and a number of it.
-        /// </summary>
-        [System.Serializable]
-        private struct RecipeElement
-        {
-            /// <summary>
-            /// Number of items.
-            /// </summary>
-            [SerializeField]
-            private int _count;
-
-            /// <summary>
-            /// Id of the item.
-            /// </summary>
-            [SerializeField]
-            private Item _itemId;
-
-            public int Count => _count;
-
-            public Item ItemId => _itemId;
-
-            public RecipeElement(Item id, int count)
-            {
-                _count = count;
-                _itemId = id;
-            }
-        }
-#endif
-    }
-
+		public int ElementsNumber => _elements.Sum(x => x.Value);
+	}
 }
 
