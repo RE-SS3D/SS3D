c430ba71782527587834a08634f6e671c834e8cd
stilnat <nathan.raynal.castang@gmail.com>
2023-10-17 14:58:38 +0200

    Partly make dinnerbooth work

---
+++ Assets/Scripts/SS3D/Systems/Tile/Connections/AdjacencyTypes/DirectionnalShapeResolver.cs
@@ -0,0 +1,182 @@
+﻿using Coimbra;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SS3D.Systems.Tile.Connections.AdjacencyTypes
+{
+    [Serializable]
+    public struct DirectionnalShapeResolver
+    {
+        [Tooltip("A mesh where no edges are connected")]
+        public Mesh o;
+        [Tooltip("A mesh where the South edge is connected")]
+        public Mesh uLeft;
+        [Tooltip("A mesh where the South edge is connected")]
+        public Mesh uRight;
+        [Tooltip("A mesh where the South & south edges are connected")]
+        public Mesh i;
+        [Tooltip("A mesh where the South & West edges are connected")]
+        public Mesh lIn;
+        [Tooltip("A mesh where the South & West edges are connected")]
+        public Mesh lOut;
+        [Tooltip("A mesh where the South, West, and west edges are connected")]
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="adjacencyMap"></param>
+        /// <param name="direction"></param>
+        /// <param name="NeighboursDirection">Neigthbours direction are only cardinal neighbours</param>
+        /// <returns></returns>
+        public Tuple<Direction, MeshDirectionInfo> GetMeshAndDirectionAndRotation(AdjacencyMap adjacencyMap, Direction direction, List<Direction> neighboursDirection)
+        {
+            // Determine rotation and mesh specially for every single case.
+            float rotation = 0.0f;
+            Mesh mesh;
+
+            AdjacencyShape shape = GetSimpleShape(adjacencyMap, direction, neighboursDirection);
+
+
+            switch (shape)
+            {
+                case AdjacencyShape.O:
+                    mesh = o;
+                    rotation = TileHelper.AngleBetween(Direction.North, direction);
+                    break;
+                case AdjacencyShape.ULeft:
+                    mesh = uLeft;
+                    rotation = TileHelper.AngleBetween(Direction.North, direction);
+                    break;
+                case AdjacencyShape.URight:
+                    mesh = uRight;
+                    rotation = TileHelper.AngleBetween(Direction.North, direction);
+                    break;
+                case AdjacencyShape.I:
+                    mesh = i;
+                    rotation = TileHelper.AngleBetween(Direction.North, direction);
+                    break;
+                case AdjacencyShape.LIn:
+                    mesh = lIn;
+                    rotation = LInRotation(adjacencyMap);
+                    break;
+                case AdjacencyShape.LOut:
+                    mesh = lOut;
+                    rotation = LOutRotation(adjacencyMap);
+                    break;
+                default:
+                    Debug.LogError($"Received unexpected shape from simple shape resolver: {shape}");
+                    mesh = o;
+                    break;
+            }
+
+            return new Tuple<Direction, MeshDirectionInfo> (direction,  new MeshDirectionInfo { Mesh = mesh, Rotation = rotation });
+        }
+
+        public static AdjacencyShape GetSimpleShape(AdjacencyMap adjacencyMap, Direction dir, List<Direction> NeighboursDirection)
+        {
+            int cardinalConnectionCount = adjacencyMap.CardinalConnectionCount;
+            switch (cardinalConnectionCount)
+            {
+                case 0:
+                    return AdjacencyShape.O;
+                case 1:
+                    var directionConnection = adjacencyMap.GetSingleConnection(true);
+                    var relative =TileHelper.GetRelativeDirection(dir, directionConnection);
+                    if(!NeighboursDirection.Contains(dir)) return AdjacencyShape.O;
+                    if (relative == Direction.East)
+                        return AdjacencyShape.ULeft;
+                    else return AdjacencyShape.URight;
+                //When two connections, checks if they're opposite or adjacent
+                case 2:
+
+                     if (adjacencyMap.HasConnection(Direction.North) == adjacencyMap.HasConnection(Direction.South)) 
+                        return AdjacencyShape.I;
+
+                    if (IsInsideConfiguration(adjacencyMap, dir))
+                        return AdjacencyShape.LIn;
+                    else
+                        return AdjacencyShape.LOut;
+                     
+                default:
+                    Debug.LogError($"Could not resolve Simple Adjacency Shape for given Adjacency Map - {adjacencyMap}");
+                    return AdjacencyShape.O;
+            }
+        }
+
+        /// <summary>
+        /// It's useful to draw the 8 different configuration of LIn shape
+        /// with 3 couch, one in the middle and two other in cardinal positions. Consider the middle couch can point towards any cardinal
+        /// direction. We don't care about the directions of the two other couches, what matters is their position relative to the middle one.
+        /// This help understand the code below.
+        /// </summary>
+        /// <param name="adjacencyMap"></param>
+        /// <param name="dir"></param>
+        /// <returns></returns>
+        private static bool IsInsideConfiguration(AdjacencyMap adjacencyMap, Direction dir)
+        {
+            var adjacencies =  adjacencyMap.GetAdjacencies(true);
+
+            switch (dir)
+            {
+                case Direction.North:
+                    return adjacencies.Contains(Direction.North) &&
+                        (adjacencies.Contains(Direction.East) || adjacencies.Contains(Direction.West));
+                case Direction.East:
+                    return adjacencies.Contains(Direction.East) &&
+                        (adjacencies.Contains(Direction.North) || adjacencies.Contains(Direction.South));
+                case Direction.South:
+                    return adjacencies.Contains(Direction.South) &&
+                        (adjacencies.Contains(Direction.East) || adjacencies.Contains(Direction.West));
+                case Direction.West:
+                    return adjacencies.Contains(Direction.West) &&
+                        (adjacencies.Contains(Direction.South) || adjacencies.Contains(Direction.North));
+                default:
+                    return false;
+            }
+        }
+
+        private static float LOutRotation(AdjacencyMap connections)
+        {
+            var adjacencies = connections.GetAdjacencies(true);
+            if(adjacencies.Contains(Direction.South) && adjacencies.Contains(Direction.East))
+            {
+                return 90f;
+            }
+
+            if (adjacencies.Contains(Direction.South) && adjacencies.Contains(Direction.West))
+            {
+                return 180f;
+            }
+
+            if (adjacencies.Contains(Direction.North) && adjacencies.Contains(Direction.West))
+            {
+                return 270f;
+            }
+
+            return 0f;
+        }
+
+        private static float LInRotation(AdjacencyMap connections)
+        {
+            var adjacencies = connections.GetAdjacencies(true);
+            if (adjacencies.Contains(Direction.South) && adjacencies.Contains(Direction.East))
+            {
+                return 90f;
+            }
+
+            if (adjacencies.Contains(Direction.South) && adjacencies.Contains(Direction.West))
+            {
+                return 180f;
+            }
+
+            if (adjacencies.Contains(Direction.North) && adjacencies.Contains(Direction.West))
+            {
+                return 270f;
+            }
+
+            return 0f;
+        }
+    }
+}
---
+++ Assets/Scripts/SS3D/Systems/Tile/Connections/DirectionnalAdjacencyConnector.cs
@@ -0,0 +1,181 @@
+﻿using FishNet.Object.Synchronizing;
+using SS3D.Core;
+using SS3D.Core.Behaviours;
+using SS3D.Logging;
+using SS3D.Systems.Tile.Connections;
+using SS3D.Systems.Tile.Connections.AdjacencyTypes;
+using SS3D.Utils;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SS3D.Systems.Tile.Connections
+{
+    public class DirectionnalAdjacencyConnector : NetworkActor, IAdjacencyConnector
+    {
+
+        protected TileObjectGenericType _genericType;
+
+
+        protected TileObjectSpecificType _specificType;
+
+        
+        public DirectionnalShapeResolver AdjacencyResolver;
+
+        protected AdjacencyMap _adjacencyMap;
+
+        protected MeshFilter _filter;
+
+        protected PlacedTileObject _placedObject;
+
+        public PlacedTileObject PlacedObject => _placedObject;
+
+        public Direction PlacedObjectDirection => _placedObject.Direction;
+
+        private bool _initialized;
+
+        public override void OnStartClient()
+        {
+            base.OnStartClient();
+            Setup();
+        }
+
+        /// <summary>
+        /// Simply set things up, including creating new references, and fetching generic and specific type
+        /// from the associated scriptable object.
+        /// </summary>
+        private void Setup()
+        {
+            if (!_initialized)
+            {
+                _adjacencyMap = new AdjacencyMap();
+                _filter = GetComponent<MeshFilter>();
+
+                _placedObject = GetComponent<PlacedTileObject>();
+                if (_placedObject == null)
+                {
+                    _genericType = TileObjectGenericType.None;
+                    _specificType = TileObjectSpecificType.None;
+                }
+                else
+                {
+                    _genericType = _placedObject.GenericType;
+                    _specificType = _placedObject.SpecificType;
+                }
+                _initialized = true;
+            }
+        }
+
+        /// <summary>
+        /// directionnal are connected only with other directionnals on their right or on their left.
+        /// </summary>
+        /// <param name="dir"></param>
+        /// <param name="neighbourObject"></param>
+        /// <returns></returns>
+        public bool IsConnected(Direction dir, PlacedTileObject neighbourObject)
+        {
+            if (neighbourObject == null) return false;
+
+            if (!TileHelper.CardinalDirections().Contains(dir)) return false;
+
+            // add a check to see if connected to another neighbour object instead of the cardinal connection count.
+
+            bool isConnected = true;
+
+            if (neighbourObject.IsInFront(_placedObject) || neighbourObject.IsBehind(_placedObject))
+            {
+                isConnected &=  neighbourObject.Direction != _placedObject.Direction &&
+                    neighbourObject.Direction != TileHelper.GetOpposite(_placedObject.Direction);
+            }
+
+            if (neighbourObject.IsOnLeft(_placedObject) || neighbourObject.IsOnRight(_placedObject))
+            {
+                isConnected &= neighbourObject.Direction != TileHelper.GetOpposite(_placedObject.Direction);
+            }
+
+            isConnected &= neighbourObject.GenericType == _genericType;
+            isConnected &= neighbourObject.SpecificType == _specificType;
+
+            return isConnected;
+        }
+
+
+        public void UpdateAllConnections(PlacedTileObject[] neighbourObjects)
+        {
+            Setup();
+
+            bool changed = false;
+            for (int i = 0; i < neighbourObjects.Length; i++)
+            {
+                changed |= UpdateSingleConnection((Direction)i, neighbourObjects[i], true);
+            }
+
+            if (changed)
+            {
+                UpdateMeshAndDirection();
+            }
+        }
+
+        public bool UpdateSingleConnection(Direction dir, PlacedTileObject neighbourObject, bool updateNeighbour)
+        {
+            Setup();
+
+            bool isConnected = IsConnected(dir, neighbourObject);
+
+            bool isUpdated = _adjacencyMap.SetConnection(dir, new AdjacencyData(TileObjectGenericType.None, TileObjectSpecificType.None, isConnected));
+
+            // Update our neighbour as well
+            if (isConnected && updateNeighbour)
+                neighbourObject.UpdateSingleAdjacency(_placedObject, TileHelper.GetOpposite(dir));
+
+            if (isUpdated)
+            {
+                UpdateMeshAndDirection();
+            }
+
+            return isUpdated;
+        }
+
+        protected void UpdateMeshAndDirection()
+        {
+
+            Tuple<Direction, MeshDirectionInfo> info;
+
+            var tileSystem = Subsystems.Get<TileSystem>();
+            var map = tileSystem.CurrentMap;
+            
+            var neighbours = map.GetNeighbourPlacedObjects(_placedObject.Layer, _placedObject.transform.position);
+
+
+            List<Direction> neighboursFacingDirection = new();
+
+            // TODO not solid code if order of neighbours gets changed
+            foreach (Direction dir in TileHelper.CardinalDirections())
+            {
+                if(_adjacencyMap.HasConnection(dir))
+                {
+                    neighboursFacingDirection.Add(neighbours[(int)dir].Direction);
+                }
+            }
+
+            info = AdjacencyResolver.GetMeshAndDirectionAndRotation(_adjacencyMap, PlacedObjectDirection, neighboursFacingDirection);
+
+            _placedObject.SetDirection(info.Item1);
+
+            if (_filter == null)
+            {
+                Log.Warning(this, "Missing mesh {meshDirectionInfo}", Logs.Generic, info);
+            }
+
+            _filter.mesh = info.Item2.Mesh;
+
+            Quaternion localRotation = transform.localRotation;
+            Vector3 eulerRotation = localRotation.eulerAngles;
+            localRotation = Quaternion.Euler(eulerRotation.x, info.Item2.Rotation, eulerRotation.z);
+
+            transform.localRotation = localRotation;
+        }
+    }
+}
+
