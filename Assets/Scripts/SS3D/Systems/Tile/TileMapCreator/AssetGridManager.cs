ec7628e4f9a8f4cb70f2cc1d2fe43891fa8f7720
Mechar418 <77565416+Mechar418@users.noreply.github.com>
2023-10-18 23:50:37 +0700

    Change grid depending on its size

---
+++ Assets/Scripts/SS3D/Systems/Tile/TileMapCreator/AssetGridManager.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Component, that changes GridLayoutGroup contstraintCount to fit it's width
+/// </summary>
+[RequireComponent(typeof(GridLayoutGroup))]
+public class AssetGridManager : MonoBehaviour
+{
+    private GridLayoutGroup _grid;
+    private float cellWidth;
+    private float paddingWidth;
+    void Start()
+    {
+        _grid = GetComponent<GridLayoutGroup>();
+        cellWidth = _grid.cellSize.x;
+        paddingWidth = _grid.spacing.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float width = transform.parent.GetComponent<RectTransform>().rect.width;
+        _grid.constraintCount = Convert.ToInt32(Math.Floor((width + paddingWidth) / (cellWidth + paddingWidth)));
+    }
+}
