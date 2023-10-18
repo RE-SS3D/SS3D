4f87fd500bd82af23b22ee0a372016f9dd74ce28
Mechar418 <77565416+Mechar418@users.noreply.github.com>
2023-10-18 22:11:54 +0700

    Make more space, make enabling/disabling work on panels

--- Assets/Scripts/SS3D/Systems/Tile/TileMapCreator/TileMapCreator.cs
+++ Assets/Scripts/SS3D/Systems/Tile/TileMapCreator/TileMapCreator.cs
@@ -1,4 +1,5 @@
 using Coimbra;
+using DynamicPanels;
 using FishNet.Connection;
 using FishNet.Object;
 using SS3D.Core;
@@ -42,6 +43,7 @@ namespace SS3D.Systems.Tile.TileMapCreator
 		private List<GenericObjectSo> _objectDatabase;
 		private Controls.TileCreatorActions _controls;
 		private InputSystem _inputSystem;
+        private PanelTab _tab;
 
 		public bool IsDeleting
 		{
@@ -62,16 +64,18 @@ namespace SS3D.Systems.Tile.TileMapCreator
 		protected override void OnStart()
 		{
 			base.OnStart();
-			ShowUI(false);
-			_inputSystem = Subsystems.Get<InputSystem>();
+            _tab = PanelUtils.GetAssociatedTab(GetComponent<RectTransform>());
+            ShowUI(false);
+            _inputSystem = Subsystems.Get<InputSystem>();
 			_controls = _inputSystem.Inputs.TileCreator;
 			_inputSystem.ToggleAction(_controls.ToggleMenu, true);
-
-			_controls.ToggleMenu.performed += HandleToggleMenu;
+            
+            _controls.ToggleMenu.performed += HandleToggleMenu;
 			_controls.Replace.performed += HandleReplace;
 			_controls.Replace.canceled += HandleReplace;
 			_controls.Place.performed += HandlePlace;
 			_controls.Rotate.performed += HandleRotate;
+            
 		}
 
 		private void HandleToggleMenu(InputAction.CallbackContext context)
@@ -271,6 +275,11 @@ namespace SS3D.Systems.Tile.TileMapCreator
 		[ServerOrClient]
 		private void ShowUI(bool show)
 		{
+            if (!show)
+            {
+                _tab.Detach();
+            }
+            _tab.Panel.gameObject.SetActive(show);
 			_menuRoot.SetActive(show);
 		}
 
