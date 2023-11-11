f3e013c546b52ffa25008811ae4ce6fd8e36bf83
stilnat <nathan.raynal.castang@gmail.com>
2023-11-12 00:46:48 +0100

    Adding state machine handler

---
+++ Assets/Scripts/SS3D/Animations/AnimationEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SS3D.Animation
+{
+    public class StateMachineEventArgs : EventArgs
+    {
+        public Animator Animator { get; set; }
+
+        public AnimatorStateInfo StateInfo { get; set; }
+
+        public int LayerIndex { get; set; }
+    }
+}
---
+++ Assets/Scripts/SS3D/Animations/StateMachineHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SS3D.Animation
+{
+    public class StateMachineHandler : MonoBehaviour
+    {
+        public StateMachineEventHandler stateMachineEventHandler;
+
+        public void Awake()
+        {
+            stateMachineEventHandler = ScriptableObject.CreateInstance<StateMachineEventHandler>();
+        }
+
+        public class StateMachineEventHandler : StateMachineBehaviour
+        {
+            // Event that will be invoked when the animation state exits
+            public event EventHandler<StateMachineEventArgs> OnStateExitEvent;
+
+            public event EventHandler<StateMachineEventArgs> OnStateEnterEvent;
+
+            public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+            {
+                var data = new StateMachineEventArgs();
+                data.Animator = animator;
+                data.StateInfo = stateInfo;
+                data.LayerIndex = layerIndex;
+                // Check if the event is not null before invoking it
+                OnStateExitEvent?.Invoke(this, data);
+            }
+
+            public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+            {
+                var data = new StateMachineEventArgs();
+                data.Animator = animator;
+                data.StateInfo = stateInfo;
+                data.LayerIndex = layerIndex;
+                // Check if the event is not null before invoking it
+                OnStateEnterEvent?.Invoke(this, data);
+            }
+        }
+    }
+    
+}
