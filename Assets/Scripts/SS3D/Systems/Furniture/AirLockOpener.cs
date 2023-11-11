d2793e52674b29185eb500c7100798384169ef34
stilnat <nathan.raynal.castang@gmail.com>
2023-11-11 18:18:32 +0100

    add airlock opener script

---
+++ Assets/Scripts/SS3D/Systems/Furniture/AirLockOpener.cs
@@ -0,0 +1,72 @@
+﻿using FishNet.Object;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+namespace SS3D.Systems.Furniture
+{
+    public class AirLockOpener : NetworkBehaviour
+    {
+        private const float DOOR_WAIT_CLOSE_TIME = 2.0f;
+
+        [SerializeField]
+        private Animator _animator;
+
+        // This defines the animation we want to trigger
+        private static readonly int OpenId = Animator.StringToHash("Open");
+
+        [SerializeField] private LayerMask doorTriggerLayers = -1;
+
+
+        private bool openState; // Server and Client
+
+
+        private int playersInTrigger; // Server Only
+        private Coroutine closeTimer; // Server Only
+
+
+        public void OnTriggerEnter(Collider other)
+        {
+            
+            if ((1 << other.gameObject.layer & doorTriggerLayers) == 0) return;
+
+            if (playersInTrigger == 0)
+            {
+                if (closeTimer != null)
+                {
+                    StopCoroutine(closeTimer);
+                    closeTimer = null;
+                }
+                SetOpen(true);
+            }
+
+            playersInTrigger += 1;
+        }
+
+        public void OnTriggerExit(Collider other)
+        {
+            if ((1 << other.gameObject.layer & doorTriggerLayers) == 0) return;
+
+            if (playersInTrigger == 1)
+            {
+                // Start the close timer (which may be stopped).
+                closeTimer = StartCoroutine(RunCloseEventually(DOOR_WAIT_CLOSE_TIME));
+            }
+
+            playersInTrigger = Math.Max(playersInTrigger - 1, 0);
+        }
+
+        private IEnumerator RunCloseEventually(float time)
+        {
+            yield return new WaitForSeconds(time);
+            SetOpen(false);
+        }
+
+        public void SetOpen(bool open)
+        {
+            _animator.SetBool(OpenId, open);
+        }
+    }
+}
