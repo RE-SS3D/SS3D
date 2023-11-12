bc08f9170da3030250d7d21086b24013c5266fcd
stilnat <nathan.raynal.castang@gmail.com>
2023-11-12 11:29:16 +0100

    proper state machine behaviour

---
+++ Assets/Scripts/SS3D/Animations/AirlockStateMachine.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SS3D.Animation
+{
+    /// <summary>
+    /// State machine behaviour to update colors upon change of state in the Airlock animator.
+    /// </summary>
+    public class AirlockStateMachine : StateMachineBehaviour
+    {
+        private const string Opening = "Opening";
+        private const string Closing = "Closing";
+
+        private readonly Color _openingColor = new Color(.07f, 1f, .32f);
+        private readonly Color _closingColor = new Color(1, 0.18f, .2f);
+        private readonly Color _idleColor = new Color(0, 0, 0);
+
+        private const int DOOR_LIGHT_MATERIAL_INDEX = 1;
+
+
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            ChangeColors(_idleColor, animator, stateInfo, layerIndex);
+        }
+
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            if (stateInfo.IsName(Opening))
+            {
+                ChangeColors(_openingColor, animator, stateInfo, layerIndex);
+            }
+            if (stateInfo.IsName(Closing))
+            {
+                ChangeColors(_closingColor, animator, stateInfo, layerIndex);
+            }
+        }
+
+        private void ChangeColors(Color color, Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            var renderers = animator.GetComponentsInChildren<MeshRenderer>();
+            foreach (var renderer in renderers)
+            {
+                renderer.materials[DOOR_LIGHT_MATERIAL_INDEX].color = color;
+            }
+        }
+    }
+}
+    
