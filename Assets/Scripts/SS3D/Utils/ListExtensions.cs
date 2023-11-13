82481aab79c4c9ffb6f273b6451b6f1070460b18
stilnat <nathan.raynal.castang@gmail.com>
2023-11-13 20:13:04 +0100

    Directionnal adjacencies (#1303)
    * readd some tilehelper methods
    
    * readd door
    
    * add simple adjacency
    
    * add advanced connector
    
    * rename door script
    
    * making door connections work
    
    * remove wrong method
    
    * change a bit setup of map
    
    * add wall connector
    
    * expand interface and implement
    
    * fixed advanced adjacency
    
    * fix multiadjcon
    
    * add abstract horizontal connector class
    
    * fix airlock generic type
    
    * trying to fix edge case for wall connector
    
    * fix edge case wall, fix advancedAdjacency
    
    * fix pipes
    
    * change pipe tile layers
    
    * fix generic and specific type editablke in editor (should not)
    
    * some doc and some cleanup
    
    * fixing disposal pipes
    
    * make disposal pipe connect to disposal stuff
    
    * remove multiple mesh on disposal pipe
    
    * fix position
    
    * fix some stuff for disposal
    
    * Partly make dinnerbooth work
    
    * no more than 2 connections for couches
    
    * simplify Lin Lout rotation computation
    
    * add some more helpers
    
    * add list extension
    
    * solve some direction resolving for directionnals
    
    * add atdirectionof method
    
    * use neighbour instead of neighbour directions
    
    * same
    
    * paradigm changes
    
    * little comment
    
    * huge refactor
    
    * some fix and update neighbours if itself get updated
    
    * add connection number
    
    * add condition for u shapes
    
    * remove conditions and keep only neighbours with lt 2 connections
    
    * add diagonal directions for lin lout
    
    * update comments
    
    * fixing more adjacencies
    
    * add some helper method
    
    * almost there
    
    * works completely !
    
    * everything works for real this time
    
    * some refactoring and clean up and doc
    
    * add disposal furniture connector
    
    * little stuff for adjacencymap
    
    * method for PlacedTileObject
    
    * updating IAdjacencyConnector and all that depends on it
    
    * adding the disposal connector scripts to prefabs
    
    * add world origin for placed tile object.
    
    * update neighbour if itself is updated
    
    * make disposal pipe work
    
    * remove random using
    
    * fix disposal pipes
    
    * remove neighbour objects parameter from updateAllConnections
    
    * doc on the interface
    
    * clean doc
    
    * put back the right disposal pipes file
    
    * complete doc
    
    * update disposal pipes  generic type
    
    * adapt code to multiple tile objects
    
    * remove multi adjacency update some prefabs
    
    * fix missing scripts
    
    * fix build
    
    * added x6 from steelwall for steelwall reinforced
    
    * fix plenum ?
    
    * fix indentation
    
    * fix X6 for reinforced steel windows
    
    * changed material of disposal pipes
    
    * Revert "fix indentation"
    
    This reverts commit 8f1825b89c6cce3b5f158e4a228bdabbad85b8c9.
    
    * fix indentation
    
    * fix errors introduced by merge
    
    * update doc
    
    * applied palette on dinerbooth fbx
    
    * updated dinnerbooth asset
    
    * remove test scene
    
    * add working client
    
    * fix diagonal directionnals not working
    
    * do some clean up
    
    * update doc
    
    * rename with a single n
    
    * remove shadows and add collider

--- Assets/Scripts/SS3D/Utils/ListExtensions.cs
+++ Assets/Scripts/SS3D/Utils/ListExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using JetBrains.Annotations;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
 
 namespace SS3D.Utils
 {
@@ -13,6 +15,15 @@ namespace SS3D.Utils
         public static bool OneElementOnly<T>([NotNull] this List<T> list)
         {
             return list.Count == 1;
+        }
+
+        /// <summary>
+        /// Checks if the content of the list contains any element from another list. Slow for large lists.
+        /// Be aware it's based upon the Equals method to compare equality.
+        /// </summary>
+        public static bool ContainsAny<T>([NotNull] this List<T> list, List<T> otherList)
+        {
+            return list.Any(x => otherList.Any(y => y.Equals(x)));
         }
     }
 }
