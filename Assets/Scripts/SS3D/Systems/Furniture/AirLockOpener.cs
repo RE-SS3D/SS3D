1f81df33c0e84e11e7a008962998820d2a2d3021
stilnat <nathan.raynal.castang@gmail.com>
2023-11-13 14:34:55 +0100

    some doc

--- Assets/Scripts/SS3D/Systems/Furniture/AirLockOpener.cs
+++ Assets/Scripts/SS3D/Systems/Furniture/AirLockOpener.cs
@@ -8,21 +8,34 @@ namespace SS3D.Systems.Furniture
 {
     /// <summary>
     /// Script controlling the opening and closing of airlocks.
+    /// When a player get close to an airlock, open the airlock. Keep the airlock opened as long as a player
+    /// is close to the opened airlock. When no player are close to it, close the airlock.
     /// </summary>
     public class AirLockOpener : NetworkBehaviour
     {
+        /// <summary>
+        /// Time in second before the door start to close when player are out of the trigger collider of the airlock.
+        /// </summary>
         private const float DOOR_WAIT_CLOSE_TIME = 2.0f;
 
         [SerializeField]
         private Animator _animator;
 
-        // This defines the animation we want to trigger
+        /// <summary>
+        /// The animation's id of the animation we want to trigger
+        /// </summary>
         private static readonly int OpenId = Animator.StringToHash("Open");
 
         [SerializeField] private LayerMask doorTriggerLayers = -1;
 
-
+        /// <summary>
+        /// Number of player close enough to the airlock.
+        /// </summary>
         private int playersInTrigger; // Server Only
+
+        /// <summary>
+        /// Coroutine to eventually close the door when no one is around.
+        /// </summary>
         private Coroutine closeTimer; // Server Only
 
 
