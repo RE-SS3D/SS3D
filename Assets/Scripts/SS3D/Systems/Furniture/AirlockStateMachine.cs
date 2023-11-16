a43a3e3c9b9abecc6e1582f0fc374e0f35cf18a5
stilnat <nathan.raynal.castang@gmail.com>
2023-11-16 21:54:51 +0100

    fix ambiguous reference

--- Assets/Scripts/SS3D/Systems/Furniture/AirlockStateMachine.cs
+++ Assets/Scripts/SS3D/Systems/Furniture/AirlockStateMachine.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using AudioType = SS3D.Systems.Audio.AudioType;
 
 namespace SS3D.Systems.Furniture
 {
