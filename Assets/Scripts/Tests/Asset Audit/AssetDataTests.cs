7547cedf55bf739614eb5354f715d0b7a9492567
stilnat <nathan.raynal.castang@gmail.com>
2023-11-18 11:46:10 +0100

    break tests

--- Assets/Scripts/Tests/Asset Audit/AssetDataTests.cs
+++ Assets/Scripts/Tests/Asset Audit/AssetDataTests.cs
@@ -46,6 +46,8 @@ namespace AssetAudit
             List<AssetDatabase> projectAssetDatabases = AssetDatabase.FindAllAssetDatabases();
             List<AssetDatabase> loadedAssetDatabases = _assetDatabaseSettings.IncludedAssetDatabases;
 
+            projectAssetDatabases = null;
+
             bool hasMissingDatabases = false;
             List<AssetDatabase> missingDatabases = new();
 
