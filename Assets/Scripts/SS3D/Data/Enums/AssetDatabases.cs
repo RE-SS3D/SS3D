208a24996868c4ef1cef36a83968171c302ab10d
stilnat <nathan.raynal.castang@gmail.com>
2023-11-19 14:05:37 +0100

    add material database

--- Assets/Scripts/SS3D/Data/Enums/AssetDatabases.cs
+++ Assets/Scripts/SS3D/Data/Enums/AssetDatabases.cs
@@ -6,10 +6,11 @@ namespace SS3D.Data.Enums
         CraftingRecipes = 1, 
         InteractionIcons = 2, 
         Items = 3, 
-        ParticlesEffects = 4, 
-        Scenes = 5, 
-        Settings = 6, 
-        UIElements = 7, 
+        Materials = 4, 
+        ParticlesEffects = 5, 
+        Scenes = 6, 
+        Settings = 7, 
+        UIElements = 8, 
     }
 }
 
