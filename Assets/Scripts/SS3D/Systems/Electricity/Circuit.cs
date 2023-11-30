b8152341379fb4c6bf5c98715d14e85523e22043
stilnat <nathan.raynal.castang@gmail.com>
2023-11-30 11:43:48 +0100

    basic setup

---
+++ Assets/Scripts/SS3D/Systems/Electricity/Circuit.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace System.Electricity
+{
+    public class Circuit
+    {
+        private List<IPowerConsumer> _consumers;
+        private List<IPowerProducer> _producer;
+
+
+
+
+        // when removing one element of the circuit, start a tree search for all connected elements to that element,
+        // accordingly updating circuits ?
+
+        public 
+
+        /// <summary>
+        /// Merge two small circuits into a big one.
+        /// </summary>
+        /// <param name="First"></param>
+        /// <param name="Second"></param>
+        private static void Merge(Circuit First, Circuit Second)
+        {
+
+        }
+
+        /// <summary>
+        /// Separate a big circuit into smaller ones.
+        /// </summary>
+        /// <param name="circuit"></param>
+        /// <param name="circuits"></param>
+        private static void Split(Circuit circuit, out List<Circuit> circuits)
+        {
+
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/SS3D/Systems/Electricity/ElectricitySystem.cs
@@ -0,0 +1,22 @@
+﻿using SS3D.Core.Behaviours;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace System.Electricity
+{
+    public class ElectricitySystem : NetworkSystem
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
