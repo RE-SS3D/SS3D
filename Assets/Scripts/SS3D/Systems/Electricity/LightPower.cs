c5ef0b5d3b4c2e46acdc9876f4bc1ae38cfca947
stilnat <nathan.raynal.castang@gmail.com>
2023-12-01 16:41:55 +0100

    add switch light script

---
+++ Assets/Scripts/SS3D/Systems/Electricity/LightPower.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace System.Electricity
+{
+    public class LightPower : MonoBehaviour
+    {
+        [SerializeField]
+        private BasicPowerConsumer _consumer;
+
+        [SerializeField]
+        private Light _light;
+
+        private float poweredIntensity;
+
+
+        void Start()
+        {
+            _consumer.OnPowerStatusUpdated += HandlePowerStatusUpdated;
+            poweredIntensity = _light.intensity;
+        }
+
+        private void HandlePowerStatusUpdated(object sender, PowerStatus newStatus)
+        {
+            if (newStatus == PowerStatus.Powered) TurnLightOn();
+            else TurnLightOff();
+
+        }
+
+        private void TurnLightOn()
+        {
+            _light.intensity = poweredIntensity;
+        }
+
+        private void TurnLightOff()
+        {
+            _light.intensity = 0;
+        }
+    }
+}
