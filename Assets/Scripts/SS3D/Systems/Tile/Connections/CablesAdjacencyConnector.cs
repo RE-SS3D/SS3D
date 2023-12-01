beacd7ca2791b5441361d6d950bdaf5a6c2b886a
stilnat <nathan.raynal.castang@gmail.com>
2023-12-01 16:42:13 +0100

    add electric connectors

---
+++ Assets/Scripts/SS3D/Systems/Tile/Connections/CablesAdjacencyConnector.cs
@@ -0,0 +1,115 @@
+﻿using SS3D.Logging;
+using SS3D.Systems.Tile;
+using SS3D.Systems.Tile.Connections.AdjacencyTypes;
+using SS3D.Systems.Tile.Connections;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FishNet.Object.Synchronizing;
+
+namespace SS3D.Systems.Tile.Connections
+{
+    public class CablesAdjacencyConnector : ElectricAdjacencyConnector
+    {
+
+        /// <summary>
+        /// A structure containing data regarding connection of this PlacedTileObject with all 8
+        /// adjacent neighbours (cardinal and diagonal connections).
+        /// </summary>
+        protected AdjacencyMap _adjacencyMap;
+
+        /// <summary>
+        /// The specific mesh this connectable has.
+        /// </summary>
+        protected MeshFilter _filter;
+
+        /// <summary>
+        /// Script that help with resolving the specific mesh a connectable should take.
+        /// </summary>
+        [SerializeField]
+        private SimpleConnector _adjacencyResolver;
+
+        /// <summary>
+        /// A byte, representing the 8 possible connections with neighbours.
+        /// </summary>
+        [SyncVar(OnChange = nameof(SyncAdjacencies))]
+        private byte _syncedConnections;
+
+        public override void UpdateAllConnections()
+        {
+            Setup();
+
+            var neighbourObjects = GetNeighbours();
+
+            bool changed = false;
+
+            foreach (var neighbourObject in neighbourObjects)
+            {
+                _placedObject.NeighbourAtDirectionOf(neighbourObject, out Direction dir);
+                changed |= UpdateSingleConnection(dir, neighbourObject, true);
+            }
+
+            if (changed)
+            {
+                UpdateMeshAndDirection();
+            }
+        }
+
+        public override bool UpdateSingleConnection(Direction dir, PlacedTileObject neighbourObject, bool updateNeighbour)
+        {
+            Setup();
+
+            bool isConnected = IsConnected(neighbourObject);
+
+            bool isUpdated = _adjacencyMap.SetConnection(dir, new AdjacencyData(TileObjectGenericType.None, TileObjectSpecificType.None, isConnected));
+
+            if (isUpdated)
+            {
+                neighbourObject?.UpdateSingleAdjacency(TileHelper.GetOpposite(dir), _placedObject, false);
+                _syncedConnections = _adjacencyMap.SerializeToByte();
+                UpdateMeshAndDirection();
+            }
+
+            return isUpdated;
+        }
+
+        /// <summary>
+        /// Update the current mesh of the game object this connector is onto, as well
+        /// as it's rotation.
+        /// </summary>
+        protected virtual void UpdateMeshAndDirection()
+        {
+
+            MeshDirectionInfo info = new();
+            info = _adjacencyResolver.GetMeshAndDirection(_adjacencyMap);
+
+            if (_filter == null)
+            {
+                Log.Warning(this, "Missing mesh {meshDirectionInfo}", Logs.Generic, info);
+            }
+
+            _filter.mesh = info.Mesh;
+
+            Quaternion localRotation = transform.localRotation;
+            Vector3 eulerRotation = localRotation.eulerAngles;
+            localRotation = Quaternion.Euler(eulerRotation.x, info.Rotation, eulerRotation.z);
+
+            transform.localRotation = localRotation;
+        }
+
+        /// <summary>
+        /// Sync adjacency map on client, and update mesh and direction using this new map.
+        /// </summary>
+        private void SyncAdjacencies(byte oldValue, byte newValue, bool asServer)
+        {
+            if (!asServer)
+            {
+                Setup();
+
+                _adjacencyMap.DeserializeFromByte(newValue);
+                UpdateMeshAndDirection();
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/SS3D/Systems/Tile/Connections/ElectricAdjacencyConnector.cs
@@ -0,0 +1,78 @@
+﻿using SS3D.Core;
+using SS3D.Core.Behaviours;
+using SS3D.Systems.Furniture;
+using SS3D.Systems.Tile;
+using SS3D.Systems.Tile.Connections;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public abstract class ElectricAdjacencyConnector : NetworkActor, IAdjacencyConnector
+{
+    /// <summary>
+    /// The placed object for this disposal pipe.
+    /// </summary>
+    protected PlacedTileObject _placedObject;
+
+    private bool _initialized;
+
+    protected void Setup()
+    {
+        if (!_initialized)
+        {
+            _placedObject = GetComponent<PlacedTileObject>();
+        }
+    }
+
+    public List<PlacedTileObject> GetNeighbours()
+    {
+        List<PlacedTileObject> neighbours = GetElectricDevicesOnSameTile();
+        neighbours.AddRange(GetNeighbourElectricDevicesOnSameLayer());
+        neighbours.RemoveAll(x => x == null);
+        return neighbours;
+    }
+
+    public bool IsConnected(PlacedTileObject neighbourObject)
+    {
+        return neighbourObject.Connector is ElectricAdjacencyConnector;
+    }
+
+    public abstract void UpdateAllConnections();
+
+    public abstract bool UpdateSingleConnection(Direction dir, PlacedTileObject neighbourObject, bool updateNeighbour);
+
+    private List<PlacedTileObject> GetElectricDevicesOnSameTile()
+    {
+        TileSystem tileSystem = Subsystems.Get<TileSystem>();
+        var map = tileSystem.CurrentMap;
+
+        List<PlacedTileObject> devicesOnSameTile = new();
+
+        TileChunk currentChunk = map.GetChunk(_placedObject.gameObject.transform.position);
+        List<ITileLocation> deviceLocations = currentChunk.GetTileLocations(_placedObject.Origin.x, _placedObject.Origin.y);
+
+        foreach(ITileLocation location in deviceLocations)
+        {
+            foreach(PlacedTileObject tileObject in location.GetAllPlacedObject())
+            {
+                if(tileObject.gameObject.TryGetComponent(out IElectricDevice device))
+                {
+                    devicesOnSameTile.Add(tileObject);
+                }
+            }
+        }
+
+        return devicesOnSameTile;
+    }
+
+    private List<PlacedTileObject> GetNeighbourElectricDevicesOnSameLayer()
+    {
+        List<PlacedTileObject> neighbours = new();
+        TileSystem tileSystem = Subsystems.Get<TileSystem>();
+        var map = tileSystem.CurrentMap;
+        neighbours.AddRange(map.GetNeighbourPlacedObjects(_placedObject.Layer,
+            _placedObject.gameObject.transform.position).Where(x => x.gameObject.TryGetComponent(out IElectricDevice device)));
+        return neighbours;
+    }
+}
