848aa6d2926438971138c24fc042def3ed736448
stilnat <nathan.raynal.castang@gmail.com>
2023-12-01 16:41:43 +0100

    add interface implementations

---
+++ Assets/Scripts/SS3D/Systems/Electricity/BasicPowerConsumer.cs
@@ -0,0 +1,45 @@
+﻿using FishNet.Object;
+using FishNet.Object.Synchronizing;
+using SS3D.Core;
+using SS3D.Systems.Tile;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace System.Electricity
+{
+    public class BasicPowerConsumer : NetworkBehaviour, IPowerConsumer
+    {
+        [SerializeField]
+        private float _powerConsumption = 1f;
+
+        [SyncVar(OnChange = nameof(SyncPowerStatus))]
+        private PowerStatus _powerStatus;
+        public float PowerNeeded => _powerConsumption;
+
+        public event EventHandler<PowerStatus> OnPowerStatusUpdated;
+
+        public PlacedTileObject TileObject => GetComponent<PlacedTileObject>();
+
+        public PowerStatus PowerStatus { get => _powerStatus; set => _powerStatus = value; }
+
+        void Start()
+        {
+            ElectricitySystem electricitySystem = Subsystems.Get<ElectricitySystem>();
+            electricitySystem.AddElectricalElement(this);
+        }
+
+        void Destroy()
+        {
+            ElectricitySystem electricitySystem = Subsystems.Get<ElectricitySystem>();
+            electricitySystem.RemoveElectricalElement(this);
+        }
+
+        private void SyncPowerStatus(PowerStatus oldValue, PowerStatus newValue, bool asServer)
+        {
+            OnPowerStatusUpdated.Invoke(this, newValue);
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/SS3D/Systems/Electricity/BasicPowerGenerator.cs
@@ -0,0 +1,29 @@
+﻿using SS3D.Core;
+using SS3D.Systems.Tile;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace System.Electricity
+{
+    public class BasicPowerGenerator : MonoBehaviour, IPowerProducer
+    {
+        [SerializeField]
+        private float _powerProduction = 10f;
+        public float PowerProduction => _powerProduction;
+
+        public PlacedTileObject TileObject => GetComponent<PlacedTileObject>();
+
+        void Start()
+        {
+            ElectricitySystem electricitySystem = Subsystems.Get<ElectricitySystem>();
+            electricitySystem.AddElectricalElement(this);
+        }
+
+        void Destroy()
+        {
+            ElectricitySystem electricitySystem = Subsystems.Get<ElectricitySystem>();
+            electricitySystem.RemoveElectricalElement(this);
+        }
+    }
+}
