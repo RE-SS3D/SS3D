7ba27cba7663e8d18e943d1d55e77b317024f352
stilnat <nathan.raynal.castang@gmail.com>
2023-12-01 17:05:36 +0100

    updating electric prefabs and adding battery script

---
+++ Assets/Scripts/SS3D/Systems/Electricity/BasicBattery.cs
@@ -0,0 +1,23 @@
+﻿using FishNet.Object;
+using SS3D.Systems.Tile;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace System.Electricity
+{
+    public class BasicBattery : NetworkBehaviour, IPowerStorage
+    {
+        private float _maxCapacity = 10;
+
+        private float _storedPower = 0;
+        public float StoredPower { get => _storedPower; set => _storedPower = value >= 0 ? MathF.Max(MaxCapacity, value) : MathF.Max(0f, value); }
+
+        public float MaxCapacity => _maxCapacity;
+
+        public float RemainingCapacity => _maxCapacity - _storedPower;
+
+        public PlacedTileObject TileObject => GetComponent<PlacedTileObject>();
+    }
+}
