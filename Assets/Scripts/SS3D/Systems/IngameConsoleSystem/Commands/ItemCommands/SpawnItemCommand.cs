5ece8c01e88afda5d84f6e3a31a1a080063161ed
Iamteapot <77565416+iamteapot422@users.noreply.github.com>
2023-12-01 19:53:55 +0700

    Improve console commands (#1354)
    * Refactor Command
    
    * Shorten some commands
    
    * Refactor commands
    
    * Fix little bug
    
    * Make comments
    
    * Replace structs with records
    
    * Delete unecessary scripts
    
    * Move IsExternalInit
    
    * do stuff
    
    * Change usage of hurt command
    
    ---------
    
    Co-authored-by: Mechar418 <77565416+Mechar418@users.noreply.github.com>

--- Assets/Scripts/SS3D/Systems/IngameConsoleSystem/Commands/ItemCommands/SpawnItemCommand.cs
+++ Assets/Scripts/SS3D/Systems/IngameConsoleSystem/Commands/ItemCommands/SpawnItemCommand.cs
@@ -12,47 +12,36 @@ namespace SS3D.Systems.IngameConsoleSystem.Commands.ItemCommands
 {
     public class SpawnItemCommand : Command
     {
-        public override string LongDescription => "Spawn item using item name at the same position as human or at position x,z";
+        public override string LongDescription => "Spawn item using item name at the same position as human";
+        public override string Usage => "(item name)";
         public override string ShortDescription => "Spawn item";
         public override ServerRoleTypes AccessLevel => ServerRoleTypes.User;
-
         public override CommandType Type => CommandType.Server;
+        
+        private record CalculatedValues(string ItemName) : ICalculatedValues;
 
         public override string Perform(string[] args, NetworkConnection conn)
         {
-            CheckArgsResponse checkArgsResponse = CheckArgs(args);
-            if (checkArgsResponse.IsValid == false)
-                return checkArgsResponse.InvalidArgs;
-            string itemName = args[0];
+            if (!ReceiveCheckResponse(args, out CheckArgsResponse response, out CalculatedValues values)) return response.InvalidArgs;
+            
             if(!Subsystems.Get<EntitySystem>().TryGetOwnedEntity(conn, out Entity entity))
-            {
                 return "Connection does not own any entity registered in entity system.";
-            };
-
+            
             ItemSystem itemSystem = Subsystems.Get<ItemSystem>();
-            itemSystem.CmdSpawnItem(itemName.ToEnum<ItemId>(), entity.transform.position, Quaternion.identity);
-            return $"item {itemName} spawned at position {entity.transform.position}";
+            itemSystem.CmdSpawnItem(values.ItemName.ToEnum<ItemId>(), entity.transform.position, Quaternion.identity);
+            return $"item {values.ItemName} spawned at position {entity.transform.position}";
         }
 
         protected override CheckArgsResponse CheckArgs(string[] args)
         {
-            CheckArgsResponse response = new CheckArgsResponse();
-            if (args.Length != 1 && args.Length != 3)
-            {
-                response.IsValid = false;
-                response.InvalidArgs = "Invalid number of arguments";
-                return response;
-            }
+            CheckArgsResponse response = new();
+            if (args.Length != 1 && args.Length != 3) return response.MakeInvalid("Invalid number of arguments");
+            
             string itemName = args[0];
-            if(!Assets.TryGet((int) AssetDatabases.Items, (int) itemName.ToEnum<ItemId>(), out Item item))
-            {
-                response.IsValid = false;
-                response.InvalidArgs = $"item with name {itemName} not found";
-                return response;
-            }
-          
-            response.IsValid = true;
-            return response;
+            if(!Assets.TryGet((int) AssetDatabases.Items, (int) itemName.ToEnum<ItemId>(), out Item _))
+                return response.MakeInvalid( $"item with name {itemName} not found");
+            
+            return response.MakeValid(new CalculatedValues(itemName));
         }
     }
 }
