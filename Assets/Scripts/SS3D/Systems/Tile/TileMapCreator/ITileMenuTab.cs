0607b0efd4a212b8d640bebd12485f991a084143
stilnat <nathan.raynal.castang@gmail.com>
2023-12-04 23:37:49 +0100

    add tilemenutab interface

---
+++ Assets/Scripts/SS3D/Systems/Tile/TileMapCreator/ITileMenuTab.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ITileMenuTab
+{
+    public void Clear();
+
+    public void Display();
+
+    public void Refresh();
+}
