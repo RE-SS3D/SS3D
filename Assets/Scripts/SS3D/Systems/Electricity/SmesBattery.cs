0c8ef32d776127703cd80d62bad9a7b5b5050767
stilnat <nathan.raynal.castang@gmail.com>
2023-12-06 17:04:21 +0100

    add smess batteries

---
+++ Assets/Scripts/SS3D/Systems/Electricity/SmesBattery.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Electricity;
+using UnityEngine;
+
+namespace System.Electricity
+{
+    public class SmesBattery : BasicBattery
+    {
+
+    }
+}
