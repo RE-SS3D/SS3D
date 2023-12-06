29d6b99eef705517d2ec6cf95ec4c90c998b30a9
stilnat <nathan.raynal.castang@gmail.com>
2023-12-06 16:02:48 +0100

    add togglable generator

---
+++ Assets/Scripts/SS3D/Systems/Electricity/FuelPowerGenerator.cs
@@ -0,0 +1,59 @@
+﻿using SS3D.Core;
+using SS3D.Data.Enums;
+using SS3D.Systems.Audio;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AudioType = SS3D.Systems.Audio.AudioType;
+
+namespace System.Electricity
+{
+    public class FuelPowerGenerator : BasicElectricDevice, IPowerProducer
+    {
+        [SerializeField]
+        private float _powerProduction = 10f;
+        public float PowerProduction => _powerProduction;
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+            GetComponent<FuelPowerGeneratorInteractionTarget>().OnGeneratorToggle += HandleGeneratorToggle;
+        }
+
+        private void HandleGeneratorToggle(bool enabled)
+        {
+            HandleSound(enabled);
+            HandleLights(enabled);
+            HandlePowerGenerated(enabled);
+        }
+
+        private void HandleSound(bool enabled)
+        {
+            if (!enabled)
+            {
+                Subsystems.Get<AudioSystem>().StopAudioSource(NetworkObject);
+            }
+            else
+            {
+                Subsystems.Get<AudioSystem>().PlayAudioSource(AudioType.music, SoundsIds.FuelPowerGenerator, Position, NetworkObject, 0.7f, 1, 1, 10);
+            }
+        }
+
+        private void HandleLights(bool enabled)
+        {
+            
+        }
+
+        private void HandlePowerGenerated(bool enabled)
+        {
+            if (!enabled)
+            {
+                _powerProduction = 0f;
+            }
+            else
+            {
+                _powerProduction = 10f;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SS3D/Systems/Electricity/FuelPowerGeneratorInteractionTarget.cs
@@ -0,0 +1,44 @@
+﻿using SS3D.Core;
+using SS3D.Data.Enums;
+using SS3D.Interactions;
+using SS3D.Interactions.Interfaces;
+using SS3D.Systems.Audio;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AudioType = SS3D.Systems.Audio.AudioType;
+
+namespace System.Electricity
+{
+    public class FuelPowerGeneratorInteractionTarget : InteractionTargetNetworkBehaviour, IToggleable
+    {
+
+        private bool _generatorOn;
+
+        public Action<bool> OnGeneratorToggle;
+
+        public override IInteraction[] CreateTargetInteractions(InteractionEvent interactionEvent)
+        {
+            List<IInteraction> interactions = new List<IInteraction>(1)
+            {
+                new ToggleInteraction()
+            };
+
+            return interactions.ToArray();
+        }
+
+        public bool GetState()
+        {
+            return _generatorOn;
+        }
+
+        public void Toggle()
+        {
+            _generatorOn = !_generatorOn;
+
+            OnGeneratorToggle?.Invoke(_generatorOn);
+
+           
+        }
+    }
+}
