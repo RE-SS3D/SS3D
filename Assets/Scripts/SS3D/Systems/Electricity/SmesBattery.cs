6946ba43a29aa7bebc9224f518d5f881da6bbbbf
stilnat <nathan.raynal.castang@gmail.com>
2023-12-07 12:34:44 +0100

    smes battery power level update

--- Assets/Scripts/SS3D/Systems/Electricity/SmesBattery.cs
+++ Assets/Scripts/SS3D/Systems/Electricity/SmesBattery.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using Coimbra.Services.Events;
+using Coimbra.Services.PlayerLoopEvents;
+using System.Collections;
 using System.Collections.Generic;
 using System.Electricity;
 using UnityEngine;
@@ -7,6 +9,39 @@ namespace System.Electricity
 {
     public class SmesBattery : BasicBattery
     {
+        [SerializeField]
+        private SkinnedMeshRenderer SmesSkinnedMesh;
+
+        private const string ChargeblendShapeName = "Charge"; 
+
+        public override void OnStartClient()
+        {
+            base.OnStartClient();
+            AddHandle(UpdateEvent.AddListener(HandleUpdate));
+        }
+
+        private void HandleUpdate(ref EventContext context, in UpdateEvent updateEvent)
+        {
+            AdjustVisualDisplay();
+        }
+
+
+        private void AdjustVisualDisplay()
+        {
+            int blendShapeIndex = SmesSkinnedMesh.sharedMesh.GetBlendShapeIndex(ChargeblendShapeName);
+
+            float chargeLevelNormalized = StoredPower / MaxCapacity;
+
+            if (blendShapeIndex != -1)
+            {
+                SmesSkinnedMesh.SetBlendShapeWeight(blendShapeIndex, chargeLevelNormalized*100);
+            }
+            else
+            {
+                Debug.LogError("Blend shape " + ChargeblendShapeName + " not found.");
+            }
+        }
+
 
     }
 }
