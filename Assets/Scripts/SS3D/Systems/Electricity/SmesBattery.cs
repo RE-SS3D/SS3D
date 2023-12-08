e7c0e84e8a6d7cf09e43c460eecd1beb6d9b1b65
stilnat <nathan.raynal.castang@gmail.com>
2023-12-09 00:40:26 +0100

    power rate display on SMES

--- Assets/Scripts/SS3D/Systems/Electricity/SmesBattery.cs
+++ Assets/Scripts/SS3D/Systems/Electricity/SmesBattery.cs
@@ -12,7 +12,22 @@ namespace System.Electricity
         [SerializeField]
         private SkinnedMeshRenderer SmesSkinnedMesh;
 
-        private const string ChargeblendShapeName = "Charge"; 
+        private const string ChargeblendShapeName = "Charge";
+
+        private const string OutputblendShapeName = "OutputMeter";
+
+        private float _previousPowerStored = 0f;
+
+        private int _currentLightOutput = 0;
+
+        private int _lightOutputTarget = 0;
+
+        [SerializeField]
+        private int _updateLightPeriod = 25;
+
+        private int _updateCount = 0;
+
+
 
         public override void OnStartClient()
         {
@@ -22,11 +37,12 @@ namespace System.Electricity
 
         private void HandleUpdate(ref EventContext context, in UpdateEvent updateEvent)
         {
-            AdjustVisualDisplay();
+            AdjustBatteryLevel();
+            AdjustBatteryOutput();
         }
 
 
-        private void AdjustVisualDisplay()
+        private void AdjustBatteryLevel()
         {
             int blendShapeIndex = SmesSkinnedMesh.sharedMesh.GetBlendShapeIndex(ChargeblendShapeName);
 
@@ -40,6 +56,44 @@ namespace System.Electricity
             {
                 Debug.LogError("Blend shape " + ChargeblendShapeName + " not found.");
             }
+        }
+
+        private void AdjustBatteryOutput()
+        {
+            ComputeLightOutputTarget();
+            _previousPowerStored = StoredPower;
+            _updateCount++;
+
+            if (_updateCount != _updateLightPeriod) return;
+
+            _updateCount = 0;
+
+            Debug.Log("target = " + _lightOutputTarget);
+            Debug.Log("current = " + _currentLightOutput);
+
+            if (_currentLightOutput < _lightOutputTarget)
+            {
+                _currentLightOutput += 1;
+                SmesSkinnedMesh.SetBlendShapeWeight(_currentLightOutput, 100);
+            }
+            else if(_currentLightOutput > _lightOutputTarget)
+            {
+                SmesSkinnedMesh.SetBlendShapeWeight(_currentLightOutput, 0);
+                _currentLightOutput -= 1;
+            }
+
+            
+        }
+
+        /// <summary>
+        /// Index of the light that should be turned on.
+        /// </summary>
+        private void ComputeLightOutputTarget()
+        {
+            float powerRemoved = Mathf.Max(_previousPowerStored - StoredPower, 0f);
+            float relativeRate = Mathf.Floor((powerRemoved / MaxPowerRate) * 10);
+
+            _lightOutputTarget = (int)relativeRate; // plus one because the blend shapes for light output start at index one.
         }
 
 
