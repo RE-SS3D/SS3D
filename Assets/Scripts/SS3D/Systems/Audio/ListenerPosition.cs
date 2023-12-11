4b2aa2fb9d41acc8c8130c4dcfdd72da2cd3c27d
stilnat <nathan.raynal.castang@gmail.com>
2023-12-11 23:33:34 +0100

    fix audio on camera (#1391)

---
+++ Assets/Scripts/SS3D/Systems/Audio/ListenerPosition.cs
@@ -0,0 +1,42 @@
+﻿using Coimbra.Services.Events;
+using Coimbra.Services.PlayerLoopEvents;
+using SS3D.Core.Behaviours;
+using SS3D.Systems.Entities.Events;
+using SS3D.Systems.Screens;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SS3D.Systems.Audio
+{
+    /// <summary>
+    /// Little script to position correctly the audio listener to be on the current player camera target.
+    /// </summary>
+    public class ListenerPosition : Actor
+    {
+        [SerializeField]
+        private AudioListener _listener;
+
+        [SerializeField]
+        private GameObject _listenerTarget;
+
+        protected override void OnAwake()
+        {
+            AddHandle(LocalPlayerObjectChanged.AddListener(HandlePlayerObjectChanged));
+            AddHandle(UpdateEvent.AddListener(HandleUpdate));
+        }
+
+        private void HandlePlayerObjectChanged(ref EventContext context, in LocalPlayerObjectChanged e)
+        {
+            _listenerTarget = e.PlayerObject;
+        }
+
+        private void HandleUpdate(ref EventContext context, in UpdateEvent updateEvent)
+        {
+            if(_listenerTarget != null)
+            {
+                _listener.transform.position = _listenerTarget.transform.position;
+            }
+        }
+    }
+}
