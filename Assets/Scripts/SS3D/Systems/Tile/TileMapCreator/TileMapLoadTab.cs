ce2e43a1938cba736bea4e6485b260afd1b515b0
stilnat <nathan.raynal.castang@gmail.com>
2023-12-11 01:28:44 +0100

    Switch between maps at run time (#1334)
    * get name of all files at path
    
    * add loading different maps
    
    * custom map save name
    
    * fixing merge
    
    * add fix
    
    * add network object
    
    * update construction menu
    
    * remove layer dropdown from save and load tabs
    
    * add overwrite check
    
    * add overwrite
    
    * add plenum and small station
    
    * add deleting maps
    
    * put some stuff back on save and load
    
    * add tilemenutab interface
    
    * use the tab interface
    
    * only one build tab and clean up the construction menu prefab
    
    * fix map name slot position (kind of)
    
    * fix initial tab
    
    * add controls off input field
    
    * try to make rename work
    
    * somewhat fix slot
    
    * fix save tab
    
    * some doc and qol
    
    * some doc
    
    * update doc
    
    * proper name for the maps
    
    * Delete Builds/Game/Data/Tilemaps/ffff.json
    
    * Delete Builds/Game/Data/Tilemaps/PlenumStation.json
    
    * Delete Builds/Game/Data/Tilemaps/ldddol.json
    
    * change font selected size and color of selection
    
    * beautify the menu (still ugly)
    
    * fix map name slot
    
    * beautify stuff
    
    * fix some more the construction menu
    
    * fix slot scale
    
    * fix build and text placement
    
    * small fix
    
    * fix merge issues
    
    * some clean up

---
+++ Assets/Scripts/SS3D/Systems/Tile/TileMapCreator/TileMapLoadTab.cs
@@ -0,0 +1,158 @@
+﻿using Coimbra;
+using SS3D.Core;
+using SS3D.Core.Behaviours;
+using SS3D.Data.Management;
+using SS3D.Logging;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace SS3D.Systems.Tile.TileMapCreator
+{
+    /// <summary>
+    /// Script to handle displaying the UI for the loading tab of the tilemap menu.
+    /// Also handle the logic of loading maps, removing maps and renaming them.
+    /// </summary>
+    public class TileMapLoadTab : NetworkActor, ITileMenuTab
+    {
+        [SerializeField]
+        private TileMapMenu _menu;
+
+        /// <summary>
+        /// A prefab containing an input field for the name of a map, 
+        /// </summary>
+        [SerializeField]
+        private GameObject _mapNameSlotPrefab;
+
+        [SerializeField]
+        private GameObject _loadMapContentRoot;
+
+        private const int FontSelectedSize = 18;
+        private const int FontUnSelectedSize = 15;
+
+        /// <summary>
+        /// Clear the load tab.
+        /// </summary>
+        public void Clear()
+        {
+            for (int i = 0; i < _loadMapContentRoot.transform.childCount; i++)
+            {
+                _loadMapContentRoot.transform.GetChild(i).gameObject.Dispose(true);
+            }
+        }
+
+        /// <summary>
+        /// Display the Load tab, including showing the names of all existing maps.
+        /// </summary>
+        public void Display()
+        {
+            var MapNames = SaveSystem.GetAllObjectsNameInFolder(Subsystems.Get<TileSystem>().SavePath);
+
+            foreach (string mapName in MapNames)
+            {
+                string mapNameWithNoExtension = mapName.Substring(0, mapName.IndexOf("."));
+                GameObject slot = Instantiate(_mapNameSlotPrefab, _loadMapContentRoot.transform, true);
+
+                // I've no idea why but something modify the slots scale so it's necessary to adjust it here.
+                slot.transform.localScale= Vector3.one;
+
+                MapNameSlot mapNameSlot = slot.GetComponent<MapNameSlot>();
+                mapNameSlot.MapNameField.readOnly = true;
+                mapNameSlot.MapNameField.text = mapNameWithNoExtension;
+
+                mapNameSlot.MapNameField.onSelect.AddListener((string x) => LoadMap(mapNameWithNoExtension));
+                mapNameSlot.DeleteButton.onClick.AddListener(() => DeleteMap(mapNameWithNoExtension));
+                mapNameSlot.RenameButton.onClick.AddListener(() => Rename(mapNameSlot.MapNameField));
+                slot.gameObject.SetActive(true);
+            }
+        }
+
+        /// <summary>
+        /// Refresh the load tab.
+        /// </summary>
+        public void Refresh()
+        {
+            Clear();
+            Display();
+        }
+
+        /// <summary>
+        /// Delete the map with name mapName.
+        /// </summary>
+        /// <param name="mapName"></param>
+        private void DeleteMap(string mapName)
+        {
+            if (IsServer)
+            {
+                SaveSystem.DeleteFile(Subsystems.Get<TileSystem>().SavePath + "/" + mapName);
+            }
+            else
+            {
+                Log.Information(this, "Cannot load the map on a client");
+            }
+
+            Refresh();
+        }
+
+        /// <summary>
+        /// Load the map with name mapName
+        /// </summary>
+        /// <param name="mapName"></param>
+        private void LoadMap(string mapName)
+        {
+            if (IsServer)
+            {
+                Subsystems.Get<TileSystem>().Load(Subsystems.Get<TileSystem>().SavePath + "/" + mapName);
+            }
+            else
+            {
+                Log.Information(this, "Cannot load the map on a client");
+            }
+        }
+
+        /// <summary>
+        /// Allow the name field to be edited, and set up listener so that at the end of edit, the name changes.
+        /// </summary>
+        /// <param name="mapNameField"> The input field containing the map to rename.</param>
+        private void Rename(ControlsOffInputField mapNameField)
+        {
+            string oldName = mapNameField.text;
+            mapNameField.readOnly = false;
+            mapNameField.onSelect.RemoveAllListeners();
+            mapNameField.ActivateInputField();
+            mapNameField.onEndEdit.AddListener((string x) => RenameSave(mapNameField, oldName));
+            mapNameField.onDeselect.AddListener((string x) => RenameSave(mapNameField, oldName));
+            mapNameField.textComponent.fontSize = FontSelectedSize;
+        }
+
+
+        /// <summary>
+        /// Does the renaming at the end of edit, when clicking away from the input field.
+        /// </summary>
+        /// <param name="mapNameField">The input field containing the map to rename.</param>
+        /// <param name="oldName"> The old name of the map</param>
+        private void RenameSave(ControlsOffInputField mapNameField, string oldName)
+        {
+            mapNameField.textComponent.fontSize = FontUnSelectedSize;
+
+            mapNameField.onSelect.RemoveAllListeners();
+            mapNameField.readOnly = true;
+
+            if (Subsystems.Get<TileSystem>().MapNameAlreadyExist(mapNameField.text))
+            {
+                mapNameField.text = oldName;
+            }
+            else
+            {
+                string savePath = Subsystems.Get<TileSystem>().SavePath;
+                SaveSystem.RenameFile(savePath + "/" + oldName, savePath + "/" + mapNameField.text);
+            }
+
+            Refresh();
+
+            mapNameField.onSelect.AddListener(delegate { LoadMap(mapNameField.text); });
+           
+        }
+    }
+}
---
+++ Assets/Scripts/SS3D/Systems/Tile/TileMapCreator/TileMapSaveTab.cs
@@ -0,0 +1,141 @@
+﻿using SS3D.Core;
+using SS3D.Core.Behaviours;
+using SS3D.Data.Management;
+using SS3D.Logging;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace SS3D.Systems.Tile.TileMapCreator
+{
+    /// <summary>
+    /// Script to handle displaying the UI for the saving tab of the tilemap menu.
+    /// Also handle the logic of saving maps.
+    /// </summary>
+    public class TileMapSaveTab : NetworkActor, ITileMenuTab
+    {
+        [SerializeField]
+        private TileMapMenu _menu;
+
+        [SerializeField]
+        private GameObject _confirmOverWriteButton;
+
+        [SerializeField]
+        private ControlsOffInputField _saveInputField;
+
+        [SerializeField]
+        private GameObject _saveMapContentRoot;
+
+        [SerializeField]
+        private TextMeshProUGUI _saveText;
+
+        [SerializeField]
+        private float _fadeDuration;
+
+        /// <summary>
+        /// Clear the save tab.
+        /// </summary>
+        public void Clear()
+        {
+            _confirmOverWriteButton.gameObject.SetActive(false);
+            _saveMapContentRoot.SetActive(false);
+            _saveText.gameObject.SetActive(false);
+        }
+
+        /// <summary>
+        /// Display the save tab.
+        /// </summary>
+        public void Display()
+        {
+            _saveMapContentRoot.gameObject.SetActive(true);
+            _confirmOverWriteButton.gameObject.SetActive(false);
+            _saveMapContentRoot.SetActive(true);
+        }
+
+        /// <summary>
+        /// Refresh the save tab.
+        /// </summary>
+        public void Refresh()
+        {
+            Clear();
+            Display();
+        }
+
+        /// <summary>
+        /// Method called when the confirm overwrite button is clicked.
+        /// </summary>
+        public void HandleConfirmOverWriteButton()
+        {
+            SaveMap(_saveInputField.text);
+            _confirmOverWriteButton.gameObject.SetActive(false);
+        }
+
+        /// <summary>
+        /// Method called when the save button is clicked.
+        /// </summary>
+        public void HandleSaveMapButton()
+        {
+            if (Subsystems.Get<TileSystem>().MapNameAlreadyExist(_saveInputField.text))
+            {
+                _confirmOverWriteButton.gameObject.SetActive(true);
+            }
+            else
+            {
+                SaveMap(_saveInputField.text);
+            }
+        }
+
+        /// <summary>
+        /// Save a map, with name mapName
+        /// </summary>
+        /// <param name="mapName"></param>
+        private void SaveMap(string mapName)
+        {
+            if (IsServer)
+            {
+                Subsystems.Get<TileSystem>().Save(mapName, true);
+                DisplaySaveText(mapName);
+            }
+            else
+            {
+                Log.Information(this, "Cannot save the map on a client");
+            }
+        }
+
+        /// <summary>
+        /// Display some text to inform player the map is saved.
+        /// </summary>
+        /// <param name="mapName"> name of the map.</param>
+        private void DisplaySaveText(string mapName)
+        {
+            _saveText.text = "map " + mapName + " saved !";
+            _saveText.gameObject.SetActive(true);
+            StartCoroutine(FadeText());
+        }
+
+        private IEnumerator FadeText()
+        {
+            Color originalColor = _saveText.color;
+
+            float currentTime = 0f;
+
+            while (currentTime < _fadeDuration)
+            {
+                currentTime += Time.deltaTime;
+
+                // Calculate the alpha value based on the current time and duration
+                float alpha = Mathf.Lerp(1f, 0f, currentTime / _fadeDuration);
+
+                // Set the text's alpha to the calculated value
+                _saveText.color = new Color(originalColor.r, originalColor.g, originalColor.b, alpha);
+
+                yield return null;
+            }
+
+            _saveText.gameObject.SetActive(false);
+        }
+
+
+    }
+}
