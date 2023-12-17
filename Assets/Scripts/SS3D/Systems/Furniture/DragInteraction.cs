55363652cc934680c3739a86c96b95928cfa58c3
stilnat <nathan.raynal.castang@gmail.com>
2023-12-17 01:21:59 +0100

    partial drag stuff around

---
+++ Assets/Scripts/SS3D/Systems/Furniture/DragInteraction.cs
@@ -0,0 +1,63 @@
+﻿using SS3D.Data.Generated;
+using SS3D.Interactions;
+using SS3D.Interactions.Extensions;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace SS3D.Systems.Furniture
+{
+    public class DragInteraction : Interaction
+    {
+        /// <summary>
+        /// If the interaction should be range limited
+        /// </summary>
+        public bool RangeCheck { get; set; } = true;
+
+        public DragInteraction(float delay, Transform characterTransform)
+        {
+            _characterTransform = characterTransform;
+            _startPosition = characterTransform.position;
+            Delay = delay;
+        }
+
+
+        public override string GetName(InteractionEvent interactionEvent)
+        {
+            if (interactionEvent.Target.GetGameObject().TryGetComponent<Draggable>(out var draggable))
+            {
+                return draggable.Dragged ? "Drop" : "Drag";
+            }
+
+            return null;
+        }
+
+        public override Sprite GetIcon(InteractionEvent interactionEvent)
+        {
+            return InteractionIcons.Discard;
+        }
+
+        public override bool CanInteract(InteractionEvent interactionEvent)
+        {
+            if (RangeCheck && !InteractionExtensions.RangeCheck(interactionEvent))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public override bool Start(InteractionEvent interactionEvent, InteractionReference reference)
+        {
+            if (!interactionEvent.Target.GetGameObject().TryGetComponent<Draggable>(out var draggable))
+            {
+                return false;
+            }
+
+            draggable
+
+            return false;
+        }
+    }
+}
---
+++ Assets/Scripts/SS3D/Systems/Furniture/Draggable.cs
@@ -0,0 +1,50 @@
+﻿using SS3D.Interactions;
+using SS3D.Interactions.Interfaces;
+using SS3D.Systems.Furniture;
+using SS3D.Systems.Inventory.Interactions;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Draggable : MonoBehaviour, IInteractionTarget
+{
+    /// <summary>
+    /// The thing that drag this object
+    /// </summary>
+    [SerializeField]
+    private Transform _dragger;
+
+    /// <summary>
+    /// The distance at which the dragged item should stay from dragger
+    /// </summary>
+    [SerializeField]
+    private float _distanceFromDragger = 1f;
+
+    /// <summary>
+    /// True if the object is currently being dragged.
+    /// </summary>
+    private bool _dragged;
+
+    public bool Dragged => _dragged;
+
+    public IInteraction[] CreateTargetInteractions(InteractionEvent interactionEvent)
+    {
+        DragInteraction dragInteraction = new DragInteraction();
+
+        return new IInteraction[] { dragInteraction };
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_dragged) return;
+        
+        gameObject.transform.position = _dragger.transform.forward * _distanceFromDragger;
+    }
+}
