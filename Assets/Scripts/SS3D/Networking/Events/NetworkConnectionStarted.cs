85663773afd9ece9c682fadd342fd6d214314f50
João Buratto <joaoburattoc@gmail.com>
2024-01-20 15:14:49 -0300

    Remove unused class

--- Assets/Scripts/SS3D/Networking/Events/NetworkConnectionStarted.cs
+++
@@ -1,9 +0,0 @@
-﻿using Coimbra.Services.Events;
-
-namespace SS3D.Networking.Events
-{
-    /// <summary>
-    /// Called when a network connection is stablished or started.
-    /// </summary>
-    public partial struct NetworkConnectionStarted : IEvent { }
-}
