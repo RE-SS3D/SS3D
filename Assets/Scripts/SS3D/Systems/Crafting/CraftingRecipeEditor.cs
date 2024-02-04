34e75a0d89a08f7732a2932809b789da052a2490
stilnat <nathan.raynal.castang@gmail.com>
2024-02-04 19:40:00 +0100

    make algorithm generic

--- Assets/Scripts/SS3D/Systems/Crafting/CraftingRecipeEditor.cs
+++ Assets/Scripts/SS3D/Systems/Crafting/CraftingRecipeEditor.cs
@@ -10,6 +10,9 @@ using Codice.Client.BaseCommands.TubeClient;
 using System;
 using QuikGraph.Algorithms;
 using UnityEditor.PackageManager.UI;
+using SS3D.Utils;
+using UnityEngine.UIElements;
+using SS3D.Substances;
 
 /// <summary>
 /// Custom window to display the recipe graph of a recipe opened in inspector.
@@ -28,7 +31,7 @@ public class CraftingRecipeEditor : EditorWindow
 
     private float _forceToStop = 0.1f;
 
-    private AdjacencyGraph<RecipeStepWithPosition, TaggedEdge<RecipeStepWithPosition, RecipeStepLink>> _graphWithPosition;
+    private AdjacencyGraph<VerticeWithPosition<RecipeStep>, TaggedEdge<VerticeWithPosition<RecipeStep>, RecipeStepLink>> _graphWithPosition;
 
     private const float KZoomMin = 0.1f;
     private const float KZoomMax = 10.0f;
@@ -69,9 +72,13 @@ public class CraftingRecipeEditor : EditorWindow
 
             if (recipe == null) return;
 
-            _graphWithPosition = InitializeGraphWithPositions(recipe.RecipeGraph);
+            //_graphWithPosition = InitializeGraphWithPositions(recipe.RecipeGraph);
 
-            EditorCoroutineUtility.StartCoroutine(SpringEmbedderAlgorithm(_graphWithPosition), this);
+
+
+            EditorCoroutineUtility.StartCoroutine(Start(recipe), this);
+
+            //EditorCoroutineUtility.StartCoroutine(SpringEmbedderAlgorithm(_graphWithPosition), this);
         }
 
 
@@ -81,7 +88,25 @@ public class CraftingRecipeEditor : EditorWindow
         _idealLenght = EditorGUILayout.Slider("Ideal lenght", _idealLenght, 0, 800);
         _delta = EditorGUILayout.Slider("Delta", _delta, 0, 50);
         _forceToStop = EditorGUILayout.Slider("Max force to stop", _forceToStop, 0.001f, 5);
-        _maxIteration = Math.Max(100, EditorGUILayout.IntField("Max iteration", _maxIteration)); 
+        _maxIteration = Math.Max(100, EditorGUILayout.IntField("Max iteration", _maxIteration));
+    }
+
+    public IEnumerator Start(CraftingRecipe recipe)
+    {
+        _graphWithPosition =
+    SpringEmbedderAlgorithm<RecipeStep, TaggedEdge<RecipeStep, RecipeStepLink>, RecipeStepLink>.InitializeGraphWithPositions(recipe.RecipeGraph);
+
+        int t = 0;
+
+        while (t < _maxIteration)
+        {
+            SpringEmbedderAlgorithm<RecipeStep, TaggedEdge<RecipeStep, RecipeStepLink>, RecipeStepLink>.SetParameters(_repulsiveConstant, _attractiveConstant,
+                _idealLenght, _delta, _forceToStop);
+            SpringEmbedderAlgorithm<RecipeStep, TaggedEdge<RecipeStep, RecipeStepLink>, RecipeStepLink>.ComputeOneStep(_graphWithPosition);
+            t++;
+            Repaint();
+            yield return null;
+        }
     }
 
     private void HandleEvents()
@@ -141,169 +166,32 @@ public class CraftingRecipeEditor : EditorWindow
         DrawZoomArea();
         DrawNonZoomArea();
 
+        
+
     }
 
-    /// <summary>
-    /// Create a new graph very similar to the one passed in parameter, but enhance with position data, and initialize those positions
-    /// in a random circle.
-    /// </summary>
-    /// <returns></returns>
-    private AdjacencyGraph<RecipeStepWithPosition, TaggedEdge<RecipeStepWithPosition, RecipeStepLink>> InitializeGraphWithPositions(
-        AdjacencyGraph<RecipeStep, TaggedEdge<RecipeStep, RecipeStepLink>> originalGraph)
-    {
-        AdjacencyGraph<RecipeStepWithPosition, TaggedEdge<RecipeStepWithPosition, RecipeStepLink>> graphWithPosition = new();
-
-        // make a new graph giving positions to vertices.
-        foreach (TaggedEdge<RecipeStep, RecipeStepLink> edge in originalGraph.Edges)
-        {
-            RecipeStepWithPosition source = graphWithPosition.Vertices.FirstOrDefault(x => x.step == edge.Source);
-            RecipeStepWithPosition target = graphWithPosition.Vertices.FirstOrDefault(x => x.step == edge.Target);
-
-            TaggedEdge<RecipeStepWithPosition, RecipeStepLink> edgeWithPosition = new TaggedEdge<RecipeStepWithPosition, RecipeStepLink>(
-                source != null ? source : new RecipeStepWithPosition() { step = edge.Source, position = GetRandomCirclePosition() },
-                target != null ? target : new RecipeStepWithPosition() { step = edge.Target, position = GetRandomCirclePosition() },
-                edge.Tag
-                );
-            graphWithPosition.AddVerticesAndEdge(edgeWithPosition);
-        }
-
-        return graphWithPosition;
-    }
-
-    private void DrawGraph(AdjacencyGraph<RecipeStepWithPosition, TaggedEdge<RecipeStepWithPosition, RecipeStepLink>> graphWithPosition)
+    private void DrawGraph(AdjacencyGraph<VerticeWithPosition<RecipeStep>, TaggedEdge<VerticeWithPosition<RecipeStep>, RecipeStepLink>> graphWithPosition)
     {
         // draw the graph
-        foreach (RecipeStepWithPosition stepWithPosition in graphWithPosition.Vertices)
+        foreach (VerticeWithPosition<RecipeStep> stepWithPosition in graphWithPosition.Vertices)
         {
-            Color color = stepWithPosition.step.IsTerminal ? Color.red : stepWithPosition.step.IsInitialState ? Color.green : Color.gray;
+            Color color = stepWithPosition.vertice.IsTerminal ? Color.red : stepWithPosition.vertice.IsInitialState ? Color.green : Color.gray;
             Handles.color = color;
             Handles.DrawSolidDisc(new Vector3(stepWithPosition.position.x, stepWithPosition.position.y, 0), Vector3.forward, CircleSize);
             Handles.color = Color.black;
             Handles.DrawWireDisc(new Vector3(stepWithPosition.position.x, stepWithPosition.position.y, 0), Vector3.forward, CircleSize);
 
-            EditorGUI.LabelField(new Rect(stepWithPosition.position.x, stepWithPosition.position.y, 200, 20), stepWithPosition.step.Name);
+            EditorGUI.LabelField(new Rect(stepWithPosition.position.x, stepWithPosition.position.y, 200, 20), stepWithPosition.vertice.Name);
         }
         Handles.color = Color.white;
 
 
-        foreach (TaggedEdge<RecipeStepWithPosition, RecipeStepLink> edge in graphWithPosition.Edges)
+        foreach (TaggedEdge<VerticeWithPosition<RecipeStep>, RecipeStepLink> edge in graphWithPosition.Edges)
         {
             Handles.DrawAAPolyLine(3, edge.Source.position, edge.Target.position);
 
             DrawArrowhead(edge.Source.position, edge.Target.position, 20, 8f);
         }
-    }
-
-    private Vector2 GetRandomCirclePosition()
-    {
-        return UnityEngine.Random.insideUnitCircle * 300 + new Vector2(400, 400);
-    }
-
-    private IEnumerator SpringEmbedderAlgorithm(AdjacencyGraph<RecipeStepWithPosition, TaggedEdge<RecipeStepWithPosition, RecipeStepLink>> graph)
-    {
-        
-        Tuple<RecipeStepWithPosition, Vector2>[] forcesOnVertices = ComputeForceAllVertices(graph);
-
-        int t = 0;
-
-        while (t < _maxIteration)
-        {
-
-            forcesOnVertices = ComputeForceAllVertices(graph);
-
-            Vector2 maxForce = forcesOnVertices.Select(x => x.Item2).Aggregate(Vector2.zero,
-                (current, next) => current.magnitude > next.magnitude ? current : next,
-                result => result);
-
-            if(maxForce.magnitude < _forceToStop) { break; }
-
-            for(int j =0; j< forcesOnVertices.Length; j++)
-            {
-                forcesOnVertices[j].Item1.position += _delta * forcesOnVertices[j].Item2;
-            }
-
-            t++;
-            yield return null;
-            Repaint();
-        }
-    }
-
-    private Tuple<RecipeStepWithPosition,Vector2>[] ComputeForceAllVertices (AdjacencyGraph<RecipeStepWithPosition, TaggedEdge<RecipeStepWithPosition, RecipeStepLink>> graph)
-    {
-        Tuple<RecipeStepWithPosition, Vector2>[] forcesOnVertice = new Tuple<RecipeStepWithPosition, Vector2>[graph.VertexCount];
-        int i = 0;
-        foreach(RecipeStepWithPosition vertice in graph.Vertices)
-        {
-            forcesOnVertice[i] = new Tuple<RecipeStepWithPosition, Vector2>(vertice, ComputeForceSingleVertice(vertice, graph));
-            i++;
-        }
-
-        return forcesOnVertice;
-    }
-
-    private Vector2 ComputeForceSingleVertice(RecipeStepWithPosition vertice,
-        AdjacencyGraph<RecipeStepWithPosition, TaggedEdge<RecipeStepWithPosition, RecipeStepLink>> graph)
-    {
-        // Issue, should not be only out edges, but also "in" edges.
-        // Idea : go trough the list of edges, keep all target or source vertice when current vertice is the edge.
-
-        List<RecipeStepWithPosition> conVertices = new List<RecipeStepWithPosition>();
-
-        foreach(TaggedEdge<RecipeStepWithPosition, RecipeStepLink> edge in graph.Edges)
-        {
-            if(edge.Target == vertice && edge.Source != vertice) 
-            {
-                conVertices.Add(edge.Source);
-            }
-            else if(edge.Source== vertice && edge.Target != vertice)
-            {
-                conVertices.Add(edge.Target);
-            }
-        }
-
-        IEnumerable<RecipeStepWithPosition> connectedVertices = graph.OutEdges(vertice).Select(x => x.Target);
-        List<RecipeStepWithPosition> currentVertice = new List<RecipeStepWithPosition>() { vertice };
-
-        IEnumerable<RecipeStepWithPosition> unconnectedVertices = graph.Vertices.Except(currentVertice) ;
-
-        //var notOutVertices = graph.Edges.Except(outVertices);
-        Vector2 attractive = ComputeAttractiveComponent(vertice, conVertices);
-        Vector2 repulsive = ComputeRepulsiveComponent(vertice, unconnectedVertices);
-
-        return attractive + repulsive;
-    }
-
-    private Vector2 ComputeAttractiveComponent(RecipeStepWithPosition vertice, IEnumerable<RecipeStepWithPosition> vertices)
-    {
-        Vector2 result = Vector2.zero;
-        
-        foreach (RecipeStepWithPosition target in vertices)
-        {
-            result += AttractiveForce(vertice.position, target.position);
-        }
-
-        return result;
-    }
-
-    private Vector2 ComputeRepulsiveComponent(RecipeStepWithPosition vertice, IEnumerable<RecipeStepWithPosition> vertices)
-    {
-        Vector2 result = Vector2.zero;
-        foreach (RecipeStepWithPosition target in vertices)
-        {
-            result += RepulsiveForce(vertice.position, target.position);
-        }
-
-        return result;
-    }
-
-    private Vector2 RepulsiveForce(Vector2 v1, Vector2 v2)
-    {
-        return (_repulsiveConstant/(Vector2.Distance(v1,v2) * Vector2.Distance(v1, v2))) * (v1 - v2).normalized;
-    }
-
-    private Vector2 AttractiveForce(Vector2 v1, Vector2 v2)
-    {
-        return (_attractiveConstant * Mathf.Log(Vector2.Distance(v1, v2)) / _idealLenght)* (v2 - v1).normalized;
     }
 
     // Helper method to draw arrowhead
---
+++ Assets/Scripts/SS3D/Utils/SpringEmbedderAlgorithm.cs
@@ -0,0 +1,167 @@
+﻿using QuikGraph;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace SS3D.Utils
+{
+
+    public class VerticeWithPosition<TVertex>
+    {
+        public TVertex vertice;
+        public Vector2 position;
+    }
+
+    public static class SpringEmbedderAlgorithm<TVertex, TEdge, TTag>
+    {
+
+        private static float RepulsiveConstant = 1;
+
+        private static float AttractiveConstant = 1;
+
+        private static float IdealLenght = 80;
+
+        private static float Delta = 1f;
+
+        private static float ForceToStop = 0.1f;
+
+        public static void SetParameters(float repulsive, float attractive, float idealLenght, float delta, float forceToStop)
+        {
+            RepulsiveConstant= repulsive;
+            AttractiveConstant= attractive;
+            IdealLenght = idealLenght;
+            ForceToStop = forceToStop;
+            Delta = delta;
+        }
+
+        public static AdjacencyGraph<VerticeWithPosition<TVertex>, TaggedEdge<VerticeWithPosition<TVertex>, TTag>> InitializeGraphWithPositions(
+            AdjacencyGraph<TVertex, TaggedEdge<TVertex, TTag>> originalGraph)
+        {
+            AdjacencyGraph<VerticeWithPosition<TVertex>, TaggedEdge<VerticeWithPosition<TVertex>, TTag>> graphWithPosition = new();
+
+            // make a new graph giving positions to vertices.
+            foreach (TaggedEdge<TVertex, TTag> edge in originalGraph.Edges)
+            {
+                VerticeWithPosition<TVertex> source = graphWithPosition.Vertices.FirstOrDefault(x => x.vertice.Equals(edge.Source));
+                VerticeWithPosition<TVertex> target = graphWithPosition.Vertices.FirstOrDefault(x => x.vertice.Equals(edge.Target));
+
+
+
+                TaggedEdge<VerticeWithPosition<TVertex>, TTag> edgeWithPosition = new TaggedEdge<VerticeWithPosition<TVertex>, TTag>(
+                    source != null ? source : new VerticeWithPosition<TVertex>() { vertice = edge.Source, position = GetRandomCirclePosition() },
+                    target != null ? target : new VerticeWithPosition<TVertex>() { vertice = edge.Target, position = GetRandomCirclePosition() },
+                    edge.Tag
+                    );
+                graphWithPosition.AddVerticesAndEdge(edgeWithPosition);
+            }
+
+            return graphWithPosition;
+        }
+
+        private static Vector2 GetRandomCirclePosition()
+        {
+            return UnityEngine.Random.insideUnitCircle * 300 + new Vector2(400, 400);
+        }
+
+        public static void ComputeOneStep(
+            AdjacencyGraph<VerticeWithPosition<TVertex>, TaggedEdge<VerticeWithPosition<TVertex>, TTag>> graphWithPosition)
+        {
+
+            Tuple<VerticeWithPosition<TVertex>, Vector2>[] forcesOnVertices = ComputeForceAllVertices(graphWithPosition);
+
+            Vector2 maxForce = forcesOnVertices.Select(x => x.Item2).Aggregate(Vector2.zero,
+                (current, next) => current.magnitude > next.magnitude ? current : next,
+                result => result);
+
+            if (maxForce.magnitude < ForceToStop) { return; }
+
+            for (int j = 0; j < forcesOnVertices.Length; j++)
+            {
+                forcesOnVertices[j].Item1.position += Delta * forcesOnVertices[j].Item2;
+            }
+        }
+
+        private static Tuple<VerticeWithPosition<TVertex>, Vector2>[] ComputeForceAllVertices(
+            AdjacencyGraph<VerticeWithPosition<TVertex>, TaggedEdge<VerticeWithPosition<TVertex>, TTag>> graph)
+        {
+            Tuple<VerticeWithPosition<TVertex>, Vector2>[] forcesOnVertice = new Tuple<VerticeWithPosition<TVertex>, Vector2>[graph.VertexCount];
+            int i = 0;
+            foreach (VerticeWithPosition<TVertex> vertice in graph.Vertices)
+            {
+                forcesOnVertice[i] = new Tuple<VerticeWithPosition<TVertex>, Vector2>(vertice, ComputeForceSingleVertice(vertice, graph));
+                i++;
+            }
+
+            return forcesOnVertice;
+        }
+
+        private static Vector2 ComputeForceSingleVertice(VerticeWithPosition<TVertex> vertice,
+            AdjacencyGraph<VerticeWithPosition<TVertex>, TaggedEdge<VerticeWithPosition<TVertex>, TTag>> graph)
+        {
+            // Issue, should not be only out edges, but also "in" edges.
+            // Idea : go trough the list of edges, keep all target or source vertice when current vertice is the edge.
+
+            List<VerticeWithPosition<TVertex>> conVertices = new List<VerticeWithPosition<TVertex>>();
+
+            foreach (Edge<VerticeWithPosition<TVertex>> edge in graph.Edges)
+            {
+                if (edge.Target == vertice && edge.Source != vertice)
+                {
+                    conVertices.Add(edge.Source);
+                }
+                else if (edge.Source == vertice && edge.Target != vertice)
+                {
+                    conVertices.Add(edge.Target);
+                }
+            }
+
+            List<VerticeWithPosition<TVertex>> currentVertice = new List<VerticeWithPosition<TVertex>>() { vertice };
+
+            IEnumerable<VerticeWithPosition<TVertex>> unconnectedVertices = graph.Vertices.Except(currentVertice);
+
+            //var notOutVertices = graph.Edges.Except(outVertices);
+            Vector2 attractive = ComputeAttractiveComponent(vertice, conVertices);
+            Vector2 repulsive = ComputeRepulsiveComponent(vertice, unconnectedVertices);
+
+            return attractive + repulsive;
+        }
+
+        private static Vector2 ComputeAttractiveComponent(VerticeWithPosition<TVertex> vertice, IEnumerable<VerticeWithPosition<TVertex>> vertices)
+        {
+            Vector2 result = Vector2.zero;
+
+            foreach (VerticeWithPosition<TVertex> target in vertices)
+            {
+                result += AttractiveForce(vertice.position, target.position);
+            }
+
+            return result;
+        }
+
+        private static Vector2 ComputeRepulsiveComponent(VerticeWithPosition<TVertex> vertice, IEnumerable<VerticeWithPosition<TVertex>> vertices)
+        {
+            Vector2 result = Vector2.zero;
+            foreach (VerticeWithPosition<TVertex> target in vertices)
+            {
+                result += RepulsiveForce(vertice.position, target.position);
+            }
+
+            return result;
+        }
+
+        private static Vector2 RepulsiveForce(Vector2 v1, Vector2 v2)
+        {
+            return (RepulsiveConstant / (Vector2.Distance(v1, v2) * Vector2.Distance(v1, v2))) * (v1 - v2).normalized;
+        }
+
+        private static Vector2 AttractiveForce(Vector2 v1, Vector2 v2)
+        {
+            return (AttractiveConstant * Mathf.Log(Vector2.Distance(v1, v2)) / IdealLenght) * (v2 - v1).normalized;
+        }
+
+
+    }
+}
