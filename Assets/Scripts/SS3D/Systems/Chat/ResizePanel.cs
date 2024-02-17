aa14c22abf052a6bd4eb291ca0f5e6eac03aa7b5
David Gómez <david.gomez.rooiz@gmail.com>
2024-02-18 00:00:44 +0100

    Clean ResizePanel.cs

--- Assets/Scripts/SS3D/Systems/Chat/ResizePanel.cs
+++ Assets/Scripts/SS3D/Systems/Chat/ResizePanel.cs
@@ -1,33 +1,37 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 namespace SS3D.Engine.Chat
 {
-	public class ResizePanel : MonoBehaviour, IPointerDownHandler, IDragHandler {
-	
+	public class ResizePanel : MonoBehaviour, IPointerDownHandler, IDragHandler 
+    {
 		public Vector2 minSize = new Vector2 (100, 100);
 		public Vector2 maxSize = new Vector2 (400, 400);
 	
 		[SerializeField] private RectTransform panelRectTransform;
-		private Vector2 originalLocalPointerPosition;
-		private Vector2 originalSizeDelta;
+        
+		private Vector2 _originalLocalPointerPosition;
+		private Vector2 _originalSizeDelta;
 	
-		public void OnPointerDown (PointerEventData data) {
-			originalSizeDelta = panelRectTransform.sizeDelta;
-			RectTransformUtility.ScreenPointToLocalPointInRectangle (panelRectTransform, data.position, data.pressEventCamera, out originalLocalPointerPosition);
+		public void OnPointerDown(PointerEventData data) 
+        {
+			_originalSizeDelta = panelRectTransform.sizeDelta;
+			RectTransformUtility.ScreenPointToLocalPointInRectangle(panelRectTransform, data.position, data.pressEventCamera, out _originalLocalPointerPosition);
 		}
 	
-		public void OnDrag (PointerEventData data) {
-			if (panelRectTransform == null)
-				return;
+		public void OnDrag(PointerEventData data)
+        {
+            if (panelRectTransform == null)
+            {
+                return;
+            }
+
+			RectTransformUtility.ScreenPointToLocalPointInRectangle(panelRectTransform, data.position, data.pressEventCamera, out Vector2 localPointerPosition);
+			Vector3 offsetToOriginal = localPointerPosition - _originalLocalPointerPosition;
 		
-			Vector2 localPointerPosition;
-			RectTransformUtility.ScreenPointToLocalPointInRectangle (panelRectTransform, data.position, data.pressEventCamera, out localPointerPosition);
-			Vector3 offsetToOriginal = localPointerPosition - originalLocalPointerPosition;
-		
-			Vector2 sizeDelta = originalSizeDelta + new Vector2 (offsetToOriginal.x, -offsetToOriginal.y);
-			sizeDelta = new Vector2 (
+			Vector2 sizeDelta = _originalSizeDelta + new Vector2 (offsetToOriginal.x, -offsetToOriginal.y);
+			sizeDelta = new Vector2 
+            (
 				Mathf.Clamp (sizeDelta.x, minSize.x, maxSize.x),
 				Mathf.Clamp (sizeDelta.y, minSize.y, maxSize.y)
 			);
