557b77d19e57fe5bb54e7104c1a6b7ae0c02fa38
David Gómez <david.gomez.rooiz@gmail.com>
2024-02-22 22:15:33 +0100

    Use ViewLocator in ToggleChats

--- Assets/Scripts/SS3D/Systems/Chat/ToggleChats.cs
+++ Assets/Scripts/SS3D/Systems/Chat/ToggleChats.cs
@@ -1,14 +1,14 @@
-﻿using UnityEngine;
+﻿using SS3D.Core;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace SS3D.Engine.Chat
 {
     public class ToggleChats : MonoBehaviour
     {
-        [SerializeField] private Transform _chatWindowsContainer;
-        
         public void ToggleAllChats()
         {
-            InGameChatWindow[] allChatWindows = _chatWindowsContainer.GetComponentsInChildren<InGameChatWindow>();
+            List<InGameChatWindow> allChatWindows = ViewLocator.Get<InGameChatWindow>();
             bool hideChats = allChatWindows[0].CanvasGroup.alpha >= 1.0f;
             foreach (InGameChatWindow chatWindow in allChatWindows)
             {
