32564b729853123013166c929b3875c0e3eee80a
Ryan <ryanputh@gmail.com>
2024-08-19 22:30:11 +1000

    Create basic script to localize the font depending on locale. Required as existing font assets do not contain glyphs for all supported languages.

---
+++ Assets/Scripts/SS3D/UI/LocalizeFont.cs
@@ -0,0 +1,37 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Components;
+using UnityEngine.Localization.Settings;
+
+namespace SS3D.UI
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class LocalizeFont : MonoBehaviour
+    {
+        private LocalizedAsset<TMP_FontAsset> _fontAsset;
+        [SerializeField] private string _fontKey;
+        private TextMeshProUGUI _text;
+
+        public void Start()
+        {
+            ChangeHandler(LocalizationSettings.SelectedLocale);
+        }
+
+        protected void OnEnable()
+        {
+            _text = GetComponent<TextMeshProUGUI>();
+            LocalizationSettings.SelectedLocaleChanged += ChangeHandler;
+        }
+
+        protected void OnDisable()
+        {
+            LocalizationSettings.SelectedLocaleChanged -= ChangeHandler;
+        }
+
+        protected virtual void ChangeHandler(Locale value)
+        {
+            _text.font = LocalizationSettings.AssetDatabase.GetLocalizedAsset<TMP_FontAsset>("Font", _fontKey);
+        }
+    }
+}
