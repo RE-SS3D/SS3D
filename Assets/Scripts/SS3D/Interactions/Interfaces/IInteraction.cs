555b85f3f9aca779375974bcc7543901b691e3a5
Rohit Kulkarni <36757355+rohitkulkarni97@users.noreply.github.com>
2026-01-16 18:37:56 -0500

    Refactor IInteraction interface methods for default implementations from Interaction class.

--- Assets/Scripts/SS3D/Interactions/Interfaces/IInteraction.cs
+++ Assets/Scripts/SS3D/Interactions/Interfaces/IInteraction.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace SS3D.Interactions.Interfaces
 {
@@ -11,7 +10,7 @@ namespace SS3D.Interactions.Interfaces
         /// <summary>
         /// Creates a client interaction (client-side)
         /// </summary>
-        IClientInteraction CreateClient(InteractionEvent interactionEvent);
+        IClientInteraction CreateClient(InteractionEvent interactionEvent) => new ClientDelayedInteraction();
 
         /// <summary>
         /// Gets the name when interacted with a source
@@ -53,13 +52,16 @@ namespace SS3D.Interactions.Interfaces
         /// <param name="interactionEvent">The source used in the interaction</param>
         /// <param name="reference"></param>
         /// <returns>If the interaction should continue running</returns>
-        bool Update(InteractionEvent interactionEvent, InteractionReference reference);
+        bool Update(InteractionEvent interactionEvent, InteractionReference reference) => true;
 
         /// <summary>
         /// Called when the interaction is cancelled (server-side)
         /// </summary>
         /// <param name="interactionEvent">The source used in the interaction</param>
         /// <param name="reference"></param>
-        void Cancel(InteractionEvent interactionEvent, InteractionReference reference);
+        void Cancel(InteractionEvent interactionEvent, InteractionReference reference)
+        {
+            return;
+        }
     }
 }
