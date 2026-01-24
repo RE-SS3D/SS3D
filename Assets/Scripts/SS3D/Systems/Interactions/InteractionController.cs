b320c8557b4de50062f33fe36dc6007efee2df36
rohitkulkarni97 <36757355+rohitkulkarni97@users.noreply.github.com>
2026-01-24 12:38:31 -0500

    Update Assets/Scripts/SS3D/Systems/Interactions/InteractionController.cs
    Co-authored-by: iamteapot <77565416+iamteapot422@users.noreply.github.com>

--- Assets/Scripts/SS3D/Systems/Interactions/InteractionController.cs
+++ Assets/Scripts/SS3D/Systems/Interactions/InteractionController.cs
@@ -84,9 +84,6 @@ namespace SS3D.Systems.Interactions
             }
         }
 
-        /// <summary>
-        /// Function to subscribe to input events and toggle action maps when the owner is local client.
-        /// </summary>
         private void SubscribeToInput()
         {
             _controls.RunPrimary.performed += HandleRunPrimary;
