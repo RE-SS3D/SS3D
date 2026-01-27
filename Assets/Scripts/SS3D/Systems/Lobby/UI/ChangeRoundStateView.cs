a6bc0073991cb0df1e15bcb79f5461d2e536d26c
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2026-01-27 23:56:30 +0100

    Added a permission based view to the server settings rather than destroying the object

--- Assets/Scripts/SS3D/Systems/Lobby/UI/ChangeRoundStateView.cs
+++ Assets/Scripts/SS3D/Systems/Lobby/UI/ChangeRoundStateView.cs
@@ -1,16 +1,13 @@
 using Coimbra.Services.Events;
 using FishNet;
-using SS3D.Attributes;
 using SS3D.Core;
 using SS3D.Core.Behaviours;
+using SS3D.Logging;
+using SS3D.Permissions;
 using SS3D.Systems.Rounds;
 using SS3D.Systems.Rounds.Messages;
-using SS3D.UI;
 using SS3D.UI.Buttons;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.Serialization;
-using UnityEngine.UI;
 using RoundStateUpdated = SS3D.Systems.Rounds.Events.RoundStateUpdated;
 
 namespace SS3D.Systems.Lobby.UI
@@ -20,7 +17,7 @@ namespace SS3D.Systems.Lobby.UI
     /// </summary>
     public class ChangeRoundStateView : Actor
     {
-        [NotNull] [SerializeField] private ToggleLabelButton _startRoundButton;
+        [SerializeField] private ToggleLabelButton _startRoundButton;
 
         protected override void OnAwake()
         {
@@ -45,34 +42,49 @@ namespace SS3D.Systems.Lobby.UI
 
         private void HandleRoundStateUpdated(ref EventContext context, in RoundStateUpdated e)
         {
-            RoundState roundState = e.RoundState;
-
-            bool roundStopped = roundState == RoundState.Stopped;
-            bool roundOngoing = roundState == RoundState.Ongoing;
-            bool roundWarmingUp = roundState == RoundState.WarmingUp;
-            bool roundEnding = roundState == RoundState.Ending;
-            bool roundPreparing = roundState == RoundState.Preparing;
-
-            if (roundStopped)
+            switch (e.RoundState)
             {
-                _startRoundButton.Pressed = false;
-                _startRoundButton.Disabled = false;
-            }
-
-            if (roundOngoing || roundWarmingUp)
-            {
-                _startRoundButton.Pressed = true;
-                _startRoundButton.Disabled = false;
-            }
-
-            else if (roundEnding || roundPreparing)
-            {
-                _startRoundButton.Disabled = true;
+                case RoundState.Stopped:
+                    _startRoundButton.Pressed = false;
+                    _startRoundButton.Disabled = false;
+                    break;
+                case RoundState.WarmingUp:
+                case RoundState.Ongoing:
+                    _startRoundButton.Pressed = true;
+                    _startRoundButton.Disabled = false;
+                    break;
+                case RoundState.Preparing:
+                case RoundState.Ending:
+                    _startRoundButton.Disabled = true;
+                    break;
+                default:
+                    Log.Error(this, $"Unhandled round state: {e.RoundState}", Logs.UI);
+                    break;
             }
         }
 
         private void HandleEmbarkButtonPress(bool state)
         {
+            string ckey = Core.Settings.LocalPlayer.Ckey;
+            if (string.IsNullOrEmpty(ckey))
+            {
+                Log.Error(this, "Local player ckey is null or empty, cannot change round state", Logs.UI);
+                return;
+            }
+
+            PermissionSubSystem permissionSystem = SubSystems.Get<PermissionSubSystem>();
+            if (permissionSystem == null || !permissionSystem.HasLoadedPermissions)
+            {
+                Log.Error(this, "Permissions not loaded, cannot change round state", Logs.UI);
+                return;
+            }
+
+            if (!permissionSystem.IsAtLeast(ckey, ServerRoleTypes.Administrator))
+            {
+                Log.Error(this, "User {ckey} lacks permission to change round state", Logs.UI, ckey);
+                return;
+            }
+
             ChangeRoundState(state);
         }
 
