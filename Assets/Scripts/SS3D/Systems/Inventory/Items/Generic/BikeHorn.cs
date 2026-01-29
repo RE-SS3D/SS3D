39c854399171ed5299320922c833126b163f2f27
Rohit Kulkarni <36757355+rohitkulkarni97@users.noreply.github.com>
2026-01-29 11:32:16 -0500

    Fixing error of using interaction icon for audio clip (stupid me)

--- Assets/Scripts/SS3D/Systems/Inventory/Items/Generic/BikeHorn.cs
+++ Assets/Scripts/SS3D/Systems/Inventory/Items/Generic/BikeHorn.cs
@@ -26,7 +26,7 @@ namespace SS3D.Systems.Inventory.Items.Generic
         public void Honk()
         {
             _animator.SetTrigger(HonkAnimation);
-            AudioClip bikeHornClip = Assets.Get<AudioClip>(AssetDatabases.InteractionIcons, InteractionIcons.Take);
+            AudioClip bikeHornClip = Assets.Get<AudioClip>(AssetDatabases.Sounds, Sounds.BikeHorn);
             SubSystems.Get<AudioSubSystem>().PlayAudioSource(AudioType.Sfx, bikeHornClip, GameObject.transform.position, NetworkObject, false, 0.7f, 1, 1, 5);
         }
 
