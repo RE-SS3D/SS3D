4af9fe3184ba5d92a0717d0ba137f09965b07d64
Rohit Kulkarni <36757355+rohitkulkarni97@users.noreply.github.com>
2026-02-12 10:09:44 -0500

    Refactor crafting system to support asynchronous crafting and tile placement operations

--- Assets/Scripts/SS3D/Systems/Tile/TileMap.cs
+++ Assets/Scripts/SS3D/Systems/Tile/TileMap.cs
@@ -440,8 +440,6 @@ namespace SS3D.Systems.Tile
                     }
                 }
             }
-            
-            List<Task> loadingTasks = new List<Task>();
 
             foreach (SavedPlacedItemObject savedItem in saveObject.savedItemList)
             {
