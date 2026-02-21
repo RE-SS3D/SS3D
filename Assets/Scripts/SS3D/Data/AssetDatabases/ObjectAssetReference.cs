e60e07fcd792240ce8c142e07d4a0a637c6ca4a8
Rohit Kulkarni <36757355+rohitkulkarni97@users.noreply.github.com>
2026-02-21 17:35:26 -0500

    Refactor ObjectAssetReference class for improved readability and maintainability

--- Assets/Scripts/SS3D/Data/AssetDatabases/ObjectAssetReference.cs
+++ Assets/Scripts/SS3D/Data/AssetDatabases/ObjectAssetReference.cs
@@ -1,40 +1,40 @@
 ﻿using JetBrains.Annotations;
 using SS3D.Attributes;
-using System;
 using System.Linq;
-#if UNITY_EDITOR
-using UnityEditor.AddressableAssets;
-using UnityEditor.AddressableAssets.Settings;
-  #endif
 using UnityEngine;
 using Object = UnityEngine.Object;
+#if UNITY_EDITOR
+using SS3D.Logging;
+using UnityEditor.AddressableAssets;
+using UnityEditor.AddressableAssets.Settings;
+#endif
 
 namespace SS3D.Data.AssetDatabases
 {
-	/// <summary>
-	/// This SO is used to reference world object assets in inspector fields without worrying about losing data.
-	/// World object assets are anything that can be placed in the world as a GameObject, like items and tileobjects.
-	/// </summary>
-	public sealed class ObjectAssetReference : ScriptableObject
-	{
+    /// <summary>
+    /// This SO is used to reference world object assets in inspector fields without worrying about losing data.
+    /// World object assets are anything that can be placed in the world as a GameObject, like items and tileobjects.
+    /// </summary>
+    public sealed class ObjectAssetReference : ScriptableObject
+    {
 #if UNITY_EDITOR
         /// <summary>
         /// The path in the project where to put the ObjectAssetReference assets.
         /// </summary>
         private const string ObjectAssetPath = "Assets/Content/Data/WorldObjectAssetReferences/";
 #endif
-        
-#if UNITY_EDITOR
-		[ReadOnly]
-#endif
-		[Header("This file is auto-generated, do not modify it manually")]
-		public string Id;
 
 #if UNITY_EDITOR
-		[ReadOnly]
+        [ReadOnly]
 #endif
-		[Header("This file is auto-generated, do not modify it manually")]
-		public string Database;
+        [Header("This file is auto-generated, do not modify it manually")]
+        public string Id;
+
+#if UNITY_EDITOR
+        [ReadOnly]
+#endif
+        [Header("This file is auto-generated, do not modify it manually")]
+        public string Database;
 
         public override bool Equals(object other) => other is ObjectAssetReference otherReference && Id.Equals(otherReference.Id) && Database.Equals(otherReference.Database);
 
@@ -68,19 +68,20 @@ namespace SS3D.Data.AssetDatabases
 
                 return;
             }
-            
+
             string assetPath = UnityEditor.AssetDatabase.GetAssetPath(asset);
             string guid = UnityEditor.AssetDatabase.AssetPathToGUID(assetPath);
-            
+
             AddressableAssetSettings settings = AddressableAssetSettingsDefaultObject.Settings;
             AddressableAssetEntry entry = settings.FindAssetEntry(guid);
+
             if (entry == null)
             {
                 Log.Error(this, $"Asset {asset.name} with GUID {guid} not found in Addressable Asset Settings."
 
                 return;
             }
-            
+
             AssetDatabaseSettings databaseSettings = Coimbra.ScriptableSettings.GetOrFind<AssetDatabaseSettings>();
 
             if (!databaseSettings)
@@ -89,11 +90,12 @@ namespace SS3D.Data.AssetDatabases
 
                 return;
             }
-            
+
             foreach (AssetDatabase database in databaseSettings.IncludedAssetDatabases.Where(database => database.AssetGroup == entry.parentGroup))
             {
                 Id = guid;
                 Database = database.DatabaseID;
+
                 break;
             }
         }
