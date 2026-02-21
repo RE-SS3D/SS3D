9104912c7ee14e9e05a8c6e09a7a2c0fd6f3f949
Rohit Kulkarni <36757355+rohitkulkarni97@users.noreply.github.com>
2026-02-21 17:35:07 -0500

    Change ObjectAssetPath from public to private for encapsulation in ObjectAssetReference class

--- Assets/Scripts/SS3D/Data/AssetDatabases/ObjectAssetReference.cs
+++ Assets/Scripts/SS3D/Data/AssetDatabases/ObjectAssetReference.cs
@@ -21,7 +21,7 @@ namespace SS3D.Data.AssetDatabases
         /// <summary>
         /// The path in the project where to put the ObjectAssetReference assets.
         /// </summary>
-        public static readonly string ObjectAssetPath = "Assets/Content/Data/WorldObjectAssetReferences/";
+        private const string ObjectAssetPath = "Assets/Content/Data/WorldObjectAssetReferences/";
 #endif
         
 #if UNITY_EDITOR
