fe3d7757c17766ff5362e06c1bc89bdb26554b91
Rohit Kulkarni <rohit.kulkarni.271197@gmail.com>
2026-03-26 15:43:20 -0400

    Implement asset acquisition and release for container icons. - Loaded via new asset loading system - Lifecycle tied to the component's

--- Assets/Scripts/SS3D/Systems/Inventory/Containers/ContainerInteractive.cs
+++ Assets/Scripts/SS3D/Systems/Inventory/Containers/ContainerInteractive.cs
@@ -1,12 +1,13 @@
-﻿using SS3D.Data;
-using System;
-using System.Collections.Generic;
+﻿using SS3D.Core;
+using SS3D.Data;
+using SS3D.Data.Generated;
 using SS3D.Interactions;
 using SS3D.Interactions.Interfaces;
 using SS3D.Systems.Inventory.Interactions;
 using SS3D.Systems.Inventory.Items;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
-using SS3D.Data.Generated;
 
 namespace SS3D.Systems.Inventory.Containers
 {
@@ -20,6 +21,21 @@ namespace SS3D.Systems.Inventory.Containers
         public AttachedContainer attachedContainer;
         private Sprite _viewContainerIcon;
 
+        private AssetHandle<Sprite> _takeIconHandle;
+        private AssetHandle<Sprite> _openIconHandle;
+
+        protected override void OnAwake()
+        {
+            base.OnAwake();
+            AcquireAssets();
+        }
+
+        protected override void OnDestroyed()
+        {
+            base.OnDestroyed();
+            ReleaseAssets();
+        }
+
         public override IInteraction[] CreateTargetInteractions(InteractionEvent interactionEvent)
         {
             if (attachedContainer.HasCustomInteraction)
@@ -29,25 +45,20 @@ namespace SS3D.Systems.Inventory.Containers
 
             List<IInteraction> interactions = new();
 
-            Sprite takeIcon = AssetLoader.Get<Sprite>(AssetDatabases.InteractionIcons, InteractionIcons.Take);
-            Sprite openIcon = AssetLoader.Get<Sprite>(AssetDatabases.InteractionIcons, InteractionIcons.Open);
-
             StoreInteraction storeInteraction = new(attachedContainer)
             {
-                Icon = takeIcon,
+                Icon = _takeIconHandle?.Asset,
             };
 
             TakeFirstInteraction takeFirstInteraction = new(attachedContainer)
             {
-                Icon = takeIcon,
+                Icon = _takeIconHandle?.Asset,
             };
 
             ViewContainerInteraction view = new(attachedContainer)
             {
-                MaxDistance = attachedContainer.MaxDistance, Icon = _viewContainerIcon,
+                MaxDistance = attachedContainer.MaxDistance, Icon = _openIconHandle?.Asset,
             };
-
-            view.Icon = openIcon;
 
             // Pile or Normal the Store Interaction will always appear, but View only appears in Normal containers
             if (IsOpen() | !attachedContainer.OnlyStoreWhenOpen | !attachedContainer.IsOpenable)
@@ -71,7 +82,7 @@ namespace SS3D.Systems.Inventory.Containers
 
             OpenInteraction openInteraction = new(attachedContainer)
             {
-                Icon = openIcon,
+                Icon = _openIconHandle?.Asset,
             };
 
             openInteraction.OnOpenStateChanged += OpenStateChanged;
@@ -83,6 +94,15 @@ namespace SS3D.Systems.Inventory.Containers
         protected override void OpenStateChanged(object sender, bool e)
         {
             base.OpenStateChanged(sender, e);
+        }
+
+        protected override void SyncOpenState(bool oldVal, bool newVal, bool asServer)
+        {
+            base.SyncOpenState(oldVal, newVal, asServer);
+            if (!newVal)
+            {
+                CloseUis();
+            }
         }
 
         /// <summary>
@@ -115,13 +135,39 @@ namespace SS3D.Systems.Inventory.Containers
             }
         }
 
-        protected override void SyncOpenState(bool oldVal, bool newVal, bool asServer)
+        private async void AcquireAssets()
         {
-            base.SyncOpenState(oldVal, newVal, asServer);
-            if (!newVal)
+            if (!SubSystems.TryGet(out AssetSubSystem assetSubSystem) || !assetSubSystem)
             {
-                CloseUis();
+                return;
             }
+
+            _takeIconHandle = await assetSubSystem.AcquireAsync<Sprite>(InteractionIcons.Take);
+            _openIconHandle = await assetSubSystem.AcquireAsync<Sprite>(InteractionIcons.Open);
+            
+            ValidateHandle(ref _takeIconHandle);
+            ValidateHandle(ref _openIconHandle);
+        }
+
+        private void ValidateHandle<T>(ref AssetHandle<T> handle)
+            where T : class
+        {
+            if (handle is { IsValid: true })
+            {
+                return;
+            }
+            
+            handle.Dispose();
+            handle = null;
+        }
+
+        private void ReleaseAssets()
+        {
+            _takeIconHandle?.Dispose();
+            _openIconHandle?.Dispose();
+            
+            _takeIconHandle = null;
+            _openIconHandle = null;
         }
     }
 }
