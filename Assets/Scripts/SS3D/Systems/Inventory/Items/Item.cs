9e4f8516366b808be3c626d95eff38f7c3cf17f6
Rohit Kulkarni <rohit.kulkarni.271197@gmail.com>
2026-06-02 09:37:50 -0400

    Migrate Animator API rename for Unity 6
    Unity's API updater renamed Animator.keepAnimatorControllerStateOnDisable
    to keepAnimatorStateOnDisable (same semantics) in Item.OnStart.

--- Assets/Scripts/SS3D/Systems/Inventory/Items/Item.cs
+++ Assets/Scripts/SS3D/Systems/Inventory/Items/Item.cs
@@ -153,7 +153,7 @@ namespace SS3D.Systems.Inventory.Items
 
             foreach (Animator animator in GetComponents<Animator>())
             {
-                animator.keepAnimatorControllerStateOnDisable = true;
+                animator.keepAnimatorStateOnDisable = true;
             }
 
             // Clients don't need to calculate physics for rigidbodies as this is handled by the server
