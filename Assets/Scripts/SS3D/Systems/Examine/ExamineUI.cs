06b884760356b1c09cbe5a7692e3b3be1e4e150a
BroodjeKipcorn <henkhooft@gmail.com>
2026-07-03 15:24:31 +0200

    Show detailed examine description while holding Shift (#1394)
    Restore the basic detailed examine interaction so hover text expands to
    include the localized DescriptionKey when Shift is held.

--- Assets/Scripts/SS3D/Systems/Examine/ExamineUI.cs
+++ Assets/Scripts/SS3D/Systems/Examine/ExamineUI.cs
@@ -1,7 +1,5 @@
 ﻿using SS3D.Core;
 using SS3D.Core.Behaviours;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Localization.Tables;
@@ -11,8 +9,11 @@ namespace SS3D.Systems.Examine
     public class ExamineUI : Actor
     {
         [SerializeField] private TMP_Text HoverName;
+        [SerializeField] private KeyCode DetailedExamineKey = KeyCode.LeftShift;
 
         private StringTable _currentStringTable;
+        private IExaminable _currentExaminable;
+        private bool _wasDetailedExamineHeld;
 
         protected override void OnEnabled()
         {
@@ -22,8 +23,18 @@ namespace SS3D.Systems.Examine
 
         protected override void OnDisabled()
         {
-            base.OnEnabled();
+            base.OnDisabled();
             SubSystems.Get<ExamineSubSystem>().OnExaminableChanged -= UpdateHoverText;
+        }
+
+        private void Update()
+        {
+            bool isDetailedExamineHeld = IsDetailedExamineHeld();
+            if (isDetailedExamineHeld != _wasDetailedExamineHeld)
+            {
+                _wasDetailedExamineHeld = isDetailedExamineHeld;
+                UpdateHoverText(_currentExaminable);
+            }
         }
 
         /// <summary>
@@ -32,27 +43,64 @@ namespace SS3D.Systems.Examine
         /// <param name="examinable">The object that is being examined</param>
         private void UpdateHoverText(IExaminable examinable)
         {
-            string hoverTextToDisplay = string.Empty;
+            _currentExaminable = examinable;
+            _wasDetailedExamineHeld = IsDetailedExamineHeld();
+            HoverName.text = GetHoverText(examinable, _wasDetailedExamineHeld);
+        }
 
-            if (examinable?.GetData())
+        private string GetHoverText(IExaminable examinable, bool showDetailedText)
+        {
+            ExamineData data = examinable?.GetData();
+            if (data == null || data.LocalizationTable == null)
             {
-                ExamineData data = examinable.GetData();
-
-                if (data.LocalizationTable != null)
-                {
-                    _currentStringTable = data.LocalizationTable?.GetTable();
-                    if (_currentStringTable[data.NameKey]?.LocalizedValue is null)
-                    {
-                        hoverTextToDisplay = data.NameKey + " *[to be localized]*";
-                    }
-                    else
-                    {
-                        hoverTextToDisplay = _currentStringTable[data.NameKey]?.LocalizedValue;
-                    }
-                }
+                return string.Empty;
             }
 
-            HoverName.text = hoverTextToDisplay;
+            _currentStringTable = data.LocalizationTable.GetTable();
+            if (_currentStringTable == null)
+            {
+                return string.Empty;
+            }
+
+            string name = GetLocalizedValue(data.NameKey);
+
+            if (!showDetailedText)
+            {
+                return name;
+            }
+
+            string description = GetLocalizedValue(data.DescriptionKey);
+            if (string.IsNullOrEmpty(description))
+            {
+                return name;
+            }
+
+            if (string.IsNullOrEmpty(name))
+            {
+                return description;
+            }
+
+            return $"{name}\n{description}";
+        }
+
+        private string GetLocalizedValue(string key)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                return string.Empty;
+            }
+
+            if (_currentStringTable[key]?.LocalizedValue is null)
+            {
+                return key + " *[to be localized]*";
+            }
+
+            return _currentStringTable[key].LocalizedValue;
+        }
+
+        private bool IsDetailedExamineHeld()
+        {
+            return Input.GetKey(DetailedExamineKey) || Input.GetKey(KeyCode.RightShift);
         }
     }
 }
