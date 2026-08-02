d9ecb3b6a71a41a45225d4d28bab0dd1c1ef286c
iamteapot <77565416+iamteapot422@users.noreply.github.com>
2026-08-02 23:13:59 +0700

    cleanup 5

--- Assets/Scripts/SS3D/Systems/Vision/Editor/VisionSystemEditor.cs
+++ Assets/Scripts/SS3D/Systems/Vision/Editor/VisionSystemEditor.cs
@@ -1,346 +1,70 @@
-@@ -234,12 +234,6 @@ MonoBehaviour:
-  m_Script: {fileID: 11500000, guid: 26b716c41e9b56b4baafaf13a523ba2e, type: 3}
-  m_Name: 
-  m_EditorClassIdentifier: 
-  NetworkObserver: {fileID: 0}
-  <PrefabId>k__BackingField: 65535
-  <SpawnableCollectionId>k__BackingField: 0
-  _scenePathHash: 2499635508
-  <SceneId>k__BackingField: 10735852762468231620
-  <AssetPathHash>k__BackingField: 0
-  <IsNested>k__BackingField: 1
-  <ComponentIndex>k__BackingField: 3
-  <PredictedSpawn>k__BackingField: {fileID: 0}
-@@ -255,6 +249,12 @@ MonoBehaviour:
-  _isGlobal: 0
-  _initializeOrder: 0
-  _defaultDespawnType: 0
-  NetworkObserver: {fileID: 0}
-  <PrefabId>k__BackingField: 65535
-  <SpawnableCollectionId>k__BackingField: 0
-  _scenePathHash: 2499635508
-  <SceneId>k__BackingField: 10735852762468231620
-  <AssetPathHash>k__BackingField: 0
---- !u!1 &274660130
-GameObject:
-  m_ObjectHideFlags: 0
-@@ -434,12 +434,6 @@ MonoBehaviour:
-  m_Script: {fileID: 11500000, guid: 26b716c41e9b56b4baafaf13a523ba2e, type: 3}
-  m_Name: 
-  m_EditorClassIdentifier: 
-  NetworkObserver: {fileID: 0}
-  <PrefabId>k__BackingField: 65535
-  <SpawnableCollectionId>k__BackingField: 0
-  _scenePathHash: 2499635508
-  <SceneId>k__BackingField: 10735852762014670918
-  <AssetPathHash>k__BackingField: 0
-  <IsNested>k__BackingField: 1
-  <ComponentIndex>k__BackingField: 6
-  <PredictedSpawn>k__BackingField: {fileID: 0}
-@@ -455,6 +449,12 @@ MonoBehaviour:
-  _isGlobal: 0
-  _initializeOrder: 0
-  _defaultDespawnType: 0
-  NetworkObserver: {fileID: 0}
-  <PrefabId>k__BackingField: 65535
-  <SpawnableCollectionId>k__BackingField: 0
-  _scenePathHash: 2499635508
-  <SceneId>k__BackingField: 10735852762014670918
-  <AssetPathHash>k__BackingField: 0
---- !u!1 &478499394
-GameObject:
-  m_ObjectHideFlags: 0
-@@ -563,12 +563,6 @@ MonoBehaviour:
-  m_Script: {fileID: 11500000, guid: 26b716c41e9b56b4baafaf13a523ba2e, type: 3}
-  m_Name: 
-  m_EditorClassIdentifier: 
-  NetworkObserver: {fileID: 0}
-  <PrefabId>k__BackingField: 0
-  <SpawnableCollectionId>k__BackingField: 0
-  _scenePathHash: 2499635508
-  <SceneId>k__BackingField: 10735852762399064293
-  <AssetPathHash>k__BackingField: 0
-  <IsNested>k__BackingField: 1
-  <ComponentIndex>k__BackingField: 10
-  <PredictedSpawn>k__BackingField: {fileID: 0}
-@@ -584,6 +578,12 @@ MonoBehaviour:
-  _isGlobal: 0
-  _initializeOrder: 0
-  _defaultDespawnType: 0
-  NetworkObserver: {fileID: 0}
-  <PrefabId>k__BackingField: 0
-  <SpawnableCollectionId>k__BackingField: 0
-  _scenePathHash: 2499635508
-  <SceneId>k__BackingField: 10735852762399064293
-  <AssetPathHash>k__BackingField: 0
---- !u!1 &516802433
-GameObject:
-  m_ObjectHideFlags: 0
-@@ -629,12 +629,6 @@ MonoBehaviour:
-  m_Script: {fileID: 11500000, guid: 26b716c41e9b56b4baafaf13a523ba2e, type: 3}
-  m_Name: 
-  m_EditorClassIdentifier: 
-  NetworkObserver: {fileID: 0}
-  <PrefabId>k__BackingField: 65535
-  <SpawnableCollectionId>k__BackingField: 0
-  _scenePathHash: 2499635508
-  <SceneId>k__BackingField: 10735852762495497741
-  <AssetPathHash>k__BackingField: 0
-  <IsNested>k__BackingField: 1
-  <ComponentIndex>k__BackingField: 1
-  <PredictedSpawn>k__BackingField: {fileID: 0}
-@@ -650,6 +644,12 @@ MonoBehaviour:
-  _isGlobal: 0
-  _initializeOrder: 0
-  _defaultDespawnType: 0
-  NetworkObserver: {fileID: 0}
-  <PrefabId>k__BackingField: 65535
-  <SpawnableCollectionId>k__BackingField: 0
-  _scenePathHash: 2499635508
-  <SceneId>k__BackingField: 10735852762495497741
-  <AssetPathHash>k__BackingField: 0
---- !u!4 &516802436
-Transform:
-  m_ObjectHideFlags: 0
-@@ -830,12 +830,6 @@ MonoBehaviour:
-  m_Script: {fileID: 11500000, guid: 26b716c41e9b56b4baafaf13a523ba2e, type: 3}
-  m_Name: 
-  m_EditorClassIdentifier: 
-  NetworkObserver: {fileID: 0}
-  <PrefabId>k__BackingField: 65535
-  <SpawnableCollectionId>k__BackingField: 0
-  _scenePathHash: 2499635508
-  <SceneId>k__BackingField: 10735852761210609344
-  <AssetPathHash>k__BackingField: 0
-  <IsNested>k__BackingField: 1
-  <ComponentIndex>k__BackingField: 2
-  <PredictedSpawn>k__BackingField: {fileID: 0}
-@@ -851,6 +845,12 @@ MonoBehaviour:
-  _isGlobal: 0
-  _initializeOrder: 0
-  _defaultDespawnType: 0
-  NetworkObserver: {fileID: 0}
-  <PrefabId>k__BackingField: 65535
-  <SpawnableCollectionId>k__BackingField: 0
-  _scenePathHash: 2499635508
-  <SceneId>k__BackingField: 10735852761210609344
-  <AssetPathHash>k__BackingField: 0
---- !u!1 &661652615
-GameObject:
-  m_ObjectHideFlags: 0
-@@ -915,12 +915,6 @@ MonoBehaviour:
-  m_Script: {fileID: 11500000, guid: 26b716c41e9b56b4baafaf13a523ba2e, type: 3}
-  m_Name: 
-  m_EditorClassIdentifier: 
-  NetworkObserver: {fileID: 0}
-  <PrefabId>k__BackingField: 65535
-  <SpawnableCollectionId>k__BackingField: 0
-  _scenePathHash: 2499635508
-  <SceneId>k__BackingField: 10735852759222350558
-  <AssetPathHash>k__BackingField: 0
-  <IsNested>k__BackingField: 1
-  <ComponentIndex>k__BackingField: 7
-  <PredictedSpawn>k__BackingField: {fileID: 0}
-@@ -936,6 +930,12 @@ MonoBehaviour:
-  _isGlobal: 0
-  _initializeOrder: 0
-  _defaultDespawnType: 0
-  NetworkObserver: {fileID: 0}
-  <PrefabId>k__BackingField: 65535
-  <SpawnableCollectionId>k__BackingField: 0
-  _scenePathHash: 2499635508
-  <SceneId>k__BackingField: 10735852759222350558
-  <AssetPathHash>k__BackingField: 0
---- !u!224 &669009801 stripped
-RectTransform:
-  m_CorrespondingSourceObject: {fileID: 7839063151111624133, guid: ef47552bb578a784f8ef9916b369b6fb, type: 3}
-@@ -1284,12 +1284,6 @@ MonoBehaviour:
-  m_Script: {fileID: 11500000, guid: 26b716c41e9b56b4baafaf13a523ba2e, type: 3}
-  m_Name: 
-  m_EditorClassIdentifier: 
-  NetworkObserver: {fileID: 0}
-  <PrefabId>k__BackingField: 65535
-  <SpawnableCollectionId>k__BackingField: 0
-  _scenePathHash: 2499635508
-  <SceneId>k__BackingField: 10735852761931311011
-  <AssetPathHash>k__BackingField: 0
-  <IsNested>k__BackingField: 1
-  <ComponentIndex>k__BackingField: 1
-  <PredictedSpawn>k__BackingField: {fileID: 0}
-@@ -1305,6 +1299,12 @@ MonoBehaviour:
-  _isGlobal: 0
-  _initializeOrder: 0
-  _defaultDespawnType: 0
-  NetworkObserver: {fileID: 0}
-  <PrefabId>k__BackingField: 65535
-  <SpawnableCollectionId>k__BackingField: 0
-  _scenePathHash: 2499635508
-  <SceneId>k__BackingField: 10735852761931311011
-  <AssetPathHash>k__BackingField: 0
---- !u!114 &914529552 stripped
-MonoBehaviour:
-  m_CorrespondingSourceObject: {fileID: 4302234173120300195, guid: 313c7a6f7eaaf4f4cb80cbe0fe77005d, type: 3}
-@@ -1418,12 +1418,6 @@ MonoBehaviour:
-  m_Script: {fileID: 11500000, guid: 26b716c41e9b56b4baafaf13a523ba2e, type: 3}
-  m_Name: 
-  m_EditorClassIdentifier: 
-  NetworkObserver: {fileID: 0}
-  <PrefabId>k__BackingField: 65535
-  <SpawnableCollectionId>k__BackingField: 0
-  _scenePathHash: 2499635508
-  <SceneId>k__BackingField: 10735852758797354380
-  <AssetPathHash>k__BackingField: 0
-  <IsNested>k__BackingField: 0
-  <ComponentIndex>k__BackingField: 0
-  <PredictedSpawn>k__BackingField: {fileID: 0}
-@@ -1439,6 +1433,12 @@ MonoBehaviour:
-  _isGlobal: 0
-  _initializeOrder: 0
-  _defaultDespawnType: 0
-  NetworkObserver: {fileID: 0}
-  <PrefabId>k__BackingField: 65535
-  <SpawnableCollectionId>k__BackingField: 0
-  _scenePathHash: 2499635508
-  <SceneId>k__BackingField: 10735852758797354380
-  <AssetPathHash>k__BackingField: 0
---- !u!1 &1000564555
-GameObject:
-  m_ObjectHideFlags: 0
-@@ -1502,12 +1502,6 @@ MonoBehaviour:
-  m_Script: {fileID: 11500000, guid: 26b716c41e9b56b4baafaf13a523ba2e, type: 3}
-  m_Name: 
-  m_EditorClassIdentifier: 
-  NetworkObserver: {fileID: 0}
-  <PrefabId>k__BackingField: 65535
-  <SpawnableCollectionId>k__BackingField: 0
-  _scenePathHash: 2499635508
-  <SceneId>k__BackingField: 10735852761472596133
-  <AssetPathHash>k__BackingField: 0
-  <IsNested>k__BackingField: 1
-  <ComponentIndex>k__BackingField: 8
-  <PredictedSpawn>k__BackingField: {fileID: 0}
-@@ -1524,6 +1518,12 @@ MonoBehaviour:
-  _isGlobal: 0
-  _initializeOrder: 0
-  _defaultDespawnType: 0
-  NetworkObserver: {fileID: 0}
-  <PrefabId>k__BackingField: 65535
-  <SpawnableCollectionId>k__BackingField: 0
-  _scenePathHash: 2499635508
-  <SceneId>k__BackingField: 10735852761472596133
-  <AssetPathHash>k__BackingField: 0
---- !u!1001 &1002102147
-PrefabInstance:
-  m_ObjectHideFlags: 0
-@@ -1633,7 +1633,7 @@ PrefabInstance:
-      objectReference: {fileID: 0}
-    - target: {fileID: 4468402021075485018, guid: cc5383d385556f443b2a7d3202f2b2c1, type: 3}
-      propertyPath: m_AnchoredPosition.y
-      value: -230
-      value: -229.99995
-      objectReference: {fileID: 0}
-    - target: {fileID: 4468402021449001342, guid: cc5383d385556f443b2a7d3202f2b2c1, type: 3}
-      propertyPath: m_AnchoredPosition.x
-@@ -2302,12 +2302,6 @@ MonoBehaviour:
-  m_Script: {fileID: 11500000, guid: 26b716c41e9b56b4baafaf13a523ba2e, type: 3}
-  m_Name: 
-  m_EditorClassIdentifier: 
-  NetworkObserver: {fileID: 0}
-  <PrefabId>k__BackingField: 65535
-  <SpawnableCollectionId>k__BackingField: 0
-  _scenePathHash: 2499635508
-  <SceneId>k__BackingField: 10735852761335889925
-  <AssetPathHash>k__BackingField: 0
-  <IsNested>k__BackingField: 0
-  <ComponentIndex>k__BackingField: 0
-  <PredictedSpawn>k__BackingField: {fileID: 0}
-@@ -2339,6 +2333,12 @@ MonoBehaviour:
-  _isGlobal: 0
-  _initializeOrder: 0
-  _defaultDespawnType: 0
-  NetworkObserver: {fileID: 0}
-  <PrefabId>k__BackingField: 65535
-  <SpawnableCollectionId>k__BackingField: 0
-  _scenePathHash: 2499635508
-  <SceneId>k__BackingField: 10735852761335889925
-  <AssetPathHash>k__BackingField: 0
---- !u!114 &1509603938 stripped
-MonoBehaviour:
-  m_CorrespondingSourceObject: {fileID: 7356885341650864201, guid: df2268f4f5f6669459174c6d7e2043ce, type: 3}
-@@ -2427,12 +2427,6 @@ MonoBehaviour:
-  m_Script: {fileID: 11500000, guid: 26b716c41e9b56b4baafaf13a523ba2e, type: 3}
-  m_Name: 
-  m_EditorClassIdentifier: 
-  NetworkObserver: {fileID: 0}
-  <PrefabId>k__BackingField: 65535
-  <SpawnableCollectionId>k__BackingField: 0
-  _scenePathHash: 2499635508
-  <SceneId>k__BackingField: 10735852762284701941
-  <AssetPathHash>k__BackingField: 0
-  <IsNested>k__BackingField: 1
-  <ComponentIndex>k__BackingField: 5
-  <PredictedSpawn>k__BackingField: {fileID: 0}
-@@ -2448,6 +2442,12 @@ MonoBehaviour:
-  _isGlobal: 0
-  _initializeOrder: 0
-  _defaultDespawnType: 0
-  NetworkObserver: {fileID: 0}
-  <PrefabId>k__BackingField: 65535
-  <SpawnableCollectionId>k__BackingField: 0
-  _scenePathHash: 2499635508
-  <SceneId>k__BackingField: 10735852762284701941
-  <AssetPathHash>k__BackingField: 0
---- !u!1 &1679380531
-GameObject:
-  m_ObjectHideFlags: 0
-@@ -2524,16 +2524,16 @@ MonoBehaviour:
-  m_Script: {fileID: 11500000, guid: af299b0e71ae82ed3982954cf49b476c, type: 3}
-  m_Name: 
-  m_EditorClassIdentifier: 
-  showDebug: 0
-  visionMap: {fileID: 0}
-  target: {fileID: 0}
-  viewRange: 35
-  viewConeWidth: 360
-  obstacleMask:
-  Target: {fileID: 0}
-  _visionMap: {fileID: 0}
-  _viewRange: 35
-  _showDebug: 0
-  _viewConeWidth: 360
-  _obstacleMask:
-    serializedVersion: 2
-    m_Bits: 65536
-  resolution: 5
-  detectionOffset: {x: 0, y: 0, z: 0}
-  _resolution: 5
-  _detectionOffset: {x: 0, y: 0, z: 0}
---- !u!4 &1820032161 stripped
-Transform:
-  m_CorrespondingSourceObject: {fileID: 7939122254879796211, guid: 2dff3e99d61d74c4282d2983e482289c, type: 3}
-@@ -2584,12 +2584,6 @@ MonoBehaviour:
-  m_Script: {fileID: 11500000, guid: 26b716c41e9b56b4baafaf13a523ba2e, type: 3}
-  m_Name: 
-  m_EditorClassIdentifier: 
-  NetworkObserver: {fileID: 0}
-  <PrefabId>k__BackingField: 65535
-  <SpawnableCollectionId>k__BackingField: 0
-  _scenePathHash: 2499635508
-  <SceneId>k__BackingField: 10735852761315043637
-  <AssetPathHash>k__BackingField: 0
-  <IsNested>k__BackingField: 1
-  <ComponentIndex>k__BackingField: 4
-  <PredictedSpawn>k__BackingField: {fileID: 0}
-@@ -2605,6 +2599,12 @@ MonoBehaviour:
-  _isGlobal: 0
-  _initializeOrder: 0
-  _defaultDespawnType: 0
-  NetworkObserver: {fileID: 0}
-  <PrefabId>k__BackingField: 65535
-  <SpawnableCollectionId>k__BackingField: 0
-  _scenePathHash: 2499635508
-  <SceneId>k__BackingField: 10735852761315043637
-  <AssetPathHash>k__BackingField: 0
---- !u!114 &1927660779
-MonoBehaviour:
-  m_ObjectHideFlags: 0
+using Unity.Collections;
+using UnityEditor;
+using UnityEngine;
+
+namespace SS3D.Systems.Vision
+{
+    [CustomEditor(typeof(VisionSubSystem))]
+    public class VisionSystemEditor : Editor
+    {
+        private VisionSubSystem _vision;
+
+        private SerializedProperty _detectionOffsetProp;
+        private SerializedProperty _viewConeWidthProp;
+        private SerializedProperty _viewRangeProp;
+
+        protected void OnEnable()
+        {
+            _vision = (VisionSubSystem)target;
+
+            _detectionOffsetProp = serializedObject.FindProperty("_detectionOffset");
+            _viewConeWidthProp = serializedObject.FindProperty("_viewConeWidth");
+            _viewRangeProp = serializedObject.FindProperty("_viewRange");
+        }
+
+        protected void OnSceneGUI()
+        {
+            if (!_vision.ShowDebug)
+            {
+                return;
+            }
+
+            serializedObject.Update();
+
+            GUIStyle labelStyle = new()
+            {
+                fontSize = 14,
+            };
+            NativeArray<Vector3> viewPoints = _vision.ViewPoints;
+
+            Vector3 viewAngleA = _vision.DirectionFromAngle(-_viewConeWidthProp.floatValue / 2, false);
+            Vector3 viewAngleB = _vision.DirectionFromAngle(_viewConeWidthProp.floatValue / 2, false);
+
+            Vector3 center = _vision.Target.position + _detectionOffsetProp.vector3Value;
+
+            Handles.color = Color.green;
+            Handles.Label(center, "center");
+
+            labelStyle.normal.textColor = Color.green;
+            Handles.DrawWireArc(center, Vector3.up, Vector3.forward, 360, _viewRangeProp.floatValue);
+
+            Handles.DrawLine(center, center + (viewAngleA * _viewRangeProp.floatValue));
+            Handles.DrawLine(center, center + (viewAngleB * _viewRangeProp.floatValue));
+
+            // Draw Visual Vision outline;
+            Handles.color = Color.yellow;
+            GUIStyle style = new();
+            style.normal.textColor = Color.red;
+
+            Handles.Label(viewPoints[0], "0", style);
+            for (int i = 1; i <= _vision.StepCount; i++)
+            {
+                Handles.Label(viewPoints[i % _vision.StepCount], i.ToString(), style);
+                Handles.color = Color.yellow;
+                Handles.DrawLine(viewPoints[i % _vision.StepCount], center);
+                Handles.color = Color.cyan;
+                Handles.DrawLine(viewPoints[i % _vision.StepCount], viewPoints[(i + 1) % _vision.StepCount]);
+            }
+        }
+    }
+}
